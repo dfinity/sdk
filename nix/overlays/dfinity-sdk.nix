21d9561cb66ede0d6d3ce2aefd834661847b934e
Paul Young <paul.young@dfinity.org>
2019-08-14 17:10:06 -0700

    Add shell.nix to the root of the repo

--- nix/overlays/dfinity-sdk.nix
+++ nix/overlays/dfinity-sdk.nix
@@ -8,8 +8,10 @@ let dfx = super.callPackage ../../dfx/package.nix {
   dfinity-sdk = {
     inherit dfx;
 
-    # This is to make sure CI evalutes shell derivations, builds their dependencies
-    # and populates the hydra cache with them.
+    # This is to make sure CI evalutes shell derivations, builds their
+    # dependencies and populates the hydra cache with them. We also use this in
+    # `shell.nix` in the root to provide an environment which is the composition
+    # of all the shells here.
     shells = {
       dfx = import ../../dfx/shell.nix { pkgs = self; };
     };
