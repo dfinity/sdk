8733456b2c238bfc62a9f62518c8240f652d5976
Paul Young <paul.young@dfinity.org>
2019-08-15 17:38:30 -0700

    Revert "[WIP] SDK can send a user request SDK-207"
    This reverts commit bdf40ddff6c043320adb2bf1a7c1fb9561a87999.

--- dfx/src/main.rs
+++ dfx/src/main.rs
@@ -1,32 +1,3 @@
-extern crate serde;
-
-use serde::{Deserialize, Serialize};
-
-type Blob = String;
-type CanisterId = u64;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CanisterQueryCall {
-    canister_id: CanisterId,
-    method_name: String,
-    arg: Option<Blob>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-#[serde(tag = "message_type")]
-enum Message {
-    Query {
-        #[serde(flatten)]
-        message: CanisterQueryCall,
-    },
-}
-
-// TODO: https://github.com/dfinity-lab/dfinity/blob/ebeefdc6cf4a1d2c710fce91e0451dbfe0d75d1d/docs/spec/public/index.adoc#canister-query-call
-// * Use `reqwest` to make a canister query call to /api/v1/read
-// * Define a type containing response fields
-// * if response.status == replied then println!(status.replied) else exit(1)
-
 fn main() {
     println!("dfx");
     ::std::process::exit(1);
