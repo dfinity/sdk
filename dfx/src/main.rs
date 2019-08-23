9660dc19661ac1e0667a5c0a06c77047f8cf1907
Paul Young <paul.young@dfinity.org>
2019-08-23 16:19:14 -0700

    SDK-207 SDK-401: SDK can send a user request (#4)

--- dfx/src/main.rs
+++ dfx/src/main.rs
@@ -1,4 +1,34 @@
+use dfx::*;
+use futures::future::Future;
+
 fn main() {
-    println!("dfx");
-    ::std::process::exit(1);
+    let args: Vec<String> = std::env::args().collect();
+    let url = &args[1];
+    let client = Client::new(ClientConfig {
+        url: url.to_string(),
+    });
+    let query = query(
+        client,
+        CanisterQueryCall {
+            canister_id: 42,
+            method_name: "dfn_msg greet".to_string(),
+            arg: None,
+        },
+    )
+    .map(|r| match r {
+        Response::Accepted => println!("Accepted"),
+        Response::Replied {
+            reply: QueryResponseReply { arg: Blob(blob) },
+        } => println!("{}", String::from_utf8_lossy(&blob)),
+        Response::Rejected {
+            reject_code,
+            reject_message,
+        } => panic!(format!("{:?}, {}", reject_code, reject_message)),
+        Response::Unknown => panic!("Unknown response"),
+    })
+    .map_err(|e| {
+        println!("{:#?}", e);
+        ::std::process::exit(1);
+    });
+    tokio::run(query);
 }
