1994c42f76ac8f2ee472df2f236d5d91daedeb9b
Paul Young <paul.young@dfinity.org>
2019-09-03 16:34:50 -0700

    Remove unused config

--- dfx/src/commands/start.rs
+++ dfx/src/commands/start.rs
@@ -2,7 +2,6 @@ use crate::config::cache::{get_binary_path_from_config};
 use crate::config::dfinity::Config;
 use crate::lib::error::DfxResult;
 use clap::{App, Arg, ArgMatches, SubCommand};
-use console::style;
 
 pub fn construct() -> App<'static, 'static> {
     SubCommand::with_name("start")
@@ -15,30 +14,9 @@ pub fn construct() -> App<'static, 'static> {
         )
 }
 
-pub fn exec(args: &ArgMatches<'_>) -> DfxResult {
+pub fn exec(_args: &ArgMatches<'_>) -> DfxResult {
     // Read the config.
     let config = Config::from_current_dir()?;
-
-    let default_address = &config.get_config().get_defaults().get_start().address;
-    let default_address = default_address
-        .clone()
-        .unwrap_or_else(|| "127.0.0.1".to_owned());
-    let address = args
-        .value_of("address")
-        .unwrap_or_else(|| default_address.as_str());
-
-    // let nodes = match args.value_of("nodes") {
-    //     Some(n) => n.parse::<u64>()?,
-    //     None => config.get_config().get_defaults().get_start().get_nodes(1),
-    // };
-    let port = match args.value_of("port") {
-        Some(port) => port.parse::<u16>()?,
-        None => config
-            .get_config()
-            .get_defaults()
-            .get_start()
-            .get_port(8080),
-    };
 
     println!("Starting up the DFINITY node manager...");
 
@@ -53,12 +31,6 @@ pub fn exec(args: &ArgMatches<'_>) -> DfxResult {
     let _child = cmd.spawn()?;
 
     println!("DFINITY node manager started...");
-
-    let addr = format!("{}:{}", address, port);
-    println!(
-        "Listening for requests at {}",
-        style(format!("http://{}", addr)).blue().bold().underlined()
-    );
 
     Ok(())
 }
