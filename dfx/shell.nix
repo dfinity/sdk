0e1a80858cd1fb75d74c7fdc9fa67fdf775084e4
Paul Young <paul.young@dfinity.org>
2019-09-03 14:52:30 -0700

    Make executable assets available in shell.nix

--- dfx/shell.nix
+++ dfx/shell.nix
@@ -1,8 +1,21 @@
 { pkgs ? import ../. {} }:
 
+let assets = import ./assets.nix {
+  inherit (pkgs) actorscript dfinity;
+}; in
+
 pkgs.mkCiShell {
   name = "dfinity-sdk-dfx-env";
   inputsFrom = [
     pkgs.dfinity-sdk.dfx
   ];
+  shellHook = ''
+    ${assets.copy}
+
+    # Clean up before we exit the shell
+    trap "{ \
+      rm -rf ${assets.subdir}; \
+      exit 255; \
+    }" EXIT
+  '';
 }
