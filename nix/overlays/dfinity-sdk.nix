6a15cc4e09c3cd547f007c7754a33add6af3bf80
Paul Young <paul.young@dfinity.org>
2019-09-11 16:17:32 -0700

    Add top-level overlay

--- nix/overlays/dfinity-sdk.nix
+++ nix/overlays/dfinity-sdk.nix
@@ -2,15 +2,6 @@ self: super:
 
 let dfx = super.callPackage ../../dfx/package.nix {
   inherit (self) actorscript dfinity runCommand;
-
-  libressl = self.libressl_2_9;
-
-  # rustfmt needs to be compatible with rustc. The rust repo actually
-  # specifies the compatible rustfmt src using a git submodule.
-  # self.pkgsUnstable.rustfmt is a derivation for rustfmt based on that
-  # src. See: https://github.com/NixOS/nixpkgs/pull/66713
-  rustfmt = self.pkgsUnstable.rustfmt;
-  rls = self.pkgsUnstable.rls;
 }; in
 
 {
