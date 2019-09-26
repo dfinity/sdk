efcde08971fbacfba25a14940ea7ddbe9976424e
Paul Young <paul.young@dfinity.org>
2019-09-26 13:32:01 -0700

    Override installPhase so that it works as expected

--- js-user-library/package.nix
+++ js-user-library/package.nix
@@ -1,8 +1,17 @@
 { napalm }:
 
-napalm.buildPackage ./. {
+let package = napalm.buildPackage ./. {
   npmCommands = [
     "npm install"
     "npm test"
   ];
-}
+}; in
+
+package.overrideAttrs (oldAttrs: {
+  installPhase = ''
+    mkdir -p $out
+    cp package.json $out
+    cp README.adoc $out
+    cp -r out/ $out
+  '';
+})
