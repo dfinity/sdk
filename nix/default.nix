777ce9086b1f6acd9c2a37153374a4a26b4e4e26
Bas van Dijk <v.dijk.bas@gmail.com>
2019-09-27 10:50:27 +0200

    nix: allow overriding the path to the common repo

--- nix/default.nix
+++ nix/default.nix
@@ -8,10 +8,20 @@
 let
   # The `common` repo provides code (mostly Nix) that is used in the
   # `infra`, `dfinity` and `sdk` repositories.
-  commonSrc = builtins.fetchGit {
-    url = "ssh://git@github.com/dfinity-lab/common";
-    rev = "055572a3421f19204cc5534faa369046d50aa506";
-  };
+  #
+  # To conveniently test changes to a local `common` repo you point
+  # the `COMMON` environment variable to it. The path should be
+  # relative to the root of the `sdk` repo. For example:
+  #
+  #   COMMON="../common" nix-build -A rust-workspace
+  commonSrc =
+    let localCommonSrc = builtins.getEnv "COMMON"; in
+    if localCommonSrc != ""
+    then ../. + "/${localCommonSrc}"
+    else builtins.fetchGit {
+      url = "ssh://git@github.com/dfinity-lab/common";
+      rev = "055572a3421f19204cc5534faa369046d50aa506";
+    };
 in import commonSrc {
   inherit system crossSystem config;
   overlays = import ./overlays ++ overlays;
