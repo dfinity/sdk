c732cca47c9b27e1127d7f04bd9ca99eab5ba77f
Paul Young <paul.young@dfinity.org>
2019-09-27 16:03:37 -0700

    Override the name of the package derivation

--- js-user-library/package.nix
+++ js-user-library/package.nix
@@ -8,6 +8,7 @@ let package = napalm.buildPackage ./. {
 }; in
 
 package.overrideAttrs (oldAttrs: {
+  name = "dfinity-sdk-js-user-library";
   installPhase = ''
     mkdir -p $out
     cp package.json $out
