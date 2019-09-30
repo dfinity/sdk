058baeb9120adc4615ed9e7c06abdba610e0b737
Paul Young <paul.young@dfinity.org>
2019-09-30 13:55:55 -0700

    Fix quotes

--- js-user-library/webpack.config.js
+++ js-user-library/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "development",
   entry: "./out/index.js",
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   output: {
     libraryTarget: "umd",
   },
