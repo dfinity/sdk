e01e8ea058ff3e1dbf7775373a7bdb07e341d283
Paul Young <paul.young@dfinity.org>
2019-10-03 16:46:02 -0700

    Use built-in conversion to hex

--- js-user-library/src/buffer.ts
+++ js-user-library/src/buffer.ts
@@ -1,8 +1,5 @@
 import { Buffer } from "buffer";
 
 export const toHex = (buffer: Buffer): string => {
-  return Array
-    .from(buffer)
-    .map((x) => `00${x.toString(16)}`.slice(-2))
-    .join("");
+  return buffer.toString("hex");
 };
