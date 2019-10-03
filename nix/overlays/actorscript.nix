481365be93061a7157cbc5c96d33ad7aec421550
Hans Larsen <hans@larsen.online>
2019-10-03 12:56:20 -0700

    build: upgrade actorscript and dfinity to latest

--- nix/overlays/actorscript.nix
+++ nix/overlays/actorscript.nix
@@ -3,7 +3,7 @@ self: super:
 let src = builtins.fetchGit {
   url = "ssh://git@github.com/dfinity-lab/actorscript";
   ref = "master";
-  rev = "852e31c44742c31f77dd759a4888f3b727631f6f";
+  rev = "71e333aacd83e05d94c5720cb07419bcd0a1858e";
 }; in
 
 {
