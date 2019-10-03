c6c09413892f0c8c37168b8262e8d9ffef71e749
chenyan-dfinity <48968912+chenyan-dfinity@users.noreply.github.com>
2019-10-02 19:24:28 -0700

    IDL Deserialization for Rust (#40)

--- lib/serde_idl/src/lib.rs
+++ lib/serde_idl/src/lib.rs
@@ -2,26 +2,41 @@
 
 extern crate dfx_info;
 extern crate leb128;
+extern crate num_enum;
 extern crate serde;
 
-// Re-export the [items recommended by serde](https://serde.rs/conventions.html).
-//#[doc(inline)]
-//pub use crate::de::{from_str, Deserializer};
-#[doc(inline)]
+pub use crate::de::IDLDeserialize;
 pub use crate::error::{Error, Result};
-#[doc(inline)]
-pub use crate::ser::to_vec;
 
-//pub mod de;
+pub mod de;
 pub mod error;
-#[macro_use]
 pub mod ser;
 
 #[macro_export]
-macro_rules! IDL {
+macro_rules! Encode {
     ( $($x:expr),+ ) => {{
         let mut idl = serde_idl::ser::IDLBuilder::new();
         $(idl.arg($x);)+
         idl.to_vec().unwrap()
     }}
+}
+
+#[macro_export]
+macro_rules! Decode {
+    ( $hex:expr, $($name:ident: $ty:ty),+ ) => {
+        let mut de = serde_idl::de::IDLDeserialize::new($hex);
+        $(let $name: $ty = de.get_value().unwrap();)+
+        de.done().unwrap();
+    }
+}
+
+// IDL hash function is specified in
+// https://github.com/dfinity-lab/actorscript/blob/master/design/IDL.md#shorthand-symbolic-field-ids
+// which comes from https://caml.inria.fr/pub/papers/garrigue-polymorphic_variants-ml98.pdf
+pub fn idl_hash(id: &str) -> u32 {
+    let mut s: u32 = 0;
+    for c in id.chars() {
+        s = s.wrapping_mul(223).wrapping_add(c as u32);
+    }
+    s
 }
