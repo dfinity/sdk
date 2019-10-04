baefc317bb95781ed38f68156fdf5d4758119115
Hans Larsen <hans.larsen@dfinity.org>
2019-10-04 13:36:21 -0700

    refactor: allow Clone on config

--- dfx/src/commands/config.rs
+++ dfx/src/commands/config.rs
@@ -1,4 +1,4 @@
-use crate::config::dfinity::Config;
+use crate::lib::env::ProjectConfigEnv;
 use crate::lib::error::{DfxError, DfxResult};
 use clap::{App, Arg, ArgMatches, SubCommand};
 use serde_json::value::Value;
@@ -16,25 +16,41 @@ pub fn construct() -> App<'static, 'static> {
         ))
 }
 
-pub fn exec<T>(_env: &T, args: &ArgMatches<'_>) -> DfxResult {
+pub fn exec<T: ProjectConfigEnv>(env: &T, args: &ArgMatches<'_>) -> DfxResult {
     // Cannot use the `env` variable as we need a mutable copy.
-    let mut config = Config::from_current_dir()?;
+    let mut config = env
+        .get_config()
+        .ok_or_else(DfxError::CommandMustBeRunInAProject)?
+        .clone();
 
     let config_path = args.value_of("config_path").unwrap();
-    if let Some(value) = args.value_of("value") {
-        // Try to parse the type of the value (which is a string). By default we will just assume
-        // the type is string (if all parsing fails).
-        if let Ok(new_value) = serde_json::from_str(value) {
-            *config.get_mut_json().pointer_mut(config_path).unwrap() = new_value;
-        } else {
-            *config.get_mut_json().pointer_mut(config_path).unwrap() =
-                Value::String(value.to_owned());
-        }
+
+    // We replace `.` with `/` so the user can use `path.value.field` instead of forcing him
+    // to use `path/value/field`. Since none of our keys have slashes or tildes in them it
+    // won't be a problem.
+    let mut config_path = config_path.replace(".", "/");
+    // We change config path to starts with a `/` if it doesn't already. This is because
+    // JSON pointers can be relative, but we don't have a place to start if is it.
+    if !config_path.starts_with('/') {
+        config_path.insert(0, '/');
+    }
+
+    if let Some(arg_value) = args.value_of("value") {
+        // Try to parse the type of the value (which is a string from the arguments) as
+        // JSON. By default we will just assume the type is string (if all parsing fails).
+        let value = serde_json::from_str::<Value>(arg_value)
+            .unwrap_or_else(|_| Value::String(arg_value.to_owned()));
+
+        *config
+            .get_mut_json()
+            .pointer_mut(config_path.as_str())
+            .unwrap() = value;
+
         config.save()
-    } else if let Some(value) = config.get_json().pointer(config_path) {
+    } else if let Some(value) = config.get_json().pointer(config_path.as_str()) {
         println!("{}", value);
         Ok(())
     } else {
-        Err(DfxError::ConfigPathDoesNotExist(config_path.to_owned()))
+        Err(DfxError::ConfigPathDoesNotExist(config_path))
     }
 }
--- dfx/src/lib/error.rs
+++ dfx/src/lib/error.rs
@@ -30,6 +30,9 @@ pub enum DfxError {
     // Cannot create a new project because the directory already exists.
     ProjectExists(),
 
+    // Not in a project.
+    CommandMustBeRunInAProject(),
+
     // The client returned an error. It normally specifies the error as an
     // HTTP status (so 400-599), and has a string as the error message.
     // Once the client support errors from the public spec or as an enum,
