90c2c721d9c35c3c9f2d6882843aa6310d67dc17
Paul Young <paul.young@dfinity.org>
2019-10-07 16:12:28 -0700

    Update function signature

--- js-user-library/examples/hello/shell.nix
+++ js-user-library/examples/hello/shell.nix
@@ -34,8 +34,8 @@ pkgs.mkShell {
     echo "export default ({ IDL }) => {" > build/canisters/hello/main.js
     echo "  const Text = IDL.Text;" >> build/canisters/hello/main.js
     echo "  return new IDL.ActorInterface({" >> build/canisters/hello/main.js
-    echo "    'greet': IDL.Func(IDL.Obj({'0': Text}), IDL.Obj({'0': Text}))" >> build/canisters/hello/main.js
-    # echo "    'greet': IDL.Func([Text], [Text])" >> build/canisters/hello/main.js
+    # echo "    'greet': IDL.Func(IDL.Obj({'0': Text}), IDL.Obj({'0': Text}))" >> build/canisters/hello/main.js
+    echo "    'greet': IDL.Func([Text], [Text])" >> build/canisters/hello/main.js
     echo "  });" >> build/canisters/hello/main.js
     echo "};" >> build/canisters/hello/main.js
 
