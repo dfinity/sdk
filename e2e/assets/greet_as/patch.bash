d4d2454f1489429b73d770e103089b307e2a5422
Hans Larsen <hans.larsen@dfinity.org>
2019-10-08 08:57:30 -0700

    test: add a bit of e2e library to help with readability
    Batslib is a library thats not distributed on Nix, and assertions are custom
    made for this project.

---
+++ e2e/assets/greet_as/patch.bash
@@ -0,0 +1,1 @@
+dfx config canisters/hello/main greet.as
---
+++ e2e/utils/_.bash
@@ -0,0 +1,30 @@
+#!
+
+load utils/batslib
+load utils/assertions
+
+# Takes a name of the asset folder, and copy those files to the current project.
+install_asset() {
+    ASSET_ROOT=${BATS_TEST_DIRNAME}/assets/$1/
+    cp -R $ASSET_ROOT/* .
+
+    [ -f ./patch.bash ] && source ./patch.bash
+}
+
+dfx_new() {
+    dfx new e2e-project
+    test -d e2e-project
+    test -f e2e-project/dfinity.json
+    cd e2e-project
+
+    echo PWD: $(pwd) >&2
+}
+
+# Start the client in the background.
+dfx_start() {
+    # Bats create a FD 3 for test output, but child processes inherit it and Bats will
+    # wait for it to close. Because `dfx start` leave a child process running, we need
+    # to close this pipe, otherwise Bats will wait indefinitely.
+    dfx start --background "$@" 3>&-
+}
+
