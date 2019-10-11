878afdddc425e1db85009704b7c22f184966aa9d
Paul Young <paul.young@dfinity.org>
2019-10-11 12:01:49 -0700

    Remove commented out canister ID implementation

--- js-user-library/src/canisterId.ts
+++ js-user-library/src/canisterId.ts
@@ -7,10 +7,6 @@ import * as int from "./int";
 // represented as u64. This `Int` type will not be sufficient for u64 but may be
 // good enough for now.
 
-// export type CanisterId = BinaryBlob & { __canisterId__: void };
 export type CanisterId = Int & { __canisterId__: void };
 
-// export const fromHex = (hex: Hex): CanisterId => {
-//   return blob.fromHex(hex) as CanisterId;
-// };
 export const fromHex = (hex: Hex): CanisterId => int.fromHex(hex) as CanisterId;
