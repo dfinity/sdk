acb43393bd38b19cd014d5e4269999f1ba611bd9
Paul Young <paul.young@dfinity.org>
2019-10-11 14:02:53 -0700

    Reword comment about nonce since it matches spec

--- js-user-library/src/request.ts
+++ js-user-library/src/request.ts
@@ -5,9 +5,9 @@ import { RequestType } from "./requestType";
 export interface Request extends Record<string, any> {
   request_type: RequestType;
   // expiry?:;
-  // NOTE: `nonce` is optional in the spec, but we provide it so that requests
-  // are unique and we avoid a bug in the client when the same request is
-  // submitted more than once: https://dfinity.atlassian.net/browse/DFN-895
+  // NOTE: `nonce`, but we provide it so that requests are unique and we avoid a
+  // bug in the client when the same request is submitted more than once:
+  // https://dfinity.atlassian.net/browse/DFN-895
   nonce?: BinaryBlob;
   // sender:;
   sender_pubkey: BinaryBlob;
