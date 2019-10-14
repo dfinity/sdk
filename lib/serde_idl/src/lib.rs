2be3988c8926494445e10132be587917b2929f60
chenyan-dfinity <48968912+chenyan-dfinity@users.noreply.github.com>
2019-10-14 09:54:32 -0700

    Error handling, extra fields, and error handling for IDL deserialization (#53)
    * refactor and check_error
    
    * checkpoint for struct subtype
    
    * checkpoint
    
    * remove unwrap
    
    * checkpoint
    
    * struct subtype works
    
    * enum inside struct works
    
    * cargo fmt
    
    * cargo fmt
    
    * update doc
    
    * fix
    
    * fix
    
    * tests
    
    * remove eprintln to pass e2e
    
    * add states to error
    
    * fix
    
    * out-of-bounds check
    
    * make error module more generic to take strings
    
    * move idltype test to dfx_info; add docs for public functions
    
    * fix
    
    * fix doc

--- lib/serde_idl/src/lib.rs
+++ lib/serde_idl/src/lib.rs
@@ -1,4 +1,31 @@
 //! # Serde Dfinity IDL
+//!
+//! # Using the library
+//!
+//! ```
+//! use serde_idl::{IDLType, Deserialize, Encode, Decode};
+//! // Serialization
+//! let bytes = Encode!(&[(42, "text")], &(42, "text"));
+//! // Deserialization
+//! Decode!(&bytes, a: Vec<(i64, &str)>, b: (i32, String));
+//! assert_eq!(a, [(42, "text")]);
+//! assert_eq!(b, (42i32, "text".to_string()));
+//! ```
+//!
+//! # Serialize/Deserialize struct/enum
+//!
+//! ```
+//! # #[macro_use] extern crate serde_idl; fn main() {
+//! #[derive(IDLType, Deserialize)]
+//! struct List {
+//!     head: i32,
+//!     tail: Option<Box<List>>,
+//! }
+//! let list = List { head: 42, tail: None };
+//! let bytes = Encode!(&list);
+//! Decode!(&bytes, l: List);
+//! # }
+//! ```
 
 extern crate dfx_info;
 extern crate leb128;
@@ -7,6 +34,8 @@ extern crate serde;
 
 pub use crate::de::IDLDeserialize;
 pub use crate::error::{Error, Result};
+pub use dfx_info::IDLType;
+pub use serde::Deserialize;
 
 pub mod de;
 pub mod error;
@@ -14,31 +43,22 @@ pub mod ser;
 
 pub const EMPTY_DIDL: &[u8] = b"DIDL\0\0";
 
+/// Encode sequence of Rust values into IDL message.
 #[macro_export]
 macro_rules! Encode {
-    ( $($x:expr),+ ) => {{
+    ( $($x:expr),* ) => {{
         let mut idl = serde_idl::ser::IDLBuilder::new();
-        $(idl.arg($x);)+
+        $(idl.arg($x);)*
         idl.to_vec().unwrap()
     }}
 }
 
+/// Decode IDL message into a sequence of Rust values.
 #[macro_export]
 macro_rules! Decode {
-    ( $hex:expr, $($name:ident: $ty:ty),+ ) => {
+    ( $hex:expr, $($name:ident: $ty:ty),* ) => {
         let mut de = serde_idl::de::IDLDeserialize::new($hex);
-        $(let $name: $ty = de.get_value().unwrap();)+
-        de.done().unwrap();
+        $(let $name: $ty = de.get_value().unwrap();)*
+        de.done().unwrap()
     }
-}
-
-// IDL hash function is specified in
-// https://github.com/dfinity-lab/actorscript/blob/master/design/IDL.md#shorthand-symbolic-field-ids
-// which comes from https://caml.inria.fr/pub/papers/garrigue-polymorphic_variants-ml98.pdf
-pub fn idl_hash(id: &str) -> u32 {
-    let mut s: u32 = 0;
-    for c in id.chars() {
-        s = s.wrapping_mul(223).wrapping_add(c as u32);
-    }
-    s
 }
