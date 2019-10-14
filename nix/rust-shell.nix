5c467ee974cbfd9dd1ccc97801e49665851340b0
Nicolas Mattia <nicolas@dfinity.org>
2019-10-14 14:20:02 +0200

    INF-473: Don't require prebuilt deps in nix shell

--- nix/rust-shell.nix
+++ nix/rust-shell.nix
@@ -1,7 +1,16 @@
 { pkgs ? (import ./. {}).pkgs }:
 pkgs.mkCompositeShell {
   name = "dfinity-sdk-rust-env";
-  inputsFrom = [ pkgs.dfinity-sdk.packages.rust-workspace ];
+  inputsFrom = [
+
+    (pkgs.dfinity-sdk.packages.rust-workspace.overrideAttrs (_oldAttrs: {
+      # _oldAttrs.configurePhase refers to the dfinity-application-and-others-deps
+      # derivation which is the build of all 3rd-party Rust dependencies. Since in this
+      # nix-shell we use cargo locally to build all dependencies we don't need to depend
+      # on this derivation saving a lot of time downloading/building.
+      configurePhase = "";
+    })) ];
+
   shellHook = ''
     # Set CARGO_HOME to minimize interaction with any environment outside nix
     export CARGO_HOME=${pkgs.lib.dfinityRoot}/.cargo-home
