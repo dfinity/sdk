21554afd8768cf84f6f187b8a8dcb6fbb80724be
Paul Young <paul.young@dfinity.org>
2019-10-14 17:03:23 -0700

    Add ability to make an actor from an interface

---
+++ js-user-library/src/actor.ts
@@ -0,0 +1,88 @@
+import { BinaryBlob } from "./blob";
+import { HttpAgent } from "./httpAgent";
+import _IDL from "./IDL";
+import * as requestId from "./requestId";
+
+import {
+  RequestStatusResponse,
+  RequestStatusResponseStatus,
+} from "./requestStatusResponse";
+
+import retry from "async-retry";
+
+// Make an actor from an actor interface.
+//
+// Allows for one HTTP agent for the lifetime of the actor:
+//
+// ```
+// const actor = makeActor(actorInterface)(httpAgent);
+// const reply = await actor.greet();
+// ```
+//
+// or using a different HTTP agent for the same actor if necessary:
+//
+// ```
+// const actor = makeActor(actorInterface);
+// const reply1 = await actor(httpAgent1).greet();
+// const reply2 = await actor(httpAgent2).greet();
+// ```
+export const makeActor = (
+  makeActorInterface: ({ IDL }: { IDL: typeof _IDL }) => _IDL.ActorInterface,
+) => (
+  httpAgent: HttpAgent,
+): Record<string, (...args: Array<any>) => any> => {
+  const actorInterface = makeActorInterface({ IDL: _IDL });
+  const entries = Object.entries(actorInterface.__fields);
+  return Object.fromEntries(entries.map((entry) => {
+    const [methodName, func] = entry as [string, _IDL.Func];
+    return [methodName, async (...args: Array<any>) => {
+      // TODO
+      // * Throw if func.argTypes.length !== args.length
+      // * Encode arguments
+      const {
+        requestId: requestIdent,
+        response: callResponse,
+      } = await httpAgent.call({
+        methodName,
+        arg: Uint8Array.from([]) as BinaryBlob,
+      });
+
+      if (!callResponse.ok) {
+        throw new Error([
+          `Request failed:`,
+          `  Request ID: ${requestId.toHex(requestIdent)}`,
+          `  HTTP status code: ${callResponse.status}`,
+          `  HTTP status text: ${callResponse.statusText}`,
+        ].join("\n"));
+      }
+
+      const maxAttempts = 3;
+
+      const reply = await retry(async (bail, attempts) => {
+        const response: RequestStatusResponse = await httpAgent.requestStatus({
+          requestId: requestIdent,
+        });
+
+        switch (response.status) {
+          case RequestStatusResponseStatus.Replied: {
+            // TODO
+            // * Throw if func.retTypes.length !== response.reply.arg.length
+            // * Decode response
+            return response.reply.arg;
+          }
+          default: {
+            throw new Error([
+              `Failed to retrieve a reply for request after ${attempts} attempts:`,
+              `  Request ID: ${requestId.toHex(requestIdent)}`,
+              `  Request status: ${response.status}`,
+            ].join("\n"));
+          }
+        }
+      }, {
+        retries: maxAttempts - 1,
+      });
+
+      return reply;
+    }];
+  }));
+};
