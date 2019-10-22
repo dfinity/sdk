e77930d14b066811f15dab8110846b848dbf8f1d
chenyan-dfinity <yan.chen@dfinity.org>
2019-10-22 16:17:31 -0700

    another try

--- lib/idl_value/build.rs
+++ lib/idl_value/build.rs
@@ -3,6 +3,6 @@ extern crate lalrpop;
 fn main() {
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
-        .process_current_dir()
-        .unwrap();    
+        .process_file("src/idl.lalrpop")
+        .unwrap();
 }
