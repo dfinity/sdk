2981147e0e7c3f92467dde6b6653fbf93826df08
Paul Young <paul.young@dfinity.org>
2019-10-23 13:35:24 -0700

    Use a commit before HTTP handler was changed
    This is the commit before https://github.com/dfinity-lab/dfinity/pull/1623 was merged

--- nix/overlays/dfinity.nix
+++ nix/overlays/dfinity.nix
@@ -3,7 +3,7 @@ self: super:
 let src = builtins.fetchGit {
   url = "ssh://git@github.com/dfinity-lab/dfinity";
   # ref = "v0.2.0"; # TODO
-  rev = "b40b62471693f3700d91362d712fccef88255cec";
+  rev = "bd8b24d7e28638a73e616dabf6077a2869f14f32";
 }; in
 
 {
