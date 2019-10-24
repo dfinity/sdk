7721121d2762e6aedfbb90325ce29ea67db4e03a
Paul Young <paul.young@dfinity.org>
2019-10-23 17:25:25 -0700

    Upgrade dfinity to the latest commit on master

--- nix/overlays/dfinity.nix
+++ nix/overlays/dfinity.nix
@@ -3,7 +3,7 @@ self: super:
 let src = builtins.fetchGit {
   url = "ssh://git@github.com/dfinity-lab/dfinity";
   # ref = "v0.2.0"; # TODO
-  rev = "d0dc3a032c2dfa6782bcd8925fd9bf4a36b0870d";
+  rev = "b40b62471693f3700d91362d712fccef88255cec";
 }; in
 
 {
