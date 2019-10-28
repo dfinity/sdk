302f2cf2113b3d93c0936e79ac01c67224819ac3
chenyan-dfinity <yan.chen@dfinity.org>
2019-10-28 15:20:17 -0700

    fix

--- e2e/basic-project.bash
+++ e2e/basic-project.bash
@@ -123,5 +123,5 @@ teardown() {
     dfx canister install 1 canisters/counter_idl.wasm --wait
 
     assert_command dfx canister call 1 inc "(42,false,testzZ,vec{1;2;3},opt record{head=42; tail=opt record{head=43; tail=none}})" --wait
-    assert_eq "(record { 0 = 43; 1 = true; 2 = uftu{[; 3 = vec { 2; 3; 4; }; 4 = opt record { 1158359328 = 43; 1291237008 = opt record { 1158359328 = 44; 1291237008 = none; }; }; })"
+    assert_eq "(43, true, uftu{[, vec { 2; 3; 4; }, opt record { 1158359328 = 43; 1291237008 = opt record { 1158359328 = 44; 1291237008 = none; }; })"
 }
