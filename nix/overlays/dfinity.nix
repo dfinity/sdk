fd8113221fb685d4201ed879686aaadaa02e3e47
Paul Young <paul.young@dfinity.org>
2019-10-28 09:57:31 -0700

    Upgrade dfinity to allow queries via call

--- nix/overlays/dfinity.nix
+++ nix/overlays/dfinity.nix
@@ -3,7 +3,7 @@ self: super:
 let src = builtins.fetchGit {
   url = "ssh://git@github.com/dfinity-lab/dfinity";
   # ref = "v0.2.0"; # TODO
-  rev = "b40b62471693f3700d91362d712fccef88255cec";
+  rev = "b6d5e10dabb2665a7468809ecf49d1952b79a84a";
 }; in
 
 {
