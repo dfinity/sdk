c145847b7785ff47f22363b287312c72e3517de7
Paul Young <paul.young@dfinity.org>
2019-10-28 15:26:38 -0700

    Use run function [Cto capture output

--- e2e/print.bash
+++ e2e/print.bash
@@ -21,6 +21,6 @@ teardown() {
     dfx build
     dfx canister install 1 canisters/print.wasm --wait
     dfx canister call 1 hello --wait
-    tail -2 stderr.txt | head -1
+    run bash -c 'tail -2 stderr.txt | head -1'
     assert_eq "debug.print: Hello, World! from DFINITY"
 }
