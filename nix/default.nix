c123d7977765b4ff6a49180db5bdb3220ea26823
Nicolas Mattia <nicolas@dfinity.org>
2019-10-30 19:30:10 +0100

    Use isMaster to retrigger build on master (#111)

--- nix/default.nix
+++ nix/default.nix
@@ -19,7 +19,7 @@ let
     then localCommonSrc
     else builtins.fetchGit {
       url = "ssh://git@github.com/dfinity-lab/common";
-      rev = "3fd8991a369e21c69e4e713e627aaa325f420c73";
+      rev = "9034bf84910c4cc23a41c8363c5f725f63d5a81b";
     };
 in import commonSrc {
   inherit system crossSystem config;
