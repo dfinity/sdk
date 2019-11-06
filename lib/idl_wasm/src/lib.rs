1997f8cc4354532fecf04d03814541705b7f8dbc
chenyan-dfinity <yan.chen@dfinity.org>
2019-11-06 12:03:55 -0800

    checkpoint

--- lib/idl_wasm/src/lib.rs
+++ lib/idl_wasm/src/lib.rs
@@ -1,11 +1,48 @@
-extern crate wasm_bindgen;
 extern crate serde_idl;
+extern crate wasm_bindgen;
 
-use wasm_bindgen::prelude::*;
 use serde_idl::IDLArgs;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn encode(str: &str) -> Vec<u8> {
     let args: IDLArgs = str.parse().unwrap();
     args.to_bytes().unwrap()
+}
+
+/*
+#[wasm_bindgen]
+pub fn decode(bytes: &[u8]) -> IDLArgs {
+    IDLArgs::from_bytes(bytes).unwrap()
+}*/
+
+#[wasm_bindgen]
+pub enum Type {
+    Null,
+    Bool,
+    Nat,
+    Int,
+    Text,
+    Opt,
+    Vec,
+    Record,
+    Variant,
+}
+
+#[wasm_bindgen]
+pub struct Value {
+    pub ty: Type,
+    pub bval: bool,
+    pub nat: u64,
+    pub int: i64,
+    //pub opt: Option<Box<Value>>,
+    pub vec: Vec<Value>,
+    pub record: Vec<Field>,
+    pub variant: Field,
+}
+
+#[wasm_bindgen]
+pub struct Field {
+    pub id: u32,
+    pub val: Value,
 }
