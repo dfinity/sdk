6a0f64cdb502926fd325f185476be595105ec11c
Paul Young <paul.young@dfinity.org>
2019-11-08 14:38:53 -0800

    Remove export in response to webpack warning

--- js-user-library/src/index.ts
+++ js-user-library/src/index.ts
@@ -1,6 +1,6 @@
 export { makeActor } from "./actor";
-export { HttpAgent, makeHttpAgent } from "./httpAgent";
 export { generateKeyPair } from "./auth";
+export { makeHttpAgent } from "./httpAgent";
 
 import * as IDL from "./IDL";
 export { IDL };
