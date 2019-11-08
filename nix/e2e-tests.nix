978ee425020b3437edc9db1829a3b507e480a17a
Bas van Dijk <bas@dfinity.org>
2019-11-08 10:52:19 +0100

    nix/e2e-tests.nix: use niv instead of builtins.fetchGit (#150)
    We should use `builtins.fetchGit` as less as possible because it
    fetches at evaluation time (causing more stress on the hydra-evaluator
    and making evaluation slower in general). And it's not supported in
    strict evaluation mode which we want to move to at some point.
    
    For now we should only use `builtins.fetchGit` for fetching private
    repositories.
    
    All other sources should be fetched using `niv`. This is easier anyway
    because `niv` handles hash updates.

--- nix/e2e-tests.nix
+++ nix/e2e-tests.nix
@@ -6,20 +6,15 @@
 ,   runCommandNoCC
 ,   stdenv
 ,   killall
+,   sources
 }:
-let batslib = builtins.fetchGit {
-    url = "https://github.com/ztombol/bats-support";
-    ref = "refs/tags/v0.3.0";
-    rev = "24a72e14349690bcbf7c151b9d2d1cdd32d36eb1";
-}; in
-
 runCommandNoCC "e2e-tests" {
-    buildInputs = [ bats batslib coreutils curl dfinity-sdk.packages.rust-workspace-debug stdenv.cc killall netcat ];
+    buildInputs = [ bats coreutils curl dfinity-sdk.packages.rust-workspace-debug stdenv.cc killall netcat ];
 } ''
     # We want $HOME/.cache to be in a new temporary directory.
     export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
     # We use BATSLIB in our scripts to find the root of the BATSLIB repo.
-    export BATSLIB="${batslib}"
+    export BATSLIB="${sources.bats-support}"
 
     # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
     # less resources than a dev's computer, so e2e might take longer.
