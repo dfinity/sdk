aee9fb2430352b46b973b02ec937312a25caa358
Paul Young <paul.young@dfinity.org>
2019-11-15 11:34:42 -0800

    Use binary blob abstraction

--- js-user-library/src/canisterId.ts
+++ js-user-library/src/canisterId.ts
@@ -1,5 +1,5 @@
 import borc from "borc";
-import { Buffer } from "buffer/";
+import * as blob from "./blob";
 import { CborTag } from "./cbor";
 import { Hex } from "./hex";
 
@@ -9,8 +9,8 @@ export type CanisterId = borc.Tagged & { __canisterID__: void };
 export const fromHex = (hex: Hex): CanisterId => {
   return new borc.Tagged(
     CborTag.Uint64LittleEndian,
-    Buffer.from(`0x${hex}`),
+    blob.fromHex(hex),
   ) as CanisterId;
 };
 
-export const toHex = (id: CanisterId) => id.value.toString("hex");
+export const toHex = (id: CanisterId) => blob.toHex(id.value);
