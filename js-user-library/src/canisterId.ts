f2e77a76d25ef387c6d96337c59dfb664ca097f9
Paul Young <paul.young@dfinity.org>
2019-11-15 18:38:16 -0800

    Attempt to represent CanisterId using bignumber.js

--- js-user-library/src/canisterId.ts
+++ js-user-library/src/canisterId.ts
@@ -1,16 +1,13 @@
-import borc from "borc";
-import * as blob from "./blob";
-import { CborTag } from "./cbor";
+import BigNumber from "bignumber.js";
 import { Hex } from "./hex";
 
 // Canister IDs are represented as u64 in the HTTP handler of the client.
-export type CanisterId = borc.Tagged & { __canisterID__: void };
+export type CanisterId = BigNumber & { __canisterID__: void };
 
 export const fromHex = (hex: Hex): CanisterId => {
-  return new borc.Tagged(
-    CborTag.Uint64LittleEndian,
-    blob.fromHex(hex),
-  ) as CanisterId;
+  return new BigNumber(`0x${hex}`) as CanisterId;
 };
 
-export const toHex = (id: CanisterId) => blob.toHex(id.value);
+export const toHex = (id: CanisterId): Hex => {
+  return id.toString(16) as Hex;
+};
