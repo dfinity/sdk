e1ddff7d89388866d60e0c68259494516f40430b
Hans Larsen <hans@larsen.online>
2019-11-25 11:39:46 -0800

    fixup

--- dfx/src/commands/cache/show.rs
+++ dfx/src/commands/cache/show.rs
@@ -1,9 +1,8 @@
-use crate::config::{cache, dfx_version};
+use crate::config::cache;
 use crate::lib::env::VersionEnv;
 use crate::lib::error::DfxResult;
 use crate::lib::message::UserMessage;
 use clap::{App, ArgMatches, SubCommand};
-use std::io::Write;
 
 pub fn construct() -> App<'static, 'static> {
     SubCommand::with_name("show").about(UserMessage::CacheShow.to_str())
@@ -13,6 +12,6 @@ pub fn exec<T>(env: &T, _args: &ArgMatches<'_>) -> DfxResult
 where
     T: VersionEnv,
 {
-    println!("{}", cache::get_bin_cache(env.get_version())?);
+    println!("{:?}", cache::get_bin_cache(env.get_version())?);
     Ok(())
 }
