ba0f83d0fa2c81b857a02ff65af840e4e6cf4f30
Hans <hans.larsen@dfinity.org>
2019-12-01 15:21:08 -0800

    build: allow split of install.sh (#199)
    And remove comments and separate the linting into its own derivation. That
    should allow us to split up in different modules (great) and also lint
    locally without having to "do a release".

---
+++ public/install/000_header.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+## 000_header.sh
+##
+## Borrowed from rustup (https://sh.rustup.rs)
+##
+## This is just a little script that can be downloaded from the internet to
+## install the DFINITY SDK. It just does platform detection, downloads the installer
+## and runs it.
+##
+## You are NOT AUTHORIZED to remove any license agreements or prompts from the following script.
+##
+set -u
