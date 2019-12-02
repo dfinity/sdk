01e604d43b479107d3a7eca05696e97fb7a71562
chenyan-dfinity <yan.chen@dfinity.org>
2019-12-02 12:33:22 -0800

    bump

--- nix/overlays/dfinity.nix
+++ nix/overlays/dfinity.nix
@@ -4,7 +4,7 @@ let src = builtins.fetchGit {
   name = "dfinity-sources";
   url = "ssh://git@github.com/dfinity-lab/dfinity";
   ref = "master";
-  rev = "5c7efff0524adbf97d85b27adb180e6137a3428f";
+  rev = "a77f9b30fa5b1f35bef2913c2329e2c8e81c1af8";
 }; in
 
 {
