9ef41ffc94b6a2dd1a7345a3554248359f641596
Nicolas Mattia <nicolas@dfinity.org>
2019-12-03 13:52:50 +0100

    INF-673: Don't set repoRoot globally (#217)
    This removes the overlay setting `repoRoot` and removes uses of `pkgs.repoRoot`.
    
    This was dangerous, as repositories importing `common` would override it.

--- nix/rust-shell.nix
+++ nix/rust-shell.nix
@@ -6,7 +6,7 @@ pkgs.mkCompositeShell {
   inputsFrom = [ shell ];
   shellHook = ''
     # Set CARGO_HOME to minimize interaction with any environment outside nix
-    export CARGO_HOME=${pkgs.lib.dfinityRoot}/.cargo-home
+    export CARGO_HOME=${if pkgs.lib.isHydra then "." else toString ../.}/.cargo-home
 
     # Set environment variable for debug version.
     export DFX_TIMESTAMP_DEBUG_MODE_ONLY=$(date +%s)
