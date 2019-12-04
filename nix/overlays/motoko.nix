17d90c60341eb56a9013b216e6939d642632476c
Joachim Breitner <mail@joachim-breitner.de>
2019-12-04 12:57:13 +0100

    Bump motoko (less restrictions around await)

--- nix/overlays/motoko.nix
+++ nix/overlays/motoko.nix
@@ -4,7 +4,7 @@ let src = builtins.fetchGit {
   name = "motoko-sources";
   url = "ssh://git@github.com/dfinity-lab/motoko";
   ref = "joachim/ic-calls-on-drun";
-  rev = "881ec6855c3859eb45ed97625ac2062fdc9cafd5";
+  rev = "b2899e3c7b48ce90032739a45c4d61b60ecc5bf8";
 }; in
 
 {
