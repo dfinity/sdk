dd1d55c79adfdf9b9be60bfe5404fa29403962a9
chenyan-dfinity <48968912+chenyan-dfinity@users.noreply.github.com>
2019-12-04 12:05:45 -0800

    bump motoko (#215)
    * bump motoko
    
    * bump dfinity

--- nix/overlays/motoko.nix
+++ nix/overlays/motoko.nix
@@ -4,7 +4,7 @@ let src = builtins.fetchGit {
   name = "motoko-sources";
   url = "ssh://git@github.com/dfinity-lab/motoko";
   ref = "master";
-  rev = "2f71cfc9590741425db752a029e0758f94284e79";
+  rev = "62f6e414583d0c7efa1bce04690f4bbc4d789fba";
 }; in
 
 {
