82d9e4bdb1949118c9f02f0b9b28521626258100
Bas van Dijk <bas@dfinity.org>
2019-12-20 12:26:17 +0100

    ci: add the dfinity-sdk.packages.cargo-security-audit job (#265)
    The `dfinity-sdk.packages.cargo-security-audit` job only exists in the
    `sdk` jobset (i.e. the jobset which corresponds to the `master`
    branch).
    
    The job will run `cargo audit` which scans `Cargo.lock` for security
    vulnerabilities in crates reported in the RustSec advisory-db. The DB
    is an input to the jobset. This means that whenever a new
    vulnerability is reported or when `Cargo.lock` changes `cargo audit`
    will run.

--- ci/ci-pr.nix
+++ ci/ci-pr.nix
@@ -1,4 +1,4 @@
 # This file is used to govern CI jobs for GitHub PRs
 
 args@{supportedSystems ? [ "x86_64-linux" ], ...}:
-import ./ci.nix (args // { inherit supportedSystems; })
+import ./ci.nix (args // { inherit supportedSystems; isMaster = false; })
--- ci/ci.nix
+++ ci/ci.nix
@@ -1,9 +1,14 @@
 { supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
 , scrubJobs ? true
+, RustSec-advisory-db ? null
+, isMaster ? true
 }:
 let pkgs = import ../nix {};
 in
 pkgs.ci ../jobset.nix
-  { inherit supportedSystems scrubJobs; isMaster = true;
+  { inherit supportedSystems scrubJobs isMaster;
     rev = pkgs.lib.commitIdFromGitRepo (pkgs.lib.gitDir ../.);
+    packageSetArgs = {
+      inherit RustSec-advisory-db;
+    };
   }
