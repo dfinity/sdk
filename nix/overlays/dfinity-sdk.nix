19726ab80c6e9a0babe34efefab2d45a41974c4d
Nicolas Mattia <nicolas@dfinity.org>
2020-01-06 14:50:54 +0100

    Make public directory buildable
    The public directory is now buildable with `nix-build ./public` as opposed to `nix-build -A pkgs.dfinity-sdk.packages.public`.

--- nix/overlays/dfinity-sdk.nix
+++ nix/overlays/dfinity-sdk.nix
@@ -29,7 +29,7 @@ in {
 
         e2e-tests = super.callPackage ../e2e-tests.nix {};
 
-        public-folder = super.callPackage ../public.nix {};
+        public-folder = import ../../public { pkgs = super; };
     } //
     # We only run `cargo audit` on the `master` branch so to not let PRs
     # fail because of an updated RustSec advisory-db. Also we only add the
