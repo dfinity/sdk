e67b1776985f5b324f74f5c118151043c6b5a32b
chenyan-dfinity <yan.chen@dfinity.org>
2020-01-08 20:30:53 -0800

    add tests

--- src/userlib/js/src/utils/hash.test.ts
+++ src/userlib/js/src/utils/hash.test.ts
@@ -11,7 +11,12 @@ test('IDL label', () => {
   testLabel('short_name', 3261810734);
   testLabel('Hi ☃', 1419229646);
   testLabel('_0_', 0);
+  testLabel('_1_', 1);
+  testLabel('_+1_', 1055658234);
+  testLabel('_-1_', 1055757692);
   testLabel('_123_', 123);
+  testLabel('_4294967295_', 4294967295);
+  testLabel('_4294967296_', 1569808370);
   testLabel('_0xa_', 10);
   testLabel('_0d_', 1055918252);
   testLabel('_1.23_', 1360503298);
