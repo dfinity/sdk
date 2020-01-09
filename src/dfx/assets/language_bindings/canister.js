420e280680ae1597b1eb58ad70041e72fe440b0c
Hans <hans.larsen@dfinity.org>
2020-01-09 13:35:54 -0800

    feat: fetch canister javascript from the canister assets (#295)
    * feat: fetch canister javascript from the canister assets
    
    * fixup: build failure fixes
    
    * fixup: default index html file
    
    * fixup: fix tests
    
    * fixup: comments and discovery
    
    * fixup: comments and discovery

--- src/dfx/assets/language_bindings/canister.js
+++ src/dfx/assets/language_bindings/canister.js
@@ -1,36 +1,5 @@
 import actorInterface from "ic:idl/{project_name}";
-import {
-  generateKeyPair,
-  makeActorFactory,
-  makeAuthTransform,
-  HttpAgent,
-  makeNonceTransform,
-} from "ic:userlib";
 
-if (!window.icHttpAgent) {
-  const identityIndex = "dfinity-ic-user-identity";
-  let k = window.localStorage.getItem(identityIndex);
-  let keyPair;
-  if (k) {
-    keyPair = JSON.parse(k);
-  } else {
-    keyPair = generateKeyPair();
-    // TODO(eftycis): use a parser+an appropriate format to avoid
-    // leaking the key when constructing the string for
-    // localStorage.
-    window.localStorage.setItem(identityIndex, JSON.stringify(keyPair));
-  }
-
-  const agent = new HttpAgent({});
-  agent.addTransform(makeNonceTransform());
-  agent.addTransform(makeAuthTransform(keyPair));
-
-  window.icHttpAgent = agent;
-}
-
-
-const actor = makeActorFactory(actorInterface)({
+export default icHttpAgent.makeActorFactory(actorInterface)({
   canisterId: "{canister_id}",
 });
-
-export default actor;
--- src/userlib/js/src/index.ts
+++ src/userlib/js/src/index.ts
@@ -1,5 +1,6 @@
 export * from './actor';
-export { generateKeyPair, makeAuthTransform } from './auth';
+export { generateKeyPair, makeAuthTransform, makeKeyPair } from './auth';
+export * from './canisterId';
 export * from './http_agent';
 export * from './http_agent_transforms';
 export * from './http_agent_types';
