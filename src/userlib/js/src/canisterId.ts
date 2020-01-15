47ce741485818f417de66449265e1dc3b350c1fa
chenyan-dfinity <yan.chen@dfinity.org>
2020-01-14 17:32:46 -0800

    revert ts change

--- src/userlib/js/src/canisterId.ts
+++ src/userlib/js/src/canisterId.ts
@@ -1,13 +1,3 @@
-const changeEndianness = (str: string) => {
-        const result = [];
-        let len = str.length - 2;
-        while (len >= 0) {
-          result.push(str.substr(len, 2));
-          len -= 2;
-        }
-        return result.join('');
-}
-
 // Canister IDs are represented as u64 in the HTTP handler of the client.
 export class CanisterId {
   public static fromText(hex: string): CanisterId {
@@ -17,8 +7,7 @@ export class CanisterId {
       return CanisterId.fromText(hex.slice(3, -2));
     }
 
-    console.log("HERE");
-    return new this(changeEndianness(hex));
+    return new this(hex.padStart(16, '0'));
   }
 
   protected constructor(private _idHex: string) {}
