3e8fd97006570f8f49b3cdb17975932c78f4ddea
Enzo Haussecker <enzo@sovereign.io>
2020-01-27 14:11:07 -0800

    Remove another header

--- src/dfx/src/commands/bootstrap/mod.rs
+++ src/dfx/src/commands/bootstrap/mod.rs
@@ -1,9 +1,3 @@
-//! File       : mod.rs
-//! License    : Apache 2.0 with LLVM Exceptions
-//! Copyright  : 2020 DFINITY Stiftung
-//! Maintainer : Enzo Haussecker <enzo@dfinity.org>
-//! Stability  : Experimental
-
 mod configure;
 
 pub mod construct;
