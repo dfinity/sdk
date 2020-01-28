e2ebcc4b5f2ddac734412e9f66a43edb81ad727c
Nicolas Mattia <nicolas@dfinity.org>
2020-01-28 14:15:57 +0100

    Simplify ./nix directory import
    This clarifies the intent and prevents us from forgetting to forward an argument in the future.

--- default.nix
+++ default.nix
@@ -4,9 +4,6 @@
 , overlays ? []
 , src ? null
 , RustSec-advisory-db ? null
-}: {
-  inherit (import ./nix {
-    inherit system crossSystem config overlays RustSec-advisory-db;
-  }) dfinity-sdk
-    ;
+}@args: {
+  inherit (import ./nix args) dfinity-sdk ;
 }
