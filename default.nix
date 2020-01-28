5050b08904de4463e1de53cca8cb864ba51cf252
Nicolas Mattia <nicolas@dfinity.org>
2020-01-28 15:55:35 +0100

    Move e2e-tests, cargo-audit, nix-fmt and public to top-level

--- default.nix
+++ default.nix
@@ -4,9 +4,26 @@
 , overlays ? []
 , src ? null
 , RustSec-advisory-db ? null
-}: {
-  inherit (import ./nix {
+}:
+let
+  pkgs = import ./nix {
     inherit system crossSystem config overlays RustSec-advisory-db;
-  }) dfinity-sdk
-    ;
+  };
+in
+{
+  inherit (pkgs) dfinity-sdk;
+
+  e2e-tests = import ./e2e { inherit pkgs; };
+
+  # The cargo audit job for known vulnerabilities. This generally run
+  # against the advisory database pinned in sources.json; on Hydra
+  # (master) however the latest advisory database is fetched from
+  # RustSec/advisory-db. This means that whenever a new security
+  # vulnerability is published or when Cargo.lock has been changed `cargo
+  # audit` will run.
+  cargo-audit = import ./cargo-audit.nix { inherit pkgs; };
+
+  inherit (pkgs) nix-fmt nix-fmt-check;
+
+  public = import ./public { inherit pkgs; };
 }
--- nix/overlays/dfinity-sdk.nix
+++ nix/overlays/dfinity-sdk.nix
@@ -3,7 +3,6 @@ let
   mkRelease = super.callPackage ./mk-release.nix {};
   rust-package = import ../../dfx.nix { pkgs = self; };
   rust-workspace = rust-package.build;
-  public = import ../../public { pkgs = self; };
 in
 {
   dfinity-sdk = rec {
@@ -21,21 +20,9 @@ in
             exename = "dfx";
             usePackager = false;
           };
-
-        e2e-tests = import ../../e2e { pkgs = self; };
-
-        # The cargo audit job for known vulnerabilities. This generally run
-        # against the advisory database pinned in sources.json; on Hydra
-        # (master) however the latest advisory database is fetched from
-        # RustSec/advisory-db. This means that whenever a new security
-        # vulnerability is published or when Cargo.lock has been changed `cargo
-        # audit` will run.
-        cargo-security-audit = import ../../cargo-audit.nix { pkgs = self; };
       };
 
     dfx-release = mkRelease "dfx" self.releaseVersion packages.rust-workspace-standalone "dfx";
-
-    inherit (public) install-sh install-sh-lint install-sh-release;
 
     # This is to make sure CI evalutes shell derivations, builds their
     # dependencies and populates the hydra cache with them. We also use this in
@@ -49,7 +36,5 @@ in
     licenses = {
       rust-workspace = super.lib.runtime.runtimeLicensesReport packages.rust-workspace;
     };
-
-    inherit (self) nix-fmt-check;
   };
 }
