50419f1d920cf6969a2b0f706fd92afa80b9f285
Jude Taylor <jude.taylor@dfinity.org>
2020-01-30 09:43:36 -0800

    use single arg for builder

--- e2e/default.nix
+++ e2e/default.nix
@@ -27,21 +27,18 @@ let
     inherit system;
     PATH = pkgs.lib.makeSearchPath "bin" inputs;
     BATSLIB = sources.bats-support;
-    builder = [
-      (
-        pkgs.writeScript "builder.sh" ''
-          #!${pkgs.stdenv.shell}
-          set -eo pipefail
+    builder =
+      pkgs.writeScript "builder.sh" ''
+        #!${pkgs.stdenv.shell}
+        set -eo pipefail
 
-          # We want $HOME/.cache to be in a new temporary directory.
-          export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
+        # We want $HOME/.cache to be in a new temporary directory.
+        export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
 
-          # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
-          # less resources than a dev's computer, so e2e might take longer.
-          timeout --preserve-status 600 bats --recursive ${e2e}/* | tee $out
-        ''
-      )
-    ];
+        # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
+        # less resources than a dev's computer, so e2e might take longer.
+        timeout --preserve-status 600 bats --recursive ${e2e}/* | tee $out
+      '';
   } // { meta = {}; };
 in
 self
