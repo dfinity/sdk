3ef89dd5447d6471ac3ab5300636992577047e5e
Jude Taylor <jude.taylor@dfinity.org>
2020-01-30 08:55:12 -0800

    restore the comment

--- e2e/default.nix
+++ e2e/default.nix
@@ -23,6 +23,8 @@ builtins.derivation {
     # We want $HOME/.cache to be in a new temporary directory.
     export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
 
+    # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
+    # less resources than a dev's computer, so e2e might take longer.
     timeout --preserve-status 600 bats --recursive ${e2e}/* | tee $out
   '') ];
 }
