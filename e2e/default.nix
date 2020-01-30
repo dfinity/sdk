8eda2689251f2b01ae4eddb856a7cc622be1b276
Jude Taylor <jude.taylor@dfinity.org>
2020-01-30 09:10:47 -0800

    nixfmt

--- e2e/default.nix
+++ e2e/default.nix
@@ -7,24 +7,41 @@ let
   sources = pkgs.sources;
 
   inputs = with pkgs; [
-    bats bash coreutils curl findutils gnugrep gnutar gzip netcat ps python3 which
+    bats
+    bash
+    coreutils
+    curl
+    findutils
+    gnugrep
+    gnutar
+    gzip
+    netcat
+    ps
+    python3
+    which
     dfinity-sdk.packages.rust-workspace-standalone
   ];
+
+  self = builtins.derivation {
+    name = "e2e-tests";
+    inherit system;
+    PATH = pkgs.lib.makeSearchPath "bin" inputs;
+    BATSLIB = sources.bats-support;
+    builder = [
+      (
+        pkgs.writeScript "builder.sh" ''
+          #!${pkgs.stdenv.shell}
+          set -eo pipefail
+
+          # We want $HOME/.cache to be in a new temporary directory.
+          export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
+
+          # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
+          # less resources than a dev's computer, so e2e might take longer.
+          timeout --preserve-status 600 bats --recursive ${e2e}/* | tee $out
+        ''
+      )
+    ];
+  } // { meta = {}; };
 in
-builtins.derivation {
-  name = "e2e-tests";
-  inherit system;
-  PATH = pkgs.lib.makeSearchPath "bin" inputs;
-  BATSLIB = sources.bats-support;
-  builder = [ (pkgs.writeScript "builder.sh" ''
-    #!${pkgs.stdenv.shell} 
-    set -eo pipefail
-
-    # We want $HOME/.cache to be in a new temporary directory.
-    export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
-
-    # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
-    # less resources than a dev's computer, so e2e might take longer.
-    timeout --preserve-status 600 bats --recursive ${e2e}/* | tee $out
-  '') ];
-}
+self
