be09e7d8c2632bb8bad8311e1cf0f2321548c65f
Jude Taylor <jude.taylor@dfinity.org>
2020-01-30 09:55:28 -0800

    remove useless self

--- e2e/default.nix
+++ e2e/default.nix
@@ -21,24 +21,23 @@ let
     which
     dfinity-sdk.packages.rust-workspace-standalone
   ];
-
-  self = builtins.derivation {
-    name = "e2e-tests";
-    inherit system;
-    PATH = pkgs.lib.makeSearchPath "bin" inputs;
-    BATSLIB = sources.bats-support;
-    builder =
-      pkgs.writeScript "builder.sh" ''
-        #!${pkgs.stdenv.shell}
-        set -eo pipefail
-
-        # We want $HOME/.cache to be in a new temporary directory.
-        export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
-
-        # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
-        # less resources than a dev's computer, so e2e might take longer.
-        timeout --preserve-status 600 bats --recursive ${e2e}/* | tee $out
-      '';
-  } // { meta = {}; };
 in
-self
+
+builtins.derivation {
+  name = "e2e-tests";
+  inherit system;
+  PATH = pkgs.lib.makeSearchPath "bin" inputs;
+  BATSLIB = sources.bats-support;
+  builder =
+    pkgs.writeScript "builder.sh" ''
+      #!${pkgs.stdenv.shell}
+      set -eo pipefail
+
+      # We want $HOME/.cache to be in a new temporary directory.
+      export HOME=$(mktemp -d -t dfx-e2e-home-XXXX)
+
+      # Timeout of 10 minutes is enough for now. Reminder; CI might be running with
+      # less resources than a dev's computer, so e2e might take longer.
+      timeout --preserve-status 600 bats --recursive ${e2e}/* | tee $out
+    '';
+} // { meta = {}; }
