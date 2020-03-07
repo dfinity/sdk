1768411e2ced99202a4e97253e45f390ca87d723
eftychis <eftychis@users.noreply.github.com>
2020-03-06 17:52:00 -0800

    SDK-745 SDK-757   The reverse stack play of identity integration PR (#427)
    * feat: Integrate identity in agent and dfx
    
    We introduce a signer trait that provides principals and signatures
    for every submit request. Right now signatures are only expected for
    submit requests by the HTTP handler. Furthermore, there is a weird
    limbo regarding the envelope type that needs clarification, as to when
    it is going to be supported.
    
    About Signer:
    For the agent only signatures matter at the end of the day. Thus
    signer provides a sign trait that takes a serializable type and
    provides a request id with a serializable value containing a
    signature. Signer is not parameterized on the type on purpose.
    
    Recall, that currently object safety rules instruct that parametric
    functions can not be members of trait objects. There is a way to
    bypass that rule, by instantiating a container object that implements
    both the resulting trait and realizes a trait monomorphizes on the fly
    the original function.
    
    However, Serialize is also one such trait. To keep this sane and
    simple, and not parameterize the trait itself, we accept and return
    Serialize types in the associated sign() function.
    
    Regardless we end up with a complicated valie validity story though,
    as we need to ensure ownership. Note we pass values with elided
    lifetimes. That is currently not good enough for async traits
    though. To avoid polluting with lifetime parameters, and in search of
    a better path, we keep sign as a non-async function for now.
    
    * fix: debug: Disable Sender field; Not computed correctly by someone
    
    * use agent correctly
    
    * Fix fmt
    
    * Rm identity type for now
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/ic_http_agent/src/agent/signer.rs
@@ -0,0 +1,102 @@
+use crate::agent::agent_error::AgentError;
+use crate::agent::replica_api::SignedMessage;
+use crate::types::request_id::to_request_id;
+use crate::{Blob, RequestId};
+
+/// A Signer amends the request with the [Signature] fields, computing
+/// the request id in the process.
+///
+/// # Warnings / Panics
+///
+/// While the argument type indicates anything serializable, in
+/// reality we can only process only anything that can have a request
+/// id. If an argument is provided with no derivable request id, the
+/// behaviour is undefined and it is left up to the implementation.
+// Note: Turning a trait into async at the moment imposes a static
+// lifetime, which ends up complicating and polluting the remaining
+// code.
+pub trait Signer: Sync {
+    fn sign<'a>(
+        &self,
+        request: Box<(dyn erased_serde::Serialize + Send + Sync + 'a)>,
+    ) -> Result<
+        (
+            RequestId,
+            Box<dyn erased_serde::Serialize + Send + Sync + 'a>,
+        ),
+        AgentError,
+    >;
+}
+
+pub struct DummyIdentity {}
+
+// Right now serialize can not be made into a trait object out of the
+// box because of object safety. This should change in the future. For
+// the same reason equipping the Signer with a generic function ends
+// up in a trait that can not be made into a trait object at compile
+// time that depends on a trait with a similar ailment. This makes
+// things simply complicated. Making the Signer parametric on a
+// Serialize type means we have to pass it along and pushes the issue
+// to dfx or the agent main body of code. Thus, we simply treat the
+// issue here at its root: we pick an erased Serde Serialize trait and
+// return one too. This is compatible with serde Serialize,
+// constructing a holder object and intermediate trait in the
+// process. Doing it this manually here ends up being messy and
+// distracts from the logic. Thus, we use the erased_serde crate.
+impl Signer for DummyIdentity {
+    fn sign<'a>(
+        &self,
+        request: Box<(dyn erased_serde::Serialize + Send + Sync + 'a)>,
+    ) -> Result<
+        (
+            RequestId,
+            Box<dyn erased_serde::Serialize + Send + Sync + 'a>,
+        ),
+        AgentError,
+    > {
+        let mut sender = vec![0; 32];
+        sender.push(0x02);
+        // Bug(eftychis): Note normally the behavior here is to add a
+        // sender field that contributes to the request id. Right now
+        // there seems to be an issue with the behavior of sender in
+        // the request id. Trying to figure out if the correct
+        // behaviour changed and where the deviation happens.
+
+        // let sender = Blob::from(sender);
+        // let request_with_sender = MessageWithSender { request, sender };
+        let request_with_sender = request;
+        let request_id = to_request_id(&request_with_sender).map_err(AgentError::from)?;
+
+        let signature = Blob::from(vec![1; 32]);
+        let sender_pubkey = Blob::from(vec![2; 32]);
+        let signed_request = SignedMessage {
+            request_with_sender,
+            signature,
+            sender_pubkey,
+        };
+        Ok((request_id, Box::new(signed_request)))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+    use serde::Serialize;
+
+    // TODO(eftychis): Provide arbitrary strategies for the replica
+    // API.
+    proptest! {
+    #[test]
+    fn request_id_dummy_signer(request: String) {
+        #[derive(Clone,Serialize)]
+        struct TestAPI { inner : String}
+        let request = TestAPI { inner: request};
+
+        let request_with_sender = request.clone();
+        let actual_request_id = to_request_id(&request_with_sender).expect("Failed to produce request id");
+        let signer = DummyIdentity {};
+        let request_id = signer.sign(Box::new(request)).expect("Failed to sign").0;
+        assert_eq!(request_id, actual_request_id)
+    }}
+}
