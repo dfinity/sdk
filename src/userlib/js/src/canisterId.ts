1c04c763b35ba452173aff366e8bab72cb94f198
Hans Larsen <hans@larsen.online>
2020-03-08 12:25:25 -0700

    fixup! fix: Fix issues with Node in the Userlib

--- src/userlib/js/src/canisterId.ts
+++ src/userlib/js/src/canisterId.ts
@@ -1,8 +1,5 @@
 // Canister IDs are represented as an array of bytes in the HTTP handler of the client.
 export class CanisterId {
-  private static fromHex(hex: string): CanisterId {
-    return new this(hex);
-  }
   public static fromText(hex: string): CanisterId {
     if (hex.startsWith('ic:')) {
       // Remove the checksum from the hexadecimal.
@@ -11,6 +8,10 @@ export class CanisterId {
     } else {
       throw new Error('CanisterId not a "ic:" url: ' + hex);
     }
+  }
+
+  private static fromHex(hex: string): CanisterId {
+    return new this(hex);
   }
 
   protected constructor(private _idHex: string) {}
