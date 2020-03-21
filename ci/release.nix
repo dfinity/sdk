a9561041661ee95412c9e321edae54a8b5c1bec1
Bas van Dijk <v.dijk.bas@gmail.com>
2020-03-21 19:11:27 +0100

    ci: add a CD job for publishing dfx and install.sh to our CDN
    This `publish` job is added to the `ci/release.nix` jobset which has
    also been simplified.

--- ci/release.nix
+++ ci/release.nix
@@ -1,12 +1,27 @@
-{ supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
-, scrubJobs ? true
-, src ? null
-}:
+{ src ? null }:
 let
-  pkgs = import ../nix {};
+  # doRelease is true when the git tag is of the right release format like `0.1.2`.
+  doRelease = src != null && versionMatches != null;
+
+  # versionMatch is `null` if `src.gitTag` is not of the right format like "1.23.456"
+  # and it's a list of matches like [ "1.23.456" ] when it is.
+  versionMatches = builtins.match "([0-9]+\.[0-9]+\.[0-9]+)" src.gitTag;
+  releaseVersion = if versionMatches == null then "latest" else builtins.head versionMatches;
+
+  packages_x86_64-linux = import ../. { system = "x86_64-linux"; inherit src releaseVersion; };
+  packages_x86_64-darwin = import ../. { system = "x86_64-darwin"; inherit src releaseVersion; };
 in
-pkgs.ci ../release.nix
-  {
-    inherit supportedSystems scrubJobs src;
-    rev = if src != null then src.rev else pkgs.lib.commitIdFromGitRepo (pkgs.lib.gitDir ../.);
-  }
+if !doRelease then {} else {
+
+  # TODO: remove these jobs when the `publish.x86_64-linux` job below
+  # is working successfully and the CloudFront CDN is online.
+  dfx-release = {
+    x86_64-linux = packages_x86_64-linux.dfx-release;
+    x86_64-darwin = packages_x86_64-darwin.dfx-release;
+  };
+  install-sh-release = {
+    x86_64-linux = packages_x86_64-linux.install-sh-release;
+  };
+
+  publish.x86_64-linux = packages_x86_64-linux.publish;
+}
