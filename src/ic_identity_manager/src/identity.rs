fb077e85122212bed72249ecd88e1512d5c1eaba
eftychis <eftychis@users.noreply.github.com>
2020-03-31 12:57:40 -0700

    Revert "Store Key files locally in a project"
    This reverts commit 876d3f2ae6fbf9f80f4636224effe9e97fb98b98.

--- src/ic_identity_manager/src/identity.rs
+++ src/ic_identity_manager/src/identity.rs
@@ -1,5 +1,7 @@
-//! Provides identity related operations for the Internet
-//! Computer (IC).
+//! Provides identity management and operations for the Internet
+//! Computer (IC). Namely, we generate, load and revoke credentials
+//! related to principals, provide principal mapping seamlessly with
+//! corresponding key-pairs.
 //!
 //! # Definitions
 //!
@@ -15,6 +17,16 @@
 //! identity, while verification is the procedure whereby that claim
 //! is checked. Authentication is the assertion of an entity’s claim
 //! to an identity.
+//!
+//! A `role` represents the set of actions an entity equipped with that
+//! role can exercise.
+//!
+//! An `identifier` is a sequence of bytes/string utilized as a name for
+//! a principal. That allows a principal to be referenced.
+//!
+//! A `controller` is a principal with an administrative-control role
+//! over a corresponding canister. Each canister has one or more
+//! controllers.
 
 use crate::basic::BasicProvider;
 use crate::crypto_error::Error;
@@ -26,8 +38,13 @@ use std::path::PathBuf;
 /// An identity is a construct that denotes the set of claims of an
 /// entity about itself. Namely it collects principals, under which
 /// the owner of this object can authenticate and provides basic
-/// operations.
+/// operations. Thus, an identity may have access to multiple
+/// principals or credential services, each combination represented by
+/// a provider.
 pub struct Identity {
+    // TODO(eftychis): This changes into a precendence map. Note that
+    // principals are not going to be tied with credentials. We keep
+    // it simple, simply picking the first provider.
     inner: BasicProvider,
 }
 
--- src/ic_identity_manager/src/lib.rs
+++ src/ic_identity_manager/src/lib.rs
@@ -1,3 +1,54 @@
+//! This crate is written with the purpose to provide seamless
+//! authentication of all requests, as the IC Public Spec dictates, guide
+//! any authorization actions and manage principals.  A user should be
+//! able to use an IC agent tool, such as dfx, providing minimum input or
+//! distraction.
+//!
+//! # Goals and Guidelines
+//!
+//! Users should not worry about the signature schemes used,
+//! appropriate keys to be used, or authorizing devices.
+//!
+//! In summary, we want to ensure all requests performed by the agent
+//! using this library provide seamless authrntication of every request
+//! performed.
+//!
+//! We aim to keep the user happy while at the same time, authenticate
+//! properly avoiding temporary measures. Namely, we should sign every
+//! single request out of the box, no turn-off buttons. To that end,
+//! we aim to offer a "works out of the box" experience, meanwhile at
+//! appropriate intervals, as things stabilize, we expose more control
+//! to the user. In the end we make the user happy out of the box,
+//! while we still provide the means to the experienced user to
+//! operate and experiment.
+//!
+//! Furthermore, we want to avoid teaching the user "bad habits". For
+//! example, leaving unencrypted key PEM format files in a git
+//! directory.
+//!
+//! As a result we need to work without requiring the user to provide
+//! us key files in every invocation of dfx or other IC agent.
+//!
+//! We should not incentivize the user to provide their system
+//! credentials to communicate with the IC either. Each principal will
+//! have multiple associated keys that should be revocable and not
+//! associated with any other service. Note that different canister
+//! operations or communicating with different canisters may require
+//! different principals. The user should not be forced to provide a
+//! set of master credential on each invocation of the agent. Finally,
+//! associating a system host with an IC request enables tracking and
+//! makes portability an issue.
+//!
+//! As this is in development and constant improvement and features
+//! are added we generally aim and advise to avoid exposing non-stable
+//! features directly to the user. Internal representations are always
+//! subject to change.
+//!
+//! To that end we do need to take special care for backwards
+//! compatibility. We do not want a user to have issues while running
+//! multiple projects, or migrating a project to a newer version of
+//! the user agent and thus this library.
+//!
 //! # Usage
 //!
 //! We expose a single type [`Identity`], currently providing only
