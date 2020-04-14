f014bb4f59c77474da7f669dff2c7a1536e44966
Hans Larsen <hans@larsen.online>
2020-04-14 12:04:06 -0700

    test: do not exit(0) in teardown in node e2e

--- e2e/node/teardown.js
+++ e2e/node/teardown.js
@@ -1,13 +1,12 @@
-module.exports = function() {
+module.exports = function(config, a, b, c) {
   // Sending SIGINT here since the Replica process isn't actually the replica
   // but `dfx replica`, which should be interupted to clean up properly.
   // (ie. if directly killed it will keep the replica process running in
   // the background).
   global.replicaProcess.kill('SIGTERM');
 
-  // Just process.exit() after a second if nothing else. If we're here,
-  // the tests succeeded anyway.
-  return new Promise(resolve => setTimeout(resolve, 1000)).then(() => {
-    process.exit(0);
-  });
+  // Give the replica a second to gather its things and quit.
+  // We unfortunately cannot exit our own process here because we don't know
+  // the status of the tests (fail/success).
+  return new Promise(resolve => setTimeout(resolve, 1000));
 };
