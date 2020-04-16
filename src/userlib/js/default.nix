c66269cacd4b97c18a8cb5cd269120271d3b0106
Bas van Dijk <v.dijk.bas@gmail.com>
2020-04-16 18:11:12 +0200

    build: add missing dependencies of dfinity-sdk-userlib-js
    The `dfinity-sdk-userlib-js` derivation was reporting the following error:
    
    ```
    gyp ERR! stack Error: Could not find any Python installation to use
    ```
    
    This was fixed by bringing `python3` into scope. After that the
    derivation reported the error:
    
    ```
    make: Entering directory '/private/var/folders/5h/b11m6fxj2tqgbxwz5bgjy42c0000gn/T/nix-build-dfinity-sdk-userlib-js.drv-0/source/node_modules/fsevents/build'
      SOLINK_MODULE(target) Release/.node
      CXX(target) Release/obj.target/fse/fsevents.o
    ../fsevents.cc:10:10: fatal error: 'CoreServices/CoreServices.h' file not found
    ```
    
    Which was fixed by bringing the `CoreServices` library into scope on Darwin.

--- src/userlib/js/default.nix
+++ src/userlib/js/default.nix
@@ -11,6 +11,11 @@ pkgs.napalm.buildPackage src {
 
   outputs = [ "out" "lib" ];
 
+  buildInputs = [
+    pkgs.python3
+  ] ++ pkgs.lib.optional pkgs.stdenv.isDarwin
+         pkgs.darwin.apple_sdk.frameworks.CoreServices;
+
   # ci script now does everything CI should do. Bundle is needed because it's the output
   # of the nix derivation.
   npmCommands = [
