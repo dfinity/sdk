016f278c2342c46cc4019e05127b6d9640441ed0
Bas van Dijk <v.dijk.bas@gmail.com>
2020-04-16 18:30:43 +0200

    refactor: fix nix-fmt errors

--- src/userlib/js/default.nix
+++ src/userlib/js/default.nix
@@ -12,9 +12,11 @@ pkgs.napalm.buildPackage src {
   outputs = [ "out" "lib" ];
 
   buildInputs = [
+    # Required by node-gyp
     pkgs.python3
   ] ++ pkgs.lib.optional pkgs.stdenv.isDarwin
-         pkgs.darwin.apple_sdk.frameworks.CoreServices;
+    # Required by fsevents
+    pkgs.darwin.apple_sdk.frameworks.CoreServices;
 
   # ci script now does everything CI should do. Bundle is needed because it's the output
   # of the nix derivation.
