4f65378adadcac6a6c11132529a6c96c8fec4aeb
Hans Larsen <hans@larsen.online>
2020-05-09 12:16:11 -0700

    test: add e2e tests for query/call principal being correct

---
+++ e2e/bats/identity.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bats
+
+load utils/_
+
+setup() {
+    # We want to work from a temporary directory, different for every test.
+    cd $(mktemp -d -t dfx-e2e-XXXXXXXX)
+    export RUST_BACKTRACE=1
+
+    dfx_new
+}
+
+teardown() {
+    dfx_stop
+}
+
+@test "calls and query receive the same principal from dfx" {
+    install_asset identity_mo
+    assert_command dfx build
+    dfx_start
+    assert_command dfx canister install --all
+
+    ID_CALL=$(dfx canister call e2e_project hashFromCall)
+    ID_QUERY=$(dfx canister call e2e_project hashFromQuery)
+    if [ "$ID_CALL" -ne "$ID_QUERY" ]; then
+      echo "IDs did not match: call '${ID_CALL}' != query '${ID_QUERY}'..." | fail
+    fi
+}
