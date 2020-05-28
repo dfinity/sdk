552a89849fb9ccff5d5f6a85e3e07793e70f66ed
Eric Swanson <eric.swanson@dfinity.org>
2020-05-28 11:50:06 -0700

    assets.nix: use runCommandNoCCLocal

--- assets.nix
+++ assets.nix
@@ -4,32 +4,16 @@
 , agent-js ? import ./src/agent/javascript { inherit pkgs; }
 }:
 let
-  inputs = with pkgs; [
-    bash
-    coreutils
-    gnutar
-    gzip
-  ];
-
 in
-builtins.derivation {
-  name = "assets";
-  system = pkgs.stdenv.system;
-  PATH = pkgs.lib.makeSearchPath "bin" inputs;
-  builder =
-    pkgs.writeScript "builder.sh" ''
-      #!${pkgs.stdenv.shell}
-      set -eo pipefail
+pkgs.runCommandNoCCLocal "assets" {} ''
+  mkdir -p $out
 
-      mkdir -p $out
+  cp -R ${assets-minimal}/* $out
 
-      cp -R ${assets-minimal}/* $out
+  cp ${pkgs.dfinity.ic-replica}/bin/replica $out
+  cp ${pkgs.motoko.mo-ide}/bin/mo-ide $out
 
-      cp ${pkgs.dfinity.ic-replica}/bin/replica $out
-      cp ${pkgs.motoko.mo-ide}/bin/mo-ide $out
-
-      mkdir $out/js-user-library
-      tar xvzf ${agent-js.out}/dfinity-*.tgz --strip-component 1 --directory $out/js-user-library
-      cp -R ${agent-js.lib}/node_modules $out/js-user-library
-    '';
-} // { meta = {}; }
+  mkdir $out/js-user-library
+  tar xvzf ${agent-js.out}/dfinity-*.tgz --strip-component 1 --directory $out/js-user-library
+  cp -R ${agent-js.lib}/node_modules $out/js-user-library
+''
