670f9de56b197b11d7e26c9dd6a1e1b73b5cdbab
Hans <hans.larsen@dfinity.org>
2020-06-05 12:20:41 -0700

    fix: allow lowercase hex in CanisterId (#665)
    The rust agent already allowed it.
    
    Fixes #659

--- src/agent/javascript/src/cbor.test.ts
+++ src/agent/javascript/src/cbor.test.ts
@@ -23,7 +23,7 @@ test('round trip', () => {
     b: 'two',
     c: Buffer.from([3]) as BinaryBlob,
     d: { four: 'four' },
-    e: CanisterId.fromText('ic:FFFFFFFFFFFFFFFFD7'),
+    e: CanisterId.fromText('ic:FfFfFfFfFfFfFfFfd7'),
     f: Buffer.from([]) as BinaryBlob,
     g: new BigNumber('0xffffffffffffffff'),
   };
