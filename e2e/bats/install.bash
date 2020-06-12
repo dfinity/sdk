24fcd7330609b791ae3ce8772db7241c216c03a3
Eric Swanson <eric.swanson@dfinity.org>
2020-06-12 11:17:14 -0700

    Just make install.bash call dfx_stop in its teardown like the other e2e tests.  Since (like the others) it calls dfx_start, there is no longer any point in checking that nothing is running when it exits.

--- e2e/bats/install.bash
+++ e2e/bats/install.bash
@@ -15,14 +15,10 @@ teardown() {
 }
 
 @test "install fails if no argument is provided" {
-    skip "This is known to fail by leaving replica and dfx start processes running"
     [ "$USE_IC_REF" ] && skip "skipped for ic-ref"
 
     dfx_start
     assert_command_fail dfx canister install
     assert_match "required arguments were not provided"
     assert_match "--all"
-
-    # Verify that processes are not running (yet).
-    ! ( ps | grep -e replica -e "[d]fx start" )
 }
