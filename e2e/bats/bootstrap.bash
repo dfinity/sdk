a3a0364778d3ffb165c16c442167f4f76057888b
chenyan-dfinity <yan.chen@dfinity.org>
2020-06-25 20:05:08 -0700

    e2e

---
+++ e2e/bats/bootstrap.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bats
+
+load utils/_
+
+setup() {
+    # We want to work from a temporary directory, different for every test.
+    cd $(mktemp -d -t dfx-e2e-XXXXXXXX)
+    dfx_new
+}
+
+teardown() {
+    dfx_stop
+}
+
+@test "bootstrap fetches candid file" {
+    dfx_start
+    dfx build
+    dfx canister install hello
+    ID=$(dfx canister id hello)
+
+    assert_command curl http://localhost:8000/_/candid?canisterId="$ID"
+    assert_match '"greet": (text) -> (text);'
+}
