2f18ddad2802bb556ea2a8d8b8e8711071ada162
Prithvi Shahi <prithvi@dfinity.org>
2020-06-28 21:17:26 -0700

    fixes

--- src/agent/rust/src/canister/management_canister.rs
+++ src/agent/rust/src/canister/management_canister.rs
@@ -3,24 +3,38 @@ use crate::agent::response::Replied;
 use crate::agent::Agent;
 use crate::{Blob, CanisterAttributes, CanisterId, RequestId};
 use candid::{Decode, Encode};
+use std::str::FromStr;
 
 const MANAGEMENT_CANISTER_ID: &str = "ic:00";
-const CREATE_CMD: &str = "create_canister";
-const INSTALL_CMD: &str = "install_code";
+const CREATE_METHOD_NAME: &str = "create_canister";
+const INSTALL_METHOD_NAME: &str = "install_code";
 
 #[derive(candid::CandidType, candid::Deserialize)]
 struct CreateResult {
     canister_id: candid::Principal,
 }
 
-#[derive(candid::CandidType, candid::Deserialize)]
-enum InstallMode {
+#[derive(Clone, candid::CandidType, candid::Deserialize)]
+pub enum InstallMode {
     #[serde(rename = "install")]
     Install,
     #[serde(rename = "reinstall")]
     Reinstall,
     #[serde(rename = "upgrade")]
     Upgrade,
+}
+
+impl FromStr for InstallMode {
+    type Err = AgentError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "install" => Ok(InstallMode::Install),
+            "reinstall" => Ok(InstallMode::Reinstall),
+            "upgrade" => Ok(InstallMode::Upgrade),
+            &_ => Err(AgentError::InstallModeError(s.to_string())),
+        }
+    }
 }
 
 #[derive(candid::CandidType, candid::Deserialize)]
@@ -32,11 +46,15 @@ struct CanisterInstall {
     compute_allocation: Option<u8>,
 }
 
-pub struct ManagementCanister<'_a> {
-    pub agent: &'_a Agent,
+pub struct ManagementCanister<'agent> {
+    agent: &'agent Agent,
 }
 
-impl<'_a> ManagementCanister<'_a> {
+impl<'agent> ManagementCanister<'agent> {
+    pub fn new(agent: &'agent Agent) -> Self {
+        ManagementCanister { agent }
+    }
+
     pub async fn create_canister<W: delay::Waiter>(
         &self,
         waiter: W,
@@ -45,7 +63,7 @@ impl<'_a> ManagementCanister<'_a> {
             .agent
             .call_raw(
                 &CanisterId::from_text(MANAGEMENT_CANISTER_ID).unwrap(),
-                CREATE_CMD,
+                CREATE_METHOD_NAME,
                 &Blob::empty(),
             )
             .await?;
@@ -65,29 +83,23 @@ impl<'_a> ManagementCanister<'_a> {
     pub async fn install_code(
         &self,
         canister_id: &CanisterId,
-        mode: &str,
+        mode: InstallMode,
         module: &Blob,
         arg: &Blob,
         attributes: &CanisterAttributes,
     ) -> Result<RequestId, AgentError> {
-        let mode = match mode {
-            "install" => InstallMode::Install,
-            "reinstall" => InstallMode::Reinstall,
-            "upgrade" => InstallMode::Upgrade,
-            &_ => InstallMode::Install,
-        };
         let canister_to_install = CanisterInstall {
             mode,
-            canister_id: candid::Principal::from_text(canister_id.clone().to_text())?,
-            wasm_module: module.clone().as_slice().to_vec(),
-            arg: arg.clone().as_slice().to_vec(),
+            canister_id: candid::Principal::from_text(canister_id.to_text())?,
+            wasm_module: module.as_slice().to_vec(),
+            arg: arg.as_slice().to_vec(),
             compute_allocation: attributes.compute_allocation.map(|x| x.into()),
         };
         let bytes: Vec<u8> = candid::Encode!(&canister_to_install).unwrap();
         self.agent
             .call_raw(
                 &CanisterId::from_text(MANAGEMENT_CANISTER_ID).unwrap(),
-                INSTALL_CMD,
+                INSTALL_METHOD_NAME,
                 &Blob::from(bytes),
             )
             .await
--- src/dfx/src/commands/canister/create.rs
+++ src/dfx/src/commands/canister/create.rs
@@ -34,11 +34,10 @@ fn create_canister(env: &dyn Environment, canister_name: &str) -> DfxResult {
         .get_config()
         .ok_or(DfxError::CommandMustBeRunInAProject)?;
 
-    let mgr = ManagementCanister {
-        agent: env
-            .get_agent()
+    let mgr = ManagementCanister::new(
+        env.get_agent()
             .ok_or(DfxError::CommandMustBeRunInAProject)?,
-    };
+    );
     let mut runtime = Runtime::new().expect("Unable to create a runtime");
     let info = CanisterInfo::load(&config, canister_name)?;
 
@@ -46,21 +45,19 @@ fn create_canister(env: &dyn Environment, canister_name: &str) -> DfxResult {
     // check if the canister_manifest.json file exists
 
     if manifest_path.is_file() {
-        {
-            let file = std::fs::File::open(info.get_manifest_path()).unwrap();
-            let mut manifest: CanisterManifest = serde_json::from_reader(file).unwrap();
+        let file = std::fs::File::open(info.get_manifest_path()).unwrap();
+        let mut manifest: CanisterManifest = serde_json::from_reader(file).unwrap();
 
-            match manifest.canisters.get(info.get_name()) {
-                Some(serde_value) => {
-                    let metadata: CanManMetadata =
-                        serde_json::from_value(serde_value.to_owned()).unwrap();
-                    CanisterId::from_text(metadata.canister_id).ok();
-                }
-                None => {
-                    let cid = runtime.block_on(mgr.create_canister(create_waiter()))?;
-                    info.set_canister_id(cid.clone())?;
-                    manifest.add_entry(&info, cid)?;
-                }
+        match manifest.canisters.get(info.get_name()) {
+            Some(serde_value) => {
+                let metadata: CanManMetadata =
+                    serde_json::from_value(serde_value.to_owned()).unwrap();
+                CanisterId::from_text(metadata.canister_id).ok();
+            }
+            None => {
+                let cid = runtime.block_on(mgr.create_canister(create_waiter()))?;
+                info.set_canister_id(cid.clone())?;
+                manifest.add_entry(&info, cid)?;
             }
         }
     } else {
