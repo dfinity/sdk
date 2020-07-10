f257bf6f28978090f5cf82e28b74d309a49d6575
Enzo Haussecker <enzo@sovereign.io>
2020-07-10 00:38:18 -0700

    Override asset storage build product

--- distributed-canisters.nix
+++ distributed-canisters.nix
@@ -20,6 +20,10 @@ let
       cp -R $source_root/* $build_dir
 
       ( cd $build_dir ; DFX_ASSETS=${assets-minimal} ${dfx-minimal}/bin/dfx build --skip-manifest )
+
+      if [ -f override.wasm ]
+      then cp override.wasm canisters/$canister_name/$canister_name.wasm
+      fi
     done
   '';
 
