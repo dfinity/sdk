9f82e4bc254446aa1322329b100a88751f7b7f78
Prithvi Shahi <prithvi@dfinity.org>
2020-07-21 14:04:41 -0700

    replace jssha with js-sha256

--- src/agent/javascript/src/utils/sha224.ts
+++ src/agent/javascript/src/utils/sha224.ts
@@ -1,8 +1,8 @@
-import jsSHA from 'jssha';
+import { sha224 as jsSha224 } from 'js-sha256';
 import { BinaryBlob, blobFromUint8Array } from '../types';
 
 export function sha224(data: ArrayBuffer): BinaryBlob {
-  const shaObj = new jsSHA('SHA-224', 'ARRAYBUFFER');
+  const shaObj = jsSha224.create();
   shaObj.update(data);
-  return blobFromUint8Array(shaObj.getHash('UINT8ARRAY'));
+  return blobFromUint8Array(new Uint8Array(shaObj.array()));
 }
