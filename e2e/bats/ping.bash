6286bf1bc03ed5800cc0adeb342995ce85763cff
Eric Swanson <eric.swanson@dfinity.org>
2020-07-24 17:47:28 -0700

    test: add e2e ping test that does not use the default local network

--- e2e/bats/ping.bash
+++ e2e/bats/ping.bash
@@ -49,3 +49,30 @@ teardown() {
 
     assert_match "\"ic_api_version\""
 }
+
+@test "dfx ping succeeds by arbitrary network name to a nonstandard port" {
+    if [ "$USE_IC_REF" ]; then
+        dfx_start
+        local_bind=$(jq -r .networks.local.bind dfx.json)
+        cat <<<$(jq .networks.arbitrary.providers=[\"http://${local_bind}\"] dfx.json) >dfx.json
+        # without this, `dfx ping` below will succeed:
+        cat <<<$(jq .networks.local.bind=\"127.0.0.1:22113\" dfx.json) >dfx.json
+    else
+        dfx_start --host 127.0.0.1:12345
+        cat <<<$(jq .networks.arbitrary.providers=[\"http://127.0.0.1:12345\"] dfx.json) >dfx.json
+    fi
+
+    assert_command dfx ping arbitrary
+    assert_match "\"ic_api_version\""
+
+    if [ "$USE_IC_REF" ]; then
+        # calling out that this works too (and connects to the bootstrap).
+        # This is the reason for repointing the local.bind address above.
+        assert_command dfx ping http://127.0.0.1:8000
+    fi
+
+    assert_command_fail dfx ping
+    # this port won't match the ephemeral port that the ic ref picked
+    cat <<<$(jq .networks.arbitrary.providers=[\"127.0.0.1:22113\"] dfx.json) >dfx.json
+    assert_command_fail dfx ping arbitrary
+}
