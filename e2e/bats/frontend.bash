93bf4b7d39cca18d55615b5d5289b657db7463ab
Prithvi Shahi <50885601+p-shahi@users.noreply.github.com>
2020-09-01 17:55:27 -0700

    feat: build granularity (#974)
    - `dfx build` now optionally takes a `canister_name` and optionally takes `--all` (`--all` and `canister_name` are mutually exclusive)
    - builds direct and transitive dependencies specified in `dfx.json` i.e. if dfx.json has `"dependencies": ["{canister_B}"]` for a `canister_A`, `dfx build canister_A` builds `canister_A` and `canister_B`
    
    # Breaking Changes
    - removes `dfx build --skip-frontend` as it is no longer required

--- e2e/bats/frontend.bash
+++ e2e/bats/frontend.bash
@@ -16,7 +16,7 @@ teardown() {
 @test "dfx start serves a frontend" {
     dfx_start
     dfx canister create --all
-    dfx build --skip-frontend
+    dfx build e2e_project
 
     sleep 1
     assert_command curl http://localhost:8000 # 8000 = default port.
@@ -31,7 +31,7 @@ teardown() {
     cat <<<$(jq '.networks.local.bind="127.0.0.1:12345"' dfx.json) >dfx.json
 
     dfx canister create --all
-    dfx build --skip-frontend
+    dfx build e2e_project
 
     assert_command curl http://localhost:12345 # 8000 = default port.
     assert_match "<html>"
