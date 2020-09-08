0cce65c4f95e49529fce6260c3d2ecf54b30004f
Lisa Gunn <44206363+lsgunnlsgunn@users.noreply.github.com>
2020-09-08 12:22:46 -0700

    docs: Update some usage text strings (#997)

--- src/dfx/src/lib/message.rs
+++ src/dfx/src/lib/message.rs
@@ -20,17 +20,17 @@ macro_rules! user_message {
 user_message!(
     // dfx bootstrap
     BootstrapCommand => "Starts the bootstrap server.",
-    BootstrapIP => "IP address that the bootstrap server listens on. Defaults to 127.0.0.1.",
-    BootstrapPort => "Port number that the bootstrap server listens on. Defaults to 8081.",
-    BootstrapRoot => "Directory containing static assets served by the bootstrap server. Defaults to $HOME/.cache/dfinity/versions/$DFX_VERSION/js-user-library/dist/bootstrap.",
-    BootstrapTimeout => "Maximum amount of time, in seconds, the bootstrap server will wait for upstream requests to complete. Defaults to 30.",
+    BootstrapIP => "Specifies the IP address that the bootstrap server listens on. Defaults to 127.0.0.1.",
+    BootstrapPort => "Specifies the port number that the bootstrap server listens on. Defaults to 8081.",
+    BootstrapRoot => "Specifies the directory containing static assets served by the bootstrap server. Defaults to $HOME/.cache/dfinity/versions/$DFX_VERSION/js-user-library/dist/bootstrap.",
+    BootstrapTimeout => "Specifies the maximum number of seconds that the bootstrap server will wait for upstream requests to complete. Defaults to 30.",
 
     // dfx cache
     ManageCache => "Manages the dfx version cache.",
-    CacheDelete => "Delete a specific versioned cache of dfx.",
-    CacheUnpack => "Force unpacking the cache from this dfx version.",
-    CacheList => "List installed and used version.",
-    CacheShow => "Show the path of the cache used by this version.",
+    CacheDelete => "Deletes a specific versioned cache of dfx.",
+    CacheUnpack => "Forces unpacking the cache from this dfx version.",
+    CacheList => "Lists installed and used version.",
+    CacheShow => "Shows the path of the cache used by this version.",
 
     // dfx canister id
     IdCanister => "Prints the identifier of a canister.",
@@ -38,7 +38,7 @@ user_message!(
     // dfx canister call
     CallCanister => "Calls a method on a deployed canister.",
     MethodName => "Specifies the method name to call on the canister.",
-    AsyncResult => "Do not wait for the result of the call to be returned by polling the replica. Instead return a response ID.",
+    AsyncResult => "Specifies not to wait for the result of the call to be returned by polling the replica. Instead return a response ID.",
     ArgumentType => "Specifies the data type for the argument when making the call using an argument.",
     OutputType => "Specifies the format for displaying the method's return result.",
     ArgumentValue => "Specifies the argument to pass to the method.",
@@ -50,11 +50,11 @@ user_message!(
     CreateAll => "Creates all canisters configured in dfx.json.",
 
     // dfx canister install
-    InstallCanister => "Installs compiled code as a canister on the replica.",
-    InstallAll => "Install all canisters configured in dfx.json.",
-    InstallCanisterName => "Specifies the canister name. Either this or the --all flag are required.",
+    InstallCanister => "Deploys compiled code as a canister on the Internet Computer.",
+    InstallAll => "Deploys all canisters configured in the project dfx.json files.",
+    InstallCanisterName => "Specifies the canister name to deploy. You must specify either canister name or the --all option.",
     InstallComputeAllocation => "Specifies the canister's compute allocation. This should be a percent in the range [0..100]",
-    InstallMode => "Install, Reinstall, or Upgrade the canister.",
+    InstallMode => "Specifies the type of deployment. You can set the canister deployment modes to install, reinstall, or upgrade.",
 
     // dfx canister mod
     ManageCanister => "Manages canisters deployed on a network replica.",
@@ -76,7 +76,7 @@ user_message!(
 
     // dfx canister stop
     StopCanister => "Stops a canister that is currently running on the Internet Computer network.",
-    StopCanisterName => "Specifies the name of the canister to stop. You must specify either a canister name or the --all flag.",
+    StopCanisterName => "Specifies the name of the canister to stop. You must specify either a canister name or the --all option.",
     StopAll => "Stops all of the canisters configured in the dfx.json file.",
 
     // dfx canister query
@@ -88,11 +88,11 @@ user_message!(
     RequestId => "Specifies the request identifier. The request identifier is an hexadecimal string starting with 0x.",
 
     // dfx build
-    BuildAll => "Builds all canisters configured in dfx.json.",
-    BuildCanisterName => "Specifies the canister name. Either this or the --all flag are required.",
+    BuildAll => "Builds all canisters configured in the dfx.json file.",
+    BuildCanisterName => "Specifies the name of the canister to build. You must specify either a canister name or the --all option.",
     BuildCanister => "Builds all or specific canisters from the code in your project. By default, all canisters are built.",
     BuildCheck => "Build canisters without creating them. This can be used to check that canisters build ok.",
-    CanisterComputeNetwork => "Override the compute network to connect to.  By default uses the local network.",
+    CanisterComputeNetwork => "Override the compute network to connect to. By default, the local network is used.",
 
     // dfx config
     ConfigureOptions => "Configures project options for your currently-selected project.",
@@ -101,43 +101,43 @@ user_message!(
     OptionFormat => "Specifies the format of the output. By default, it uses JSON.",
 
     // dfx identity mod
-    ManageIdentity => "Manages identities used to communicate with the Internet Computer network. Setting an identity enables to test user-based access controls",
+    ManageIdentity => "Manages identities used to communicate with the Internet Computer network. Setting an identity enables you to test user-based access controls.",
 
     // dfx identity new
-    NewIdentity => "Create a new identity.",
+    NewIdentity => "Creates a new identity.",
 
     // dfx identity list
-    ListIdentities => "List identities.",
+    ListIdentities => "Lists existing identities.",
 
     // dfx identity remove
-    RemoveIdentity => "Remove an identity.",
+    RemoveIdentity => "Removes an existing identity.",
 
     // dfx identity rename
-    RenameIdentity => "Rename an identity.",
+    RenameIdentity => "Renames an existing identity.",
 
     // dfx identity use
-    UseIdentity => "Specify the identity to use.",
+    UseIdentity => "Specifies the identity to use.",
 
     // dfx identity whoami
-    ShowIdentity => "Show the name of the current identity.",
+    ShowIdentity => "Shows the name of the current identity.",
 
     // dfx new
     CreateProject => "Creates a new project.",
     ProjectName => "Specifies the name of the project to create.",
     DryRun => "Provides a preview the directories and files to be created without adding them to the file system.",
-    NewFrontend => "Install the frontend code example for the default canister. This defaults to true if Node is installed, or false if it isn't.",
+    NewFrontend => "Installs the frontend code example for the default canister. This defaults to true if Node is installed, or false if it isn't.",
 
     // dfx ping
-    Ping => "Ping an Internet Computer and returns its status.",
+    Ping => "Pings an Internet Computer network and returns its status.",
 
     // dfx replica
-    Replica => "Start a local replica.",
-    ReplicaMessageGasLimit => "Maximum amount of gas a single message can consume.",
-    ReplicaPort => "The port the local replica should listen to.",
-    ReplicaRoundGasLimit => "Maximum amount of gas a single round can consume.",
+    Replica => "Starts a local Internet Computer replica.",
+    ReplicaMessageGasLimit => "Specifies the maximum number of cycles a single message can consume.",
+    ReplicaPort => "Specifies the port the local replica should listen to.",
+    ReplicaRoundGasLimit => "Specifies the maximum number of cycles a single round can consume.",
 
     // dfx start
-    CleanState => "Cleans state of current project.",
+    CleanState => "Cleans the state of the current project.",
     StartNode => "Starts the local replica and a web server for the current project.",
     NodeAddress => "Specifies the host name and port number to bind the frontend to.",
     StartBackground => "Exits the dfx leaving the replica running. Will wait until the replica replies before exiting.",
@@ -149,7 +149,7 @@ user_message!(
     StopNode => "Stops the local network replica.",
     // dfx ide
     StartLanguageService => "Starts the Motoko IDE Language Server. This is meant to be run by editor plugins not the end-user.",
-    ForceTTY => "Forces the language server to start even when run from a terminal",
+    ForceTTY => "Forces the language server to start even when run from a terminal.",
 );
 
 impl fmt::Display for UserMessage {
