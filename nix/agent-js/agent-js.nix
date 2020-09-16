f0aa7d2e9a39e68ac6858d628b38f350c58f1169
Hans <hans.larsen@dfinity.org>
2020-09-15 19:35:44 -0700

    revert: "rm nix/agent-js/agent-js.nix, which is not used by anything (#1008)" (#1026)
    This reverts commit fc3de2b082461338e9fa770c81d31eb6acac714d.

---
+++ nix/agent-js/agent-js.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import ../. { inherit system; }, system ? builtins.currentSystem }:
+pkgs.stdenv.mkDerivation {
+  name = "agent-js-monorepo-package-agent";
+  src = "${pkgs.agent-js-monorepo}";
+  outputs = [
+    "out"
+    "lib"
+  ];
+  buildInputs = [
+    pkgs.nodejs
+    pkgs.agent-js-monorepo
+  ];
+  configurePhase = ''
+    export HOME=$(mktemp -d)
+  '';
+  buildPhase = ''
+    cd packages/agent
+    echo "calling npm pack"
+    npm pack
+    cd -
+  '';
+  installPhase = ''
+    # $out: everything
+    mkdir -p $out
+    cp -R ${pkgs.agent-js-monorepo.agent}/* $out/
+    cp -R packages/agent/dfinity-agent*.tgz $out/
+
+    # $lib/node_modules: node_modules dir that must be resolvable by npm
+    #   for future build steps to work (e.g. at ../../node_modules)
+    mkdir -p $lib
+    agent_node_modules="${pkgs.agent-js-monorepo}/packages/agent/node_modules"
+    if test -d "$agent_node_modules"; then
+      cp -R "$agent_node_modules" $lib;
+    fi
+
+  '';
+}
