220c571916747f69e29ad2e8badba272e344b7d9
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2020-09-21 16:54:08 -0700

    refactor: revert: "when starting webserver, do not create an extra thread" (#1048)
    Reverts dfinity-lab/sdk#1037
    
    This unexplained thread may have a purpose: removing it triggered some sort of condition the result of which was a segmentation fault on program exit, but only in conjunction with an actor wrapper for the web server used by `dfx bootstrap`.
    
    As it turns out, `dfx bootstrap` is already actor-based.  `dfx replica` needs an actor wrapper, because it is managing an external process, but the webserver that `dfx bootstrap` creates is already an actor behind the scenes.

--- src/dfx/src/commands/bootstrap.rs
+++ src/dfx/src/commands/bootstrap.rs
@@ -68,7 +68,9 @@ pub fn exec(env: &dyn Environment, args: &ArgMatches<'_>) -> DfxResult {
 
     let providers = get_providers(&network_descriptor)?;
 
-    let _ = webserver(
+    let (sender, receiver) = crossbeam::unbounded();
+
+    webserver(
         logger.clone(),
         build_output_root,
         network_descriptor,
@@ -78,7 +80,18 @@ pub fn exec(env: &dyn Environment, args: &ArgMatches<'_>) -> DfxResult {
             .map(|uri| Url::from_str(uri).unwrap())
             .collect(),
         &config_bootstrap.root.unwrap(),
-    )?;
+        sender,
+    )?
+    .join()
+    .map_err(|e| {
+        DfxError::RuntimeError(Error::new(
+            ErrorKind::Other,
+            format!("Failed while running frontend proxy thead -- {:?}", e),
+        ))
+    })?;
+
+    // Wait for the webserver to be started.
+    let _ = receiver.recv().expect("Failed to receive server...");
 
     // Tell the user.
     info!(logger, "Webserver started...");
