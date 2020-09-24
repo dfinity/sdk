04200ad6afd9681ba4339375c63d696ae915c924
Hans Larsen <hans@larsen.online>
2020-09-24 16:23:10 -0700

    update ic-agent

--- src/dfx/src/lib/environment.rs
+++ src/dfx/src/lib/environment.rs
@@ -6,7 +6,7 @@ use crate::lib::identity::identity_manager::IdentityManager;
 use crate::lib::network::network_descriptor::NetworkDescriptor;
 use crate::lib::progress_bar::ProgressBar;
 
-use ic_agent::Agent;
+use ic_agent::{Agent, Identity};
 use semver::Version;
 use slog::{Logger, Record};
 use std::collections::BTreeMap;
@@ -15,7 +15,6 @@ use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-use ic_agent::identity::BasicIdentity;
 #[cfg(test)]
 use mockall::automock;
 
@@ -410,20 +409,18 @@ impl ic_agent::PasswordManager for AgentClient {
     }
 }
 
-fn create_agent(
-    logger: Logger,
-    url: &str,
-    identity: BasicIdentity,
-    timeout: Duration,
-) -> Option<Agent> {
+fn create_agent<I>(logger: Logger, url: &str, identity: Box<I>, timeout: Duration) -> Option<Agent>
+where
+    I: Identity + Send + Sync + 'static,
+{
     AgentClient::new(logger, url.to_string())
         .ok()
         .and_then(|executor| {
             Agent::builder()
                 .with_url(url)
-                .with_identity(identity)
+                .with_boxed_identity(identity)
                 .with_password_manager(executor)
-                .expire_after(Some(timeout))
+                .with_ingress_expiry(Some(timeout))
                 .build()
                 .ok()
         })
