c187e35853b3319e4ab2e71c0381764be805202d
Hans <hans.larsen@dfinity.org>
2020-10-02 13:52:59 -0700

    fix: allow any characters after the version number (#1072)
    It seems like sw_vers now shows the whole version instead of major+minor.

--- public/install/200_downloader.sh
+++ public/install/200_downloader.sh
@@ -14,10 +14,10 @@ check_help_for() {
     # fail to find these options to force fallback
     if check_cmd sw_vers; then
         case "$(sw_vers -productVersion)" in
-            10.13) ;; # High Sierra
-            10.14) ;; # Mojave
-            10.15) ;; # Catalina
-            11.*) ;;  # Big Sur
+            10.13*) ;; # High Sierra
+            10.14*) ;; # Mojave
+            10.15*) ;; # Catalina
+            11.*) ;;   # Big Sur
             *)
                 warn "Detected OS X platform older than 10.13 (High Sierra)"
                 _ok="n"
