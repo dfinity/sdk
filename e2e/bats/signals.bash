3d48a48e535f34017eafd4a05958e5c2381ff56b
Eric Swanson <eric.swanson@dfinity.org>
2020-10-05 11:23:48 -0700

    wip e2e tests

--- e2e/bats/signals.bash
+++ e2e/bats/signals.bash
@@ -14,40 +14,41 @@ teardown() {
 }
 
 @test "dfx replica kills the replica upon SIGINT" {
-    [ "$USE_IC_REF" ] && skip "skip for ic-ref"
-
-    dfx replica --port 0 &
-    DFX_PID=$!
-
-    echo "dfx pid is $DFX_PID"
-
-    assert_file_eventually_exists .dfx/config/port.txt 15s
-
-    ps
-
-    kill -SIGINT $DFX_PID
-
-    assert_process_exits $DFX_PID 15s
-
-    assert_no_dfx_start_or_replica_processes
+  dfx_replica_kills_replica SIGINT
 }
 
 @test "dfx replica kills the replica upon SIGTERM" {
+  dfx_replica_kills_replica SIGTERM
+}
+
+dfx_replica_kills_replica() {
+    signal=$1
+
     [ "$USE_IC_REF" ] && skip "skip for ic-ref"
 
     dfx replica --port 0 &
     DFX_PID=$!
 
-    echo "dfx pid is $DFX_PID"
-    ps
-
+    # wait for replica to start
     assert_file_eventually_exists .dfx/config/port.txt 15s
 
-    ps
-
-    kill -SIGTERM $DFX_PID
+    kill -$signal $DFX_PID
 
     assert_process_exits $DFX_PID 15s
-
     assert_no_dfx_start_or_replica_processes
 }
+
+#@test "dfx replica kills the replica upon SIGTERM" {
+#    [ "$USE_IC_REF" ] && skip "skip for ic-ref"
+#
+#    dfx replica --port 0 &
+#    DFX_PID=$!
+#
+#    # wait for replica to start
+#    assert_file_eventually_exists .dfx/config/port.txt 15s
+#
+#    kill -SIGTERM $DFX_PID
+#
+#    assert_process_exits $DFX_PID 15s
+#    assert_no_dfx_start_or_replica_processes
+#}
