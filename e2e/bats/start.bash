538ba6a119e3a5332e139e941dc198ad7e24bb9f
Eric Swanson <eric.swanson@dfinity.org>
2020-10-07 11:03:52 -0700

    try grep with :space:

--- e2e/bats/start.bash
+++ e2e/bats/start.bash
@@ -28,32 +28,31 @@ teardown() {
     echo "xx 0"
     ps
     echo "xx 1"
-    ps | egrep replica
+    ps | grep replica
     echo "xx 2"
-    ps | egrep "[/[:space:]]replica"
+    ps | grep [/[:space:]]replica
     echo "xx 3"
-    ps | egrep "[/\s]replica" | cut -d ' ' -f 1
+    ps | grep [/[:space:]]replica | cut -d ' ' -f 1
     echo "xx 4"
 
     # find the replica that is the child of dfx.  we do not have awk.
-    REPLICA_PID=$(ps | grep "[/\s]replica" | cut -d ' ' -f 1)
+    REPLICA_PID=$(ps | grep [/[:space:]]replica | cut -d ' ' -f 1)
 
     echo "replica pid is $REPLICA_PID"
 
     kill -KILL $REPLICA_PID
     assert_process_exits $REPLICA_PID 15s
 
+    echo "replica exited"
+
     timeout $timeout sh -c \
-      'until ps | grep "[/\s]replica"; do echo waiting for replica to restart; sleep 1; done' \
+      'until ps | grep [/[:space:]]replica; do echo waiting for replica to restart; sleep 1; done' \
       || (echo "replica did not restart" && ps aux && exit 1)
 
-    echo
-    ps | grep "[/\s]replica"
-    echo
+    echo "replica restarted"
+    sleep 10
 
     assert_command dfx canister call hello greet '("Omega")'
     assert_eq '("Hello, Omega!")'
-
-    #assert_match "what"
 }
 
