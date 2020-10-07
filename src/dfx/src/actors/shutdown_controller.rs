0bd5aba471c07e0ac5349111f3fe201619c40161
Eric Swanson <eric.swanson@dfinity.org>
2020-10-07 16:34:43 -0700

    comment out automatic stop

--- src/dfx/src/actors/shutdown_controller.rs
+++ src/dfx/src/actors/shutdown_controller.rs
@@ -2,7 +2,7 @@ use crate::actors::shutdown_controller::signals::outbound::Shutdown;
 use crate::actors::shutdown_controller::signals::ShutdownTrigger;
 use actix::{Actor, Addr, AsyncContext, Context, Handler, Recipient};
 use slog::Logger;
-use std::thread;
+//use std::thread;
 use std::time::Duration;
 
 pub mod signals {
@@ -87,12 +87,12 @@ impl ShutdownController {
         .expect("Error setting Ctrl-C handler");
     }
 
-    fn shutdown_soon(&self, shutdown_controller: Addr<ShutdownController>) {
-        let _t = thread::spawn(move || {
-            std::thread::sleep(Duration::from_secs(10));
-            shutdown_controller.do_send(ShutdownTrigger());
-        });
-    }
+    // fn shutdown_soon(&self, shutdown_controller: Addr<ShutdownController>) {
+    //     let _t = thread::spawn(move || {
+    //         std::thread::sleep(Duration::from_secs(10));
+    //         shutdown_controller.do_send(ShutdownTrigger());
+    //     });
+    // }
 }
 
 impl Actor for ShutdownController {
@@ -100,7 +100,7 @@ impl Actor for ShutdownController {
 
     fn started(&mut self, ctx: &mut Self::Context) {
         self.install_ctrlc_handler(ctx.address());
-        self.shutdown_soon(ctx.address());
+        //self.shutdown_soon(ctx.address());
     }
 }
 
