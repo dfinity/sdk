c9efc920616ad5c92399c6e8f281e07c37a916fc
Hans Larsen <hans@larsen.online>
2020-10-14 16:40:48 -0700

    .

--- src/dfx/src/commands/identity/get_wallet.rs
+++ src/dfx/src/commands/identity/get_wallet.rs
@@ -2,7 +2,7 @@ use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 use crate::lib::identity::identity_manager::IdentityManager;
 use crate::lib::message::UserMessage;
-use crate::lib::provider::get_network_descriptor;
+use crate::lib::provider::{create_agent_environment, get_network_descriptor};
 use clap::{App, Arg, ArgMatches, SubCommand};
 use tokio::runtime::Runtime;
 
@@ -18,14 +18,17 @@ pub fn construct() -> App<'static, 'static> {
 }
 
 pub fn exec(env: &dyn Environment, args: &ArgMatches<'_>) -> DfxResult {
-    let identity = IdentityManager::new(env)?.instantiate_selected_identity()?;
-    let network = get_network_descriptor(env, args)?;
+    let agent_env = create_agent_environment(env, args)?;
+    let identity = IdentityManager::new(&agent_env)?.instantiate_selected_identity()?;
+    let network = get_network_descriptor(&agent_env, args)?;
 
     let mut runtime = Runtime::new().expect("Unable to create a runtime");
     runtime.block_on(async {
         println!(
             "{}",
-            identity.get_or_create_wallet(env, &network, true).await?
+            identity
+                .get_or_create_wallet(&agent_env, &network, true)
+                .await?
         );
         DfxResult::Ok(())
     })?;
