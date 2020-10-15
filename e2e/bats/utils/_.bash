bcf2d6b6f704c668fd630c3c9f7c343d33f865d2
Hans Larsen <hans@larsen.online>
2020-10-15 14:52:14 -0700

    .

--- e2e/bats/utils/_.bash
+++ e2e/bats/utils/_.bash
@@ -108,5 +108,5 @@ dfx_stop() {
 dfx_set_wallet() {
   dfx canister create --all
   export WALLET_CANISTER_ID=$(dfx identity get-wallet)
-  dfx identity set-wallet --network ic ${WALLET_CANISTER_ID}
+  dfx identity set-wallet --network ic ${WALLET_CANISTER_ID} --force
 }
