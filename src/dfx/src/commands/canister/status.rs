49b9710885541f0f25052f654c2144002cafe956
Prithvi Shahi <prithvi@dfinity.org>
2020-11-01 19:53:02 -0800

    fix canister status

--- src/dfx/src/commands/canister/status.rs
+++ src/dfx/src/commands/canister/status.rs
@@ -19,7 +19,7 @@ pub struct CanisterStatusOpts {
     canister_name: Option<String>,
 
     /// Returns status information for all of the canisters configured in the dfx.json file.
-    #[clap(long, required_unless_present("canister_name"))]
+    #[clap(long, required_unless_present("canister-name"))]
     all: bool,
 }
 
