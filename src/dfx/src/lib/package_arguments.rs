c61d85bf7f268d7f93ac3ed88ac50a47adcf9010
Enzo Haussecker <enzo@sovereign.io>
2020-11-17 22:01:06 -0800

    Revise error message

--- src/dfx/src/lib/package_arguments.rs
+++ src/dfx/src/lib/package_arguments.rs
@@ -35,7 +35,7 @@ pub fn load(cache: &dyn Cache, packtool: &Option<String>) -> DfxResult<PackageAr
     let output = cmd.output()?;
     if !output.status.success() {
         return Err(DfxError::new(BuildError::CommandError(
-            format!("Failed to invoke the package tool: {:?}", cmd),
+            format!("{:?}", cmd),
             output.status,
             String::from_utf8_lossy(&output.stdout).to_string(),
             String::from_utf8_lossy(&output.stderr).to_string(),
