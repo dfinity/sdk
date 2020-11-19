25340acb2e70f2f437df8fbf9d5c7909d5ed7720
Prithvi Shahi <prithvi@dfinity.org>
2020-11-19 11:00:36 -0800

    look for DFX_CONFIG_ROOT

--- src/dfx/src/lib/config.rs
+++ src/dfx/src/lib/config.rs
@@ -2,10 +2,12 @@ use crate::lib::error::{ConfigErrorKind, DfxError, DfxResult};
 use std::path::PathBuf;
 
 pub fn get_config_dfx_dir_path() -> DfxResult<PathBuf> {
+    let config_root = std::env::var("DFX_CONFIG_ROOT").ok();
     let home = std::env::var("HOME")
         .map_err(|_| DfxError::ConfigError(ConfigErrorKind::CannotFindUserHomeDirectory()))?;
+    let root = config_root.unwrap_or(home);
 
-    let p = PathBuf::from(home).join(".config").join("dfx");
+    let p = PathBuf::from(root).join(".config").join("dfx");
 
     if !p.exists() {
         std::fs::create_dir_all(&p).map_err(|e| {
