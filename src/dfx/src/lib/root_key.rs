cebdabf86e279c708a0bdbf60317404e12933e70
Eric Swanson <eric.swanson@dfinity.org>
2020-11-20 14:11:06 -0800

    change var name to improve readability

--- src/dfx/src/lib/root_key.rs
+++ src/dfx/src/lib/root_key.rs
@@ -4,12 +4,12 @@ use crate::lib::error::DfxResult;
 use anyhow::anyhow;
 
 pub async fn fetch_root_key_if_needed<'a>(env: &'a (dyn Environment + 'a)) -> DfxResult {
-    let must_fetch_root_key = env
+    let non_ic_network = env
         .get_network_descriptor()
         .map(|nd| nd.name != "ic")
         .unwrap_or(true);
 
-    if must_fetch_root_key {
+    if non_ic_network {
         let agent = env
             .get_agent()
             .ok_or_else(|| anyhow!("Cannot get HTTP client from environment."))?;
