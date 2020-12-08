e42dee7c9277ce9e6cb1d48ecfb2104fe8c6d1c1
Hans Larsen <hans@larsen.online>
2020-12-08 10:24:45 -0800

    .

--- e2e/bats/utils/assertions.bash
+++ e2e/bats/utils/assertions.bash
@@ -149,7 +149,7 @@ assert_process_exits() {
 # Asserts that `dfx start` and `replica` are no longer running
 assert_no_dfx_start_or_replica_processes() {
     ! ( ps | grep "[/[:space:]]dfx start" )
-    ! ( ps | grep "$(cat .dfx/replica-configuration/replica-pid)" )
+    .dfx/replica-configuration/replica-pid && (! ( ps | grep "$(cat .dfx/replica-configuration/replica-pid)" ))
 }
 
 assert_file_eventually_exists() {
