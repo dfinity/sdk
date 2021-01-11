061c35887aa3cadceef4a4077b0f487400fbf65c
Enzo Haussecker <enzo@sovereign.io>
2021-01-12 00:34:45 +0100

    Add end-to-end test

---
+++ e2e/bats/telemetry.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bats
+
+load utils/_
+
+setup() {
+    cd $(mktemp -d -t dfx-e2e-XXXXXXXX)
+}
+
+teardown() {
+    :
+}
+
+@test 'dfx renders telemetry consent message' {
+    export DFX_CONFIG_ROOT=$(pwd)
+    test ! -f telemetry/witness.blank
+    assert_command dfx identity whoami
+    test ! -f telemetry/witness.blank
+    assert_match 'SDK sends anonymous usage data'
+    assert_command dfx identity whoami
+    assert_eq 'default'
+}
