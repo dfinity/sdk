1357ebc4a3b4df3a3ad4f50645620972de909cf1
Eric Swanson <eric.swanson@dfinity.org>
2021-01-12 11:41:39 -0800

    Remove debug print stuff, simplify, update comment

--- e2e/bats/certificate.bash
+++ e2e/bats/certificate.bash
@@ -12,46 +12,36 @@ setup() {
     install_asset certificate
     dfx_start
 
-    echo "dfx deploy..." >>${BATS_TEST_DIRNAME}/xx.log
-
     dfx deploy
 
     BACKEND=$(jq -r .networks.local.bind dfx.json)
 
-    echo "trying to start mitmproxy..." >>${BATS_TEST_DIRNAME}/xx.log
+    # Sometimes, something goes wrong with mitmdump's initialization.
+    # It reports that it is listening, and the `nc` call succeeds,
+    # but it does not actually respond to requests.
+    #
+    # This happens whether using a fixed port or a dynamic port.
+    # For this reason, we retry initialization if `dfx ping` fails.
+    #
+    # I have seen this process take as many as 9 iterations to succeed,
+    # so across a large number of CI runs, it could take even more.
+    # The overall CI timeout will control the maximum time taken.
 
-    for i in $(seq 1 1000);
+    while true
     do
-        echo "attempt $i starts" >>${BATS_TEST_DIRNAME}/xx.log
-
         MITM_PORT=$(python3 ${BATS_TEST_DIRNAME}/utils/get_ephemeral_port.py)
         cat <<<$(jq .networks.local.bind=\"127.0.0.1:$MITM_PORT\" dfx.json) >dfx.json
-        #sleep 5
-        pwd >${BATS_TEST_DIRNAME}/$MITM_PORT.pwd
 
         mitmdump -p $MITM_PORT -w ${BATS_TEST_DIRNAME}/$MITM_PORT.out --mode reverse:http://$BACKEND  --replace '/~s/Hello,/Hullo,' >${BATS_TEST_DIRNAME}/$MITM_PORT.txt 2>&1 &
         MITMDUMP_PID=$!
 
-        #sleep 5
         timeout 5 sh -c \
             "until nc -z localhost $MITM_PORT; do echo waiting for mitmdump; sleep 1; done" \
             || (echo "mitmdump did not start on port $MITM_PORT" && exit 1)
 
-        echo "  - mitmdump pid $MITMDUMP_PID listening on port $MITM_PORT" >>${BATS_TEST_DIRNAME}/xx.log
-
-        # Sometimes, something goes wrong with mitmdump's initialization.  It reports that it is listening,
-        # and the `nc` call above succeeds, but it does not actually respond to requests.
-        # This happens whether using a fixed port or a dynamic port.
-        # For this reason, we retry initialization if `dfx ping` fails.
-        for p in $(seq 1 2);
-        do
-            echo "  - dfx ping ($p)..." >>${BATS_TEST_DIRNAME}/xx.log
-            if timeout 10 dfx ping; then
-                echo "    - succeeded on attempt $i (ping: $p)" >>${BATS_TEST_DIRNAME}/xx.log
-                break 2
-            fi
-        done
-        echo "    - failed" >>${BATS_TEST_DIRNAME}/xx.log
+        if timeout 10 dfx ping; then
+            break
+        fi
 
         kill -9 $MITMDUMP_PID
     done
@@ -64,11 +54,11 @@ teardown() {
 }
 
 @test "mitm attack - update: attack fails because certificate verification fails" {
-    assert_command_fail dfx canister call certificate hello_update '("Banzai")'
+    assert_command_fail dfx canister call certificate hello_update '("Buckaroo")'
     assert_match 'Certificate verification failed.'
 }
 
 @test "mitm attack - query: attack succeeds because there is no certificate to verify" {
-    assert_command dfx canister call certificate hello_query '("Banzai")'
-    assert_eq '("Hullo, Banzai!")'
+    assert_command dfx canister call certificate hello_query '("Buckaroo")'
+    assert_eq '("Hullo, Buckaroo!")'
 }
