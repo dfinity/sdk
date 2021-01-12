246370cd0cda804c4145af44c6e6e3fc6896d00b
Enzo Haussecker <enzo@sovereign.io>
2021-01-12 22:58:54 +0100

    Throw errors via anyhow

--- src/dfx/src/lib/telemetry/mod.rs
+++ src/dfx/src/lib/telemetry/mod.rs
@@ -1,37 +1,18 @@
 use crate::lib::config::get_config_dfx_dir_path;
 use crate::lib::error::{DfxError, DfxResult};
 
+use anyhow::bail;
 use std::fs::File;
 use std::path::PathBuf;
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum TelemetryError {
-    #[error("Cannot create telemetry config directory at '{0}'.")]
-    CannotCreateTelemetryDirectory(PathBuf),
-
-    #[error("Cannot find telemetry config  directory at '{0}'.")]
-    CannotFindTelemetryDirectory(PathBuf),
-
-    #[error("Cannot create telemetry consent witness file at '{0}'.")]
-    CannotCreateTelemetryWitnessFile(PathBuf),
-
-    #[error("Cannot find telemetry consent witness file at '{0}'.")]
-    CannotFindTelemetryWitnessFile(PathBuf),
-}
 
 pub fn get_telemetry_config_root() -> DfxResult<PathBuf> {
     let root = get_config_dfx_dir_path()?.join("telemetry");
     if !root.exists() {
         if std::fs::create_dir_all(&root).is_err() {
-            return Err(DfxError::new(
-                TelemetryError::CannotCreateTelemetryDirectory(root),
-            ));
+            bail!("Cannot create telemetry config directory at '{}'.", root);
         }
     } else if !root.is_dir() {
-        return Err(DfxError::new(TelemetryError::CannotFindTelemetryDirectory(
-            root,
-        )));
+        bail!("Cannot find telemetry config  directory at '{}'.", root);
     }
     Ok(root)
 }
@@ -40,15 +21,11 @@ pub fn witness_telemetry_consent() -> DfxResult<()> {
     let file = get_telemetry_config_root()?.join("witness.blank");
     if !file.exists() {
         if File::create(&file).is_err() {
-            return Err(DfxError::new(
-                TelemetryError::CannotCreateTelemetryWitnessFile(file),
-            ));
+            bail!("Cannot create telemetry consent witness file at '{}'.", file);
         }
         println!("\nThe DFINITY Canister SDK sends anonymous usage data to DFINITY Stiftung by\ndefault. If you wish to disable this behavior, then please set the environment\nvariable DFX_TELEMETRY_DISABLED=1. Learn more at https://sdk.dfinity.org.\n");
     } else if !file.is_file() {
-        return Err(DfxError::new(
-            TelemetryError::CannotFindTelemetryWitnessFile(file),
-        ));
+        bail!("Cannot find telemetry consent witness file at '{}'.", file);
     }
     Ok(())
 }
