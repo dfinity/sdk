3bfe42a830354c4cd2525f9ec7fb33d4dc9213f0
Joachim Breitner <mail@joachim-breitner.de>
2021-01-20 10:45:43 +0100

    Fix shell.nix, make nix-fmt available

--- shell.nix
+++ shell.nix
@@ -5,5 +5,5 @@ in
 pkgs.mkCompositeShell {
   name = "dfinity-sdk-env";
   inputsFrom = pkgs.stdenv.lib.attrValues packages.shells;
-  buildInputs = [ pkgs.nix-fmt ];
+  nativeBuildInputs = [ pkgs.nix-fmt ];
 }
