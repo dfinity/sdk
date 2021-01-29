a5d816a7478a174e03e8b7739176ae9b642f11c3
Prithvi Shahi <prithvi@dfinity.org>
2021-01-28 17:27:47 -0800

    add beef to ignore

--- cargo-audit.nix
+++ cargo-audit.nix
@@ -12,5 +12,21 @@ pkgs.lib.cargo-security-audit {
   name = "dfinity-sdk";
   cargoLock = ./Cargo.lock;
   db = RustSec-advisory-db;
-  ignores = [];
+	# Ignore this vulnerability until candid updates to a logos with the beef patch
+	# ID:       RUSTSEC-2020-0122
+	# Crate:    beef
+	# Version:  0.4.4
+	# Date:     2020-10-28
+	# URL:      https://rustsec.org/advisories/RUSTSEC-2020-0122
+	# Title:    beef::Cow lacks a Sync bound on its Send trait allowing for data races
+	# Solution:  upgrade to >= 0.5.0
+	# Dependency tree:
+	# beef 0.4.4
+	# └── logos-derive 0.11.5
+	#     └── logos 0.11.4
+	#         └── candid 0.6.13
+	#             ├── ic-utils 0.1.0
+	#             │   └── dfx 0.6.21
+	#             └── dfx 0.6.21
+  ignores = [ "RUSTSEC-2020-0122" ];
 }
