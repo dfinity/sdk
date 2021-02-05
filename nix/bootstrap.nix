0fa5b8796ad5b55f18363fa6ffceb027cafca6a0
Hans Larsen <hans@larsen.online>
2021-02-05 13:29:18 -0800

    .

--- nix/bootstrap.nix
+++ nix/bootstrap.nix
@@ -3,7 +3,7 @@ let dist = pkgs.fetchurl {
   url = "https://registry.npmjs.org/@dfinity/bootstrap/-/bootstrap-0.0.0.tgz";
   sha256 = "156f669sabrfsy04ap166wx6nmkw38b0njfmfhx17g6q1zr45072";
 }; in
-pkgs.runCommand "assets-bootstrap" {} ''
+pkgs.runCommandNoCC "assets-bootstrap" {} ''
   tar xvf ${dist}
   mkdir -p $out
   mv package/dist/* $out
