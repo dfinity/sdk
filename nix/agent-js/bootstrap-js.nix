dd892bec66ecb1050421e64ebad222713f796184
Hans Larsen <hans@larsen.online>
2021-02-04 18:14:37 -0800

    .

--- nix/agent-js/bootstrap-js.nix
+++ nix/agent-js/bootstrap-js.nix
@@ -1,14 +1,18 @@
 { pkgs ? import ../. { inherit system; }, system ? builtins.currentSystem }:
 pkgs.runCommandNoCC "assets-bootstrap" {
+  version = "0.0.0";
   nativeBuildInputs = [
     pkgs.nodejs
   ];
 } ''
+    # npm looks for config and cache files in the HOME folder, but tries to create HOME
+    # if it doesn't exist. Make sure we have a HOME.
+    export HOME=$(mktemp -d)
 
     mkdir -p $out
     cd $out
 
-    npm pack @dfinity/bootstrap@0.0.0
+    npm pack @dfinity/bootstrap@${version}
     tar xzvf dfinity-bootstrap-*.tgz package/dist/
     rm dfinity-bootstrap-*.tgz
     mv package/dist/* .
