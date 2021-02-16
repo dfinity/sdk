da84b6d3b297cead836c8236fe07a1aaa958fa15
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-02-15 16:56:35 -0800

    remove create flag and restrict wallet creation if one already exists for selected identity

--- src/dfx/src/commands/identity/deploy_wallet.rs
+++ src/dfx/src/commands/identity/deploy_wallet.rs
@@ -14,10 +14,6 @@ use tokio::runtime::Runtime;
 pub struct DeployWalletOpts {
     /// The ID of the canister where the wallet WASM will be deployed.
     canister_id: Option<String>,
-
-    /// Create and deploys a new wallet canister.
-    #[clap(long, conflicts_with("canister-id"))]
-    create: bool,
 }
 
 pub fn exec(env: &dyn Environment, opts: DeployWalletOpts, network: Option<String>) -> DfxResult {
@@ -32,30 +28,23 @@ pub fn exec(env: &dyn Environment, opts: DeployWalletOpts, network: Option<Strin
         .to_string();
     let network = get_network_descriptor(&agent_env, network)?;
 
-    if opts.create {
-        runtime.block_on(async {
-            Identity::create_wallet(&agent_env, &network, &identity_name, None).await?;
-            DfxResult::Ok(())
-        })?;
-    } else {
-        let canister_id = opts
-            .canister_id
-            .ok_or_else(|| anyhow!("Canister id not provided."))?;
-        match CanisterId::from_text(&canister_id) {
-            Ok(id) => {
-                runtime.block_on(async {
-                    Identity::create_wallet(&agent_env, &network, &identity_name, Some(id)).await?;
-                    DfxResult::Ok(())
-                })?;
-            }
-            Err(err) => {
-                anyhow!(format!(
-                    "Cannot convert {} to a valid canister id. Candid error: {}",
-                    canister_id, err
-                ));
-            }
-        };
-    }
+    let canister_id = opts
+        .canister_id
+        .ok_or_else(|| anyhow!("Canister id not provided."))?;
+    match CanisterId::from_text(&canister_id) {
+        Ok(id) => {
+            runtime.block_on(async {
+                Identity::create_wallet(&agent_env, &network, &identity_name, Some(id)).await?;
+                DfxResult::Ok(())
+            })?;
+        }
+        Err(err) => {
+            anyhow!(format!(
+                "Cannot convert {} to a valid canister id. Candid error: {}",
+                canister_id, err
+            ));
+        }
+    };
 
     Ok(())
 }
