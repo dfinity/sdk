4424adfaba508802cf676160adf2e3c3aa378239
Hans <hans@larsen.online>
2021-02-17 10:36:37 -0800

    test: update e2e test instructions (#1377)
    And fix whatever was needed to make it work.

--- e2e/tests-dfx/certificate.bash
+++ e2e/tests-dfx/certificate.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bats
 
-load ./utils/_
+load ../utils/_
 
 setup() {
     # We want to work from a temporary directory, different for every test.
@@ -29,7 +29,7 @@ setup() {
 
     while true
     do
-        MITM_PORT=$(python3 "${BATS_TEST_DIRNAME}/utils/get_ephemeral_port.py")
+        MITM_PORT=$(python3 "${BATS_TEST_DIRNAME}/../utils/get_ephemeral_port.py")
         # shellcheck disable=SC2094
         cat <<<"$(jq '.networks.local.bind="127.0.0.1:'"$MITM_PORT"'"' dfx.json)" >dfx.json
 
