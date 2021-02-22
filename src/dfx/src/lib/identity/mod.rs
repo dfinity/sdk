d6fe66afd3717fc60c72199efdae845665ad3546
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-02-21 23:23:42 -0800

    do not use provisional commands in sodium

--- src/dfx/src/lib/identity/mod.rs
+++ src/dfx/src/lib/identity/mod.rs
@@ -316,10 +316,18 @@ impl Identity {
                 let canister_id = match some_canister_id {
                     Some(id) => id,
                     None => {
-                        mgr.provisional_create_canister_with_cycles(None)
-                            .call_and_wait(waiter_with_timeout(expiry_duration()))
-                            .await?
-                            .0
+                        if network.is_ic {
+                            // Provisional commands are whitelisted on production
+                            mgr.create_canister()
+                                .call_and_wait(waiter_with_timeout(expiry_duration()))
+                                .await?
+                                .0
+                        } else {
+                            mgr.provisional_create_canister_with_cycles(None)
+                                .call_and_wait(waiter_with_timeout(expiry_duration()))
+                                .await?
+                                .0
+                        }
                     }
                 };
 
--- src/dfx/src/lib/operations/canister/create_canister.rs
+++ src/dfx/src/lib/operations/canister/create_canister.rs
@@ -5,7 +5,9 @@ use crate::lib::models::canister_id_store::CanisterIdStore;
 use crate::lib::provider::get_network_context;
 use crate::lib::waiter::waiter_with_timeout;
 
+use anyhow::anyhow;
 use ic_utils::call::AsyncCall;
+use ic_utils::interfaces::ManagementCanister;
 use slog::info;
 use std::format;
 use std::time::Duration;
@@ -54,11 +56,25 @@ pub async fn create_canister(
             info!(log, "Creating the canister using the wallet canister...");
             let wallet =
                 Identity::get_or_create_wallet_canister(env, network, &identity_name, true).await?;
-            let (create_result,) = wallet
-                .wallet_create_canister(1000000000001_u64, None)
-                .call_and_wait(waiter_with_timeout(timeout))
-                .await?;
-            let cid = create_result.canister_id;
+            let cid = if network.is_ic {
+                // Provisional commands are whitelisted on production
+                let mgr = ManagementCanister::create(
+                    env.get_agent()
+                        .ok_or_else(|| anyhow!("Cannot get HTTP client from environment."))?,
+                );
+                let (create_result,): (ic_utils::interfaces::wallet::CreateResult,) = wallet
+                    .call_forward(mgr.update_("create_canister").build(), 0)?
+                    .call_and_wait(waiter_with_timeout(timeout))
+                    .await?;
+                create_result.canister_id
+            } else {
+                wallet
+                    .wallet_create_canister(1000000000001_u64, None)
+                    .call_and_wait(waiter_with_timeout(timeout))
+                    .await?
+                    .0
+                    .canister_id
+            };
 
             let canister_id = cid.to_text();
             info!(
