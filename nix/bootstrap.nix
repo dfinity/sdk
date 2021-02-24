2321f8266a8d72dc83e7397813bb57f2204eece7
Benjamin Goering <ben@bengo.co>
2021-02-23 16:30:11 -0800

    nix-fmt nix/bootstrap.nix

--- nix/bootstrap.nix
+++ nix/bootstrap.nix
@@ -1,8 +1,10 @@
 { pkgs ? import ./. { inherit system; }, system ? builtins.currentSystem }:
-let dist = pkgs.fetchurl {
-  url = "https://registry.npmjs.org/@dfinity/bootstrap/-/bootstrap-0.0.0.tgz";
-  sha256 = "156f669sabrfsy04ap166wx6nmkw38b0njfmfhx17g6q1zr45072";
-}; in
+let
+  dist = pkgs.fetchurl {
+    url = "https://registry.npmjs.org/@dfinity/bootstrap/-/bootstrap-0.0.0.tgz";
+    sha256 = "156f669sabrfsy04ap166wx6nmkw38b0njfmfhx17g6q1zr45072";
+  };
+in
 pkgs.runCommandNoCC "assets-bootstrap" {} ''
   tar xvf ${dist}
   mkdir -p $out
