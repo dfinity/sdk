e9289aeb9bca6ff63c3bdaecfdd4bfb7a5d1dd54
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-02-25 23:41:07 -0800

    .

--- e2e/tests-dfx/wallet.bash
+++ e2e/tests-dfx/wallet.bash
@@ -20,31 +20,31 @@ teardown() {
     rm -rf "$x"
 }
 
-# @test "deploy wallet" {
-#     dfx_start
-#     setup_actuallylocal_network
+@test "deploy wallet" {
+    dfx_start
+    setup_actuallylocal_network
 
-#     # get Canister IDs to install the wasm onto
-#     dfx canister --network actuallylocal create dummy_canister1
-#     ID=$(dfx canister --network actuallylocal id dummy_canister1)
-#     dfx canister --network actuallylocal create dummy_canister2
-#     ID_TWO=$(dfx canister --network actuallylocal id dummy_canister2)
+    # get Canister IDs to install the wasm onto
+    dfx canister --network actuallylocal create dummy_canister1
+    ID=$(dfx canister --network actuallylocal id dummy_canister1)
+    dfx canister --network actuallylocal create dummy_canister2
+    ID_TWO=$(dfx canister --network actuallylocal id dummy_canister2)
 
-#     # set controller to user
-#     dfx canister --network actuallylocal set-controller dummy_canister1 "$(dfx identity get-principal)"
-#     dfx canister --network actuallylocal set-controller dummy_canister2 "$(dfx identity get-principal)"
+    # set controller to user
+    dfx canister --network actuallylocal set-controller dummy_canister1 "$(dfx identity get-principal)"
+    dfx canister --network actuallylocal set-controller dummy_canister2 "$(dfx identity get-principal)"
 
-#     # We're testing on a local network so the create command actually creates a wallet
-#     # Delete this file to force associate wallet created by deploy-wallet to identity
-#     rm "$HOME"/.config/dfx/identity/default/wallets.json
+    # We're testing on a local network so the create command actually creates a wallet
+    # Delete this file to force associate wallet created by deploy-wallet to identity
+    rm "$HOME"/.config/dfx/identity/default/wallets.json
 
-#     assert_command dfx identity --network actuallylocal deploy-wallet "${ID}"
-#     GET_WALLET_RES=$(dfx identity --network actuallylocal get-wallet)
-#     assert_eq "$ID" "$GET_WALLET_RES"
+    assert_command dfx identity --network actuallylocal deploy-wallet "${ID}"
+    GET_WALLET_RES=$(dfx identity --network actuallylocal get-wallet)
+    assert_eq "$ID" "$GET_WALLET_RES"
 
-#     assert_command dfx identity --network actuallylocal deploy-wallet "${ID_TWO}"
-    # assert_match "The wallet canister \"${ID}\"\ already exists for user \"default\" on \"actuallylocal\" network."
-# }
+    assert_command dfx identity --network actuallylocal deploy-wallet "${ID_TWO}"
+    assert_match "The wallet canister \"${ID}\"\ already exists for user \"default\" on \"actuallylocal\" network."
+}
 
 @test "wallet create wallet" {
     dfx_start
