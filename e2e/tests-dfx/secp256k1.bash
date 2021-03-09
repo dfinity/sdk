53ef5913469a8fc118334160b660e799e388cc4b
Enzo Haussecker <enzo@sovereign.io>
2021-03-09 19:13:03 +0100

    Resolve shellcheck warning

--- e2e/tests-dfx/secp256k1.bash
+++ e2e/tests-dfx/secp256k1.bash
@@ -7,7 +7,7 @@ setup() {
     x=$(mktemp -d -t dfx-identity-home-XXXXXXXX)
     export TEMPORARY_HOME="$x"
     export HOME="$TEMPORARY_HOME"
-    cd $HOME || exit
+    cd "$HOME" || exit
 }
 
 teardown() {
