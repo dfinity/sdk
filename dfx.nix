ff62ff29d67aeecca9d69aebe9925930c43f7f16
Hans Larsen <hans@larsen.online>
2021-03-08 22:27:41 -0800

    .

--- dfx.nix
+++ dfx.nix
@@ -30,8 +30,9 @@ let
       preConfigure = (attrs.preConfigure or "") + ''
         unset SDKROOT
       '';
-      nativeBuildInputs = (attrs.nativeBuildInputs or []) ++
-        lib.optional pkgs.stdenv.isDarwin pkgs.darwin.libiconv;
+      nativeBuildInputs = (
+        attrs.nativeBuildInputs or []
+      ) ++ lib.optional pkgs.stdenv.isDarwin pkgs.darwin.libiconv;
     };
   };
 
