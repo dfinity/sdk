230a6d5c232746a12998e1659c38f690cbc6121b
Prithvi Shahi <50885601+p-shahi@users.noreply.github.com>
2021-03-10 21:52:25 -0800

    feat: add --no-wallet flag and --wallet option to allow Users to bypass Wallet or specify a Wallet to use for calls (#1476)
    closes https://github.com/dfinity/sdk/issues/1439

--- src/dfx/src/lib/operations/canister/mod.rs
+++ src/dfx/src/lib/operations/canister/mod.rs
@@ -8,6 +8,7 @@ pub use install_canister::install_canister;
 
 use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
+use crate::lib::identity::identity_utils::CallSender;
 use crate::lib::identity::Identity;
 use crate::lib::waiter::waiter_with_timeout;
 use anyhow::anyhow;
@@ -20,11 +21,12 @@ use ic_utils::interfaces::ManagementCanister;
 use serde::Deserialize;
 use std::time::Duration;
 
-async fn do_wallet_management_call<A, O>(
+async fn do_management_call<A, O>(
     env: &dyn Environment,
     method: &str,
     arg: A,
     timeout: Duration,
+    call_sender: &CallSender,
 ) -> DfxResult<O>
 where
     A: CandidType + Sync + Send,
@@ -35,17 +37,23 @@ where
         .ok_or_else(|| anyhow!("Cannot get HTTP client from environment."))?;
     let mgr = ManagementCanister::create(agent);
 
-    // Get the wallet canister.
-    let network = env
-        .get_network_descriptor()
-        .expect("No network descriptor.");
-    let identity_name = env.get_selected_identity().expect("No selected identity.");
-    let wallet = Identity::get_wallet_canister(env, network, &identity_name).await?;
-
-    let out: O = wallet
-        .call_forward(mgr.update_(method).with_arg(arg).build(), 0)?
-        .call_and_wait(waiter_with_timeout(timeout))
-        .await?;
+    let out = match call_sender {
+        CallSender::SelectedId => {
+            mgr.update_(method)
+                .with_arg(arg)
+                .build()
+                .call_and_wait(waiter_with_timeout(timeout))
+                .await?
+        }
+        CallSender::Wallet(wallet_id) | CallSender::SelectedIdWallet(wallet_id) => {
+            let wallet = Identity::build_wallet_canister(wallet_id.clone(), env)?;
+            let out: O = wallet
+                .call_forward(mgr.update_(method).with_arg(arg).build(), 0)?
+                .call_and_wait(waiter_with_timeout(timeout))
+                .await?;
+            out
+        }
+    };
 
     Ok(out)
 }
@@ -54,6 +62,7 @@ pub async fn get_canister_status(
     env: &dyn Environment,
     canister_id: Principal,
     timeout: Duration,
+    call_sender: &CallSender,
 ) -> DfxResult<CanisterStatus> {
     #[derive(CandidType)]
     struct In {
@@ -65,8 +74,14 @@ pub async fn get_canister_status(
         status: CanisterStatus,
     }
 
-    let (out,): (Out,) =
-        do_wallet_management_call(env, "canister_status", In { canister_id }, timeout).await?;
+    let (out,): (Out,) = do_management_call(
+        env,
+        "canister_status",
+        In { canister_id },
+        timeout,
+        call_sender,
+    )
+    .await?;
     Ok(out.status)
 }
 
@@ -74,14 +89,21 @@ pub async fn start_canister(
     env: &dyn Environment,
     canister_id: Principal,
     timeout: Duration,
+    call_sender: &CallSender,
 ) -> DfxResult {
     #[derive(CandidType)]
     struct In {
         canister_id: Principal,
     }
 
-    let _: () =
-        do_wallet_management_call(env, "start_canister", In { canister_id }, timeout).await?;
+    let _: () = do_management_call(
+        env,
+        "start_canister",
+        In { canister_id },
+        timeout,
+        call_sender,
+    )
+    .await?;
     Ok(())
 }
 
@@ -89,14 +111,21 @@ pub async fn stop_canister(
     env: &dyn Environment,
     canister_id: Principal,
     timeout: Duration,
+    call_sender: &CallSender,
 ) -> DfxResult {
     #[derive(CandidType)]
     struct In {
         canister_id: Principal,
     }
 
-    let _: () =
-        do_wallet_management_call(env, "stop_canister", In { canister_id }, timeout).await?;
+    let _: () = do_management_call(
+        env,
+        "stop_canister",
+        In { canister_id },
+        timeout,
+        call_sender,
+    )
+    .await?;
     Ok(())
 }
 
@@ -105,6 +134,7 @@ pub async fn set_controller(
     canister_id: Principal,
     new_controller: Principal,
     timeout: Duration,
+    call_sender: &CallSender,
 ) -> DfxResult {
     #[derive(CandidType)]
     struct In {
@@ -112,7 +142,7 @@ pub async fn set_controller(
         new_controller: Principal,
     }
 
-    let _: () = do_wallet_management_call(
+    let _: () = do_management_call(
         env,
         "set_controller",
         In {
@@ -120,6 +150,7 @@ pub async fn set_controller(
             new_controller,
         },
         timeout,
+        call_sender,
     )
     .await?;
     Ok(())
@@ -129,13 +160,20 @@ pub async fn delete_canister(
     env: &dyn Environment,
     canister_id: Principal,
     timeout: Duration,
+    call_sender: &CallSender,
 ) -> DfxResult {
     #[derive(CandidType)]
     struct In {
         canister_id: Principal,
     }
-    let _: () =
-        do_wallet_management_call(env, "delete_canister", In { canister_id }, timeout).await?;
+    let _: () = do_management_call(
+        env,
+        "delete_canister",
+        In { canister_id },
+        timeout,
+        call_sender,
+    )
+    .await?;
 
     Ok(())
 }
