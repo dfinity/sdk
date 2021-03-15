bbfb23f72c5619c11593ec423318d7a077d2ea51
chenyan-dfinity <yan.chen@dfinity.org>
2021-03-14 21:07:05 -0700

    test: possible memory leak?

---
+++ e2e/tests-dfx/leak.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+    # We want to work from a temporary directory, different for every test.
+    cd "$(mktemp -d -t dfx-e2e-XXXXXXXX)" || exit
+    export RUST_BACKTRACE=1
+
+    dfx_new
+}
+
+teardown() {
+    dfx_stop
+}
+
+@test "repeated install wasm" {
+    [ "$USE_IC_REF" ] && skip "skipped for ic-ref"
+
+    install_asset custom_canister
+    dfx_start
+    dfx deploy
+    for i in {1..50} do
+      dfx canister install --all --mode=reinstall
+    done
+}
