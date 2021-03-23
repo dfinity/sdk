8d1654fcfe9e05b2afe28a28a1520f5dca3d1d6b
Hans <hans@larsen.online>
2021-03-23 15:03:32 -0700

    chore: fix evaluation of releases for beta release (#1539)

--- ci/release.nix
+++ ci/release.nix
@@ -5,7 +5,7 @@ let
 
   # versionMatch is `null` if `src.gitTag` is not of the right format like "1.23.456"
   # and it's a list of matches like [ "1.23.456" ] when it is.
-  versionMatches = builtins.match "([0-9]+\.[0-9]+\.[0-9]+)" src.gitTag;
+  versionMatches = builtins.match "([0-9]+\.[0-9]+\.[0-9]+(-beta\.[0-9]+)?)" src.gitTag;
   releaseVersion = if versionMatches == null then "latest" else builtins.head versionMatches;
 
   ci = import ./ci.nix { inherit src releaseVersion; };
