c6d7326a2b14022b6cdaed77071bd3b4b289c7f5
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-03-23 23:02:27 -0700

    use openssl dgst instead of shasum

--- e2e/tests-dfx/certified_info.bash
+++ e2e/tests-dfx/certified_info.bash
@@ -18,9 +18,10 @@ teardown() {
     assert_match "Controller: $(dfx identity get-wallet) Module hash: None"
 
     dfx build hello
-    RESULT="0x$(shasum -a 256 .dfx/local/canisters/hello/hello.wasm)"
+    RESULT="$(openssl dgst -sha256 .dfx/local/canisters/hello/hello.wasm)"
     # shellcheck disable=SC2034
-    HASH=$(echo "${RESULT}" | cut -d' ' -f 1)
+    HASH="0x"
+    HASH+=$(echo "${RESULT}" | cut -d' ' -f 2)
 
     dfx canister install hello    
     assert_command dfx canister info "$(dfx canister id hello)"
