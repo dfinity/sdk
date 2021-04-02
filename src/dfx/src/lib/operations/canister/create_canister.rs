d5495d266b2425105bfa2bac0840d0b234bb84d3
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-04-02 16:39:22 -0700

    split fee and deposit constants

--- src/dfx/src/lib/operations/canister/create_canister.rs
+++ src/dfx/src/lib/operations/canister/create_canister.rs
@@ -13,10 +13,11 @@ use slog::info;
 use std::format;
 use std::time::Duration;
 
+// The cycle fee for create request is 1T cycles.
+const CANISTER_CREATE_FEE: u64 = 1_000_000_000_000_u64;
 // We do not know the minimum cycle balance a canister should have.
 // For now create the canister with 10T cycle balance.
-// This value accounts for the 1T canister create fee as well.
-const CREATE_FEE_PLUS_DEPOSIT: u64 = 11_000_000_000_000_u64;
+const CANISTER_INITIAL_CYCLE_BALANCE: u64 = 10_000_000_000_000_u64;
 
 pub async fn create_canister(
     env: &dyn Environment,
@@ -89,9 +90,10 @@ pub async fn create_canister(
                         create_result.canister_id
                     } else {
                         // amount has been validated by cycle_amount_validator
-                        let cycles = with_cycles.map_or(CREATE_FEE_PLUS_DEPOSIT, |amount| {
-                            amount.parse::<u64>().unwrap()
-                        });
+                        let cycles = with_cycles.map_or(
+                            CANISTER_CREATE_FEE + CANISTER_INITIAL_CYCLE_BALANCE,
+                            |amount| amount.parse::<u64>().unwrap(),
+                        );
                         wallet
                             .wallet_create_canister(cycles, None)
                             .call_and_wait(waiter_with_timeout(timeout))
