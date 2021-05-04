79f986f6fb1fd118df8efcb94f653bfb13abbe92
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-05-03 21:35:35 -0700

    add option

--- src/dfx/src/commands/ledger/account_id.rs
+++ src/dfx/src/commands/ledger/account_id.rs
@@ -6,12 +6,19 @@ use clap::Clap;
 
 /// Prints the selected identity's AccountIdentifier.
 #[derive(Clap)]
-pub struct AccountIdOpts {}
+pub struct AccountIdOpts {
+	principal: Option<String>
+}
 
-pub async fn exec(env: &dyn Environment, _opts: AccountIdOpts) -> DfxResult {
-    let sender = env
-        .get_selected_identity_principal()
-        .expect("Selected identity not instantiated.");
-    println!("{}", AccountIdentifier::new(sender, None));
+pub async fn exec(env: &dyn Environment, opts: AccountIdOpts) -> DfxResult {
+    if let Some(p) = opts.principal {
+    	let principal = ic_types::Principal::from_text(p)?;
+    	println!("{}", AccountIdentifier::new(principal, None));
+    } else {
+	    let sender = env
+	        .get_selected_identity_principal()
+	        .expect("Selected identity not instantiated.");
+	    println!("{}", AccountIdentifier::new(sender, None));
+    }
     Ok(())
 }
