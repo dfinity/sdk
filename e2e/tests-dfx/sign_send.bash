e5d5fff5dc73d83d8834b3c0c6dfd1c8050ed0ef
Linwei Shang <linwei.shang@dfinity.org>
2021-05-20 02:48:49 -0700

    Add e2e test for signed_request_status

--- e2e/tests-dfx/sign_send.bash
+++ e2e/tests-dfx/sign_send.bash
@@ -25,12 +25,16 @@ teardown() {
     sleep 10
     echo y | assert_command dfx canister --no-wallet send message.json
 
+    assert_command_fail dfx canister --no-wallet send message.json --status
+    assert_eq "Can only check request_status on update calls."
+
     assert_command_fail dfx canister --no-wallet sign --query hello read
     assert_eq "[message.json] already exists, please specify a different output file name."
 
     assert_command dfx canister --no-wallet sign --update hello inc --file message-inc.json
-    assert_eq "Update message generated at [message-inc.json]"
+    assert_eq "Update message generated at [message-inc.json] Signed request_status append to update message in [message-inc.json]"
 
     sleep 10
     echo y | assert_command dfx canister --no-wallet send message-inc.json
+    assert_command dfx canister --no-wallet send message-inc.json --status
 }
