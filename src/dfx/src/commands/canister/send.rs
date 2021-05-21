2b5110750597a7dd086d45a3d3af8bf5e501b2c8
Linwei Shang <linwei.shang@dfinity.org>
2021-05-21 09:58:15 -0700

    Improve QueryResponse handle

--- src/dfx/src/commands/canister/send.rs
+++ src/dfx/src/commands/canister/send.rs
@@ -2,11 +2,13 @@ use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 use crate::lib::identity::identity_utils::CallSender;
 use crate::lib::sign::signed_message::SignedMessageV1;
+use crate::util::print_idl_blob;
 
+use ic_agent::agent::replica_api::QueryResponse;
 use ic_agent::agent::ReplicaV2Transport;
 use ic_agent::{agent::http_transport::ReqwestHttpReplicaV2Transport, RequestId};
 
-use anyhow::{anyhow, bail};
+use anyhow::{anyhow, bail, Context};
 use clap::Clap;
 use ic_types::Principal;
 use std::{fs::File, path::Path};
@@ -82,11 +84,24 @@ pub async fn exec(
     match message.call_type.as_str() {
         "query" => {
             let response = transport.query(canister_id, content).await?;
-            eprintln!(
-                "To see the content of response, copy-paste the encoded string into cbor.me."
-            );
-            eprint!("Response: ");
-            println!("{}", hex::encode(response));
+            let query_response: QueryResponse = serde_cbor::from_slice(&response)?;
+            match query_response {
+                QueryResponse::Replied { reply } => {
+                    let blob = reply.arg;
+                    print_idl_blob(&blob, Some("idl"), &None)
+                        .context("Invalid data: Invalid IDL blob.")?;
+                }
+                QueryResponse::Rejected {
+                    reject_code,
+                    reject_message,
+                } => {
+                    bail!(
+                        r#"The Replica returned an error: code {}, message: "{}""#,
+                        reject_code,
+                        reject_message
+                    );
+                }
+            }
         }
         "update" => {
             let request_id = RequestId::from_str(
