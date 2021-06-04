6356a46c973e474ba4c4af621d7d06778fae27de
Eric Swanson <eric.swanson@dfinity.org>
2021-06-04 16:24:45 -0700

    16

--- e2e/utils/_.bash
+++ e2e/utils/_.bash
@@ -105,10 +105,16 @@ dfx_start_replica_and_bootstrap() {
         dfx replica --emulator --port 0 "$@" 3>&- &
         export DFX_REPLICA_PID=$!
 
+        ls -alR .
+        echo "timeout / wait for ic-ref port"
+
         timeout 60 sh -c \
             "until cat .dfx/ic-ref.port; do echo waiting for ic-ref port; sleep 1; done" \
             || (echo "replica did not write to .dfx/ic-ref.port file" && exit 1)
 
+        echo "Contents of .dfx/"
+        ls -l .dfx
+        echo "check for .dfx/ic-ref.port"
 
         test -f .dfx/ic-ref.port
         local replica_port=$(cat .dfx/ic-ref.port)
