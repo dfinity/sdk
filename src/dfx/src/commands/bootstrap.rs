7730ac5c1a1bb468757d86fd7d2f268fbde6bc0d
Eric Swanson <eric.swanson@dfinity.org>
2021-06-09 10:07:13 -0700

    2

--- src/dfx/src/commands/bootstrap.rs
+++ src/dfx/src/commands/bootstrap.rs
@@ -61,7 +61,7 @@ pub fn exec(env: &dyn Environment, opts: BootstrapOpts) -> DfxResult {
         .map(|uri| Url::parse(uri).unwrap())
         .collect();
 
-    println!("bootstrap (1)");
+    println!("bootstrap (2)");
     // Since the user may have provided port "0", we need to grab a dynamically
     // allocated port and construct a resuable SocketAddr which the actix
     // HttpServer will bind to
