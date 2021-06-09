977c4ccef4c80f3886c074898090645f75f62eb4
Eric Swanson <eric.swanson@dfinity.org>
2021-06-09 09:25:40 -0700

    force rebuild/retest (1)

--- src/dfx/src/commands/bootstrap.rs
+++ src/dfx/src/commands/bootstrap.rs
@@ -60,6 +60,8 @@ pub fn exec(env: &dyn Environment, opts: BootstrapOpts) -> DfxResult {
 
     let (sender, receiver) = crossbeam::unbounded();
 
+    println!("bootstrap (1)");
+
     // Since the user may have provided port "0", we need to grab a dynamically
     // allocated port and construct a resuable SocketAddr which the actix
     // HttpServer will bind to
