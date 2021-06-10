504ec917d90124cdc4af5323900f194e51fc2842
Eric Swanson <eric.swanson@dfinity.org>
2021-06-10 11:00:39 -0700

    11

--- src/dfx/src/commands/bootstrap.rs
+++ src/dfx/src/commands/bootstrap.rs
@@ -60,7 +60,7 @@ pub fn exec(env: &dyn Environment, opts: BootstrapOpts) -> DfxResult {
 
     let (sender, receiver) = crossbeam::unbounded();
 
-    println!("bootstrap (10)");
+    println!("bootstrap (11)");
 
     // Since the user may have provided port "0", we need to grab a dynamically
     // allocated port and construct a resuable SocketAddr which the actix
