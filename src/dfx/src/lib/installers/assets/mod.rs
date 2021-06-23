2e8091d93feaea32253feaae629b07247b2dac02
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2021-06-23 07:22:41 -0700

    feat: concurrent asset uploads (#1722)

--- src/dfx/src/lib/installers/assets/mod.rs
+++ src/dfx/src/lib/installers/assets/mod.rs
@@ -6,6 +6,9 @@ use crate::lib::installers::assets::content_encoder::ContentEncoder;
 use crate::lib::waiter::waiter_with_timeout;
 use candid::{CandidType, Decode, Encode, Nat};
 
+use futures::future::try_join_all;
+use futures::TryFutureExt;
+use futures_intrusive::sync::SharedSemaphore;
 use garcon::{Delay, Waiter};
 use ic_agent::Agent;
 use ic_types::Principal;
@@ -25,6 +28,19 @@ const CREATE_CHUNK: &str = "create_chunk";
 const COMMIT_BATCH: &str = "commit_batch";
 const LIST: &str = "list";
 const MAX_CHUNK_SIZE: usize = 1_900_000;
+
+// Maximum MB of file data to load at once.  More memory may be used, due to encodings.
+const MAX_SIMULTANEOUS_LOADED_MB: usize = 50;
+
+// The most mb any one file is considered to have for purposes of limiting data loaded at once.
+// Any file counts as at least 1 mb.
+const MAX_COST_SINGLE_FILE_MB: usize = 45;
+
+// How many simultaneous Agent.call() to create_chunk
+const MAX_SIMULTANEOUS_CREATE_CHUNK_CALLS: usize = 1;
+
+// How many simultaneous Agent.wait() on create_chunk result
+const MAX_SIMULTANEOUS_CREATE_CHUNK_WAITS: usize = 1;
 
 #[derive(CandidType, Debug)]
 struct CreateBatchRequest {}
@@ -147,6 +163,8 @@ async fn create_chunk(
     canister_call_params: &CanisterCallParams<'_>,
     batch_id: &Nat,
     content: &[u8],
+    create_chunk_call_semaphore: &SharedSemaphore,
+    create_chunk_wait_semaphore: &SharedSemaphore,
 ) -> DfxResult<Nat> {
     let batch_id = batch_id.clone();
     let args = CreateChunkRequest { batch_id, content };
@@ -159,19 +177,36 @@ async fn create_chunk(
     waiter.start();
 
     loop {
-        match canister_call_params
+        let mut builder = canister_call_params
             .agent
-            .update(&canister_call_params.canister_id, CREATE_CHUNK)
-            .with_arg(&args)
-            .expire_after(canister_call_params.timeout)
-            .call_and_wait(waiter_with_timeout(canister_call_params.timeout))
-            .await
-            .map_err(DfxError::from)
-            .and_then(|response| {
-                candid::Decode!(&response, CreateChunkResponse)
-                    .map_err(DfxError::from)
-                    .map(|x| x.chunk_id)
-            }) {
+            .update(&canister_call_params.canister_id, CREATE_CHUNK);
+        let builder = builder.with_arg(&args);
+        let request_id_result = {
+            let _releaser = create_chunk_call_semaphore.acquire(1).await;
+            builder
+                .expire_after(canister_call_params.timeout)
+                .call()
+                .await
+        };
+        let wait_result = match request_id_result {
+            Ok(request_id) => {
+                let _releaser = create_chunk_wait_semaphore.acquire(1).await;
+                canister_call_params
+                    .agent
+                    .wait(
+                        request_id,
+                        &canister_call_params.canister_id,
+                        waiter_with_timeout(canister_call_params.timeout),
+                    )
+                    .await
+            }
+            Err(err) => Err(err),
+        };
+        match wait_result.map_err(DfxError::from).and_then(|response| {
+            candid::Decode!(&response, CreateChunkResponse)
+                .map_err(DfxError::from)
+                .map(|x| x.chunk_id)
+        }) {
             Ok(chunk_id) => {
                 break Ok(chunk_id);
             }
@@ -189,33 +224,57 @@ async fn upload_content_chunks(
     asset_location: &AssetLocation,
     content: &Content,
     content_encoding: &str,
+    create_chunk_call_semaphore: &SharedSemaphore,
+    create_chunk_wait_semaphore: &SharedSemaphore,
 ) -> DfxResult<Vec<Nat>> {
-    let mut chunk_ids: Vec<Nat> = vec![];
-    let chunks = content.data.chunks(MAX_CHUNK_SIZE);
-    let (num_chunks, _) = chunks.size_hint();
-    for (i, data_chunk) in chunks.enumerate() {
-        println!(
-            "  {}{} {}/{} ({} bytes)",
-            &asset_location.key,
-            content_encoding_descriptive_suffix(content_encoding),
-            i + 1,
-            num_chunks,
-            data_chunk.len(),
-        );
-        chunk_ids.push(create_chunk(canister_call_params, batch_id, data_chunk).await?);
-    }
-    if chunk_ids.is_empty() {
+    if content.data.is_empty() {
+        let empty = vec![];
+        let chunk_id = create_chunk(
+            canister_call_params,
+            batch_id,
+            &empty,
+            create_chunk_call_semaphore,
+            create_chunk_wait_semaphore,
+        )
+        .await?;
         println!(
             "  {}{} 1/1 (0 bytes)",
             &asset_location.key,
             content_encoding_descriptive_suffix(content_encoding)
         );
-        let empty = vec![];
-        chunk_ids.push(create_chunk(canister_call_params, batch_id, &empty).await?);
+        return Ok(vec![chunk_id]);
     }
-    Ok(chunk_ids)
+
+    let count = (content.data.len() + MAX_CHUNK_SIZE - 1) / MAX_CHUNK_SIZE;
+    let chunks_futures: Vec<_> = content
+        .data
+        .chunks(MAX_CHUNK_SIZE)
+        .enumerate()
+        .map(|(i, data_chunk)| {
+            create_chunk(
+                canister_call_params,
+                batch_id,
+                data_chunk,
+                create_chunk_call_semaphore,
+                create_chunk_wait_semaphore,
+            )
+            .map_ok(move |chunk_id| {
+                println!(
+                    "  {}{} {}/{} ({} bytes)",
+                    &asset_location.key,
+                    content_encoding_descriptive_suffix(content_encoding),
+                    i + 1,
+                    count,
+                    data_chunk.len(),
+                );
+                chunk_id
+            })
+        })
+        .collect();
+    try_join_all(chunks_futures).await
 }
 
+#[allow(clippy::too_many_arguments)]
 async fn make_project_asset_encoding(
     canister_call_params: &CanisterCallParams<'_>,
     batch_id: &Nat,
@@ -223,6 +282,8 @@ async fn make_project_asset_encoding(
     container_assets: &HashMap<String, AssetDetails>,
     content: &Content,
     content_encoding: &str,
+    create_chunk_call_semaphore: &SharedSemaphore,
+    create_chunk_wait_semaphore: &SharedSemaphore,
 ) -> DfxResult<ProjectAssetEncoding> {
     let sha256 = content.sha256();
 
@@ -260,6 +321,8 @@ async fn make_project_asset_encoding(
             &asset_location,
             content,
             content_encoding,
+            create_chunk_call_semaphore,
+            create_chunk_wait_semaphore,
         )
         .await?
     };
@@ -284,7 +347,19 @@ async fn make_project_asset(
     batch_id: &Nat,
     asset_location: AssetLocation,
     container_assets: &HashMap<String, AssetDetails>,
+    file_semaphore: &SharedSemaphore,
+    create_chunk_call_semaphore: &SharedSemaphore,
+    create_chunk_wait_semaphore: &SharedSemaphore,
 ) -> DfxResult<ProjectAsset> {
+    let file_size = std::fs::metadata(&asset_location.source)?.len();
+    let permits = std::cmp::max(
+        1,
+        std::cmp::min(
+            ((file_size + 999999) / 1000000) as usize,
+            MAX_COST_SINGLE_FILE_MB,
+        ),
+    );
+    let _releaser = file_semaphore.acquire(permits).await;
     let content = Content::load(&asset_location.source)?;
 
     let encodings = make_encodings(
@@ -293,6 +368,8 @@ async fn make_project_asset(
         &asset_location,
         container_assets,
         &content,
+        create_chunk_call_semaphore,
+        create_chunk_wait_semaphore,
     )
     .await?;
 
@@ -311,47 +388,96 @@ fn applicable_encoders(media_type: &Mime) -> Vec<ContentEncoder> {
     }
 }
 
+#[allow(clippy::too_many_arguments)]
+async fn make_encoding(
+    canister_call_params: &CanisterCallParams<'_>,
+    batch_id: &Nat,
+    asset_location: &AssetLocation,
+    container_assets: &HashMap<String, AssetDetails>,
+    content: &Content,
+    encoder: &Option<ContentEncoder>,
+    create_chunk_call_semaphore: &SharedSemaphore,
+    create_chunk_wait_semaphore: &SharedSemaphore,
+) -> DfxResult<Option<(String, ProjectAssetEncoding)>> {
+    match encoder {
+        None => {
+            let identity_asset_encoding = make_project_asset_encoding(
+                canister_call_params,
+                batch_id,
+                &asset_location,
+                container_assets,
+                &content,
+                CONTENT_ENCODING_IDENTITY,
+                create_chunk_call_semaphore,
+                create_chunk_wait_semaphore,
+            )
+            .await?;
+            Ok(Some((
+                CONTENT_ENCODING_IDENTITY.to_string(),
+                identity_asset_encoding,
+            )))
+        }
+        Some(encoder) => {
+            let encoded = content.encode(&encoder)?;
+            if encoded.data.len() < content.data.len() {
+                let content_encoding = format!("{}", encoder);
+                let project_asset_encoding = make_project_asset_encoding(
+                    canister_call_params,
+                    batch_id,
+                    &asset_location,
+                    container_assets,
+                    &encoded,
+                    &content_encoding,
+                    create_chunk_call_semaphore,
+                    create_chunk_wait_semaphore,
+                )
+                .await?;
+                Ok(Some((content_encoding, project_asset_encoding)))
+            } else {
+                Ok(None)
+            }
+        }
+    }
+}
+
 async fn make_encodings(
     canister_call_params: &CanisterCallParams<'_>,
     batch_id: &Nat,
     asset_location: &AssetLocation,
     container_assets: &HashMap<String, AssetDetails>,
     content: &Content,
+    create_chunk_call_semaphore: &SharedSemaphore,
+    create_chunk_wait_semaphore: &SharedSemaphore,
 ) -> DfxResult<HashMap<String, ProjectAssetEncoding>> {
-    let mut encodings = HashMap::new();
-
-    let identity_asset_encoding = make_project_asset_encoding(
-        canister_call_params,
-        batch_id,
-        &asset_location,
-        container_assets,
-        &content,
-        CONTENT_ENCODING_IDENTITY,
-    )
-    .await?;
-    encodings.insert(
-        CONTENT_ENCODING_IDENTITY.to_string(),
-        identity_asset_encoding,
-    );
-
+    let mut encoders = vec![None];
     for encoder in applicable_encoders(&content.media_type) {
-        let encoded = content.encode(&encoder)?;
-        if encoded.data.len() < content.data.len() {
-            let content_encoding = format!("{}", encoder);
-            let project_asset_encoding = make_project_asset_encoding(
-                canister_call_params,
-                batch_id,
-                &asset_location,
-                container_assets,
-                &encoded,
-                &content_encoding,
-            )
-            .await?;
-            encodings.insert(content_encoding, project_asset_encoding);
-        }
+        encoders.push(Some(encoder));
     }
 
-    Ok(encodings)
+    let encoding_futures: Vec<_> = encoders
+        .iter()
+        .map(|maybe_encoder| {
+            make_encoding(
+                canister_call_params,
+                batch_id,
+                asset_location,
+                container_assets,
+                content,
+                maybe_encoder,
+                create_chunk_call_semaphore,
+                create_chunk_wait_semaphore,
+            )
+        })
+        .collect();
+
+    let encodings = try_join_all(encoding_futures).await?;
+
+    let mut result: HashMap<String, ProjectAssetEncoding> = HashMap::new();
+
+    for (key, value) in encodings.into_iter().flatten() {
+        result.insert(key, value);
+    }
+    Ok(result)
 }
 
 async fn make_project_assets(
@@ -359,14 +485,31 @@ async fn make_project_assets(
     batch_id: &Nat,
     locs: Vec<AssetLocation>,
     container_assets: &HashMap<String, AssetDetails>,
+    file_semaphore: &SharedSemaphore,
+    create_chunk_call_semaphore: &SharedSemaphore,
+    create_chunk_wait_semaphore: &SharedSemaphore,
 ) -> DfxResult<HashMap<String, ProjectAsset>> {
-    let mut project_assets = HashMap::new();
-    for loc in locs {
-        let project_asset =
-            make_project_asset(canister_call_params, batch_id, loc, &container_assets).await?;
-        project_assets.insert(project_asset.asset_location.key.clone(), project_asset);
+    let project_asset_futures: Vec<_> = locs
+        .iter()
+        .map(|loc| {
+            make_project_asset(
+                canister_call_params,
+                batch_id,
+                loc.clone(),
+                &container_assets,
+                file_semaphore,
+                create_chunk_call_semaphore,
+                create_chunk_wait_semaphore,
+            )
+        })
+        .collect();
+    let project_assets = try_join_all(project_asset_futures).await?;
+
+    let mut hm = HashMap::new();
+    for project_asset in project_assets {
+        hm.insert(project_asset.asset_location.key.clone(), project_asset);
     }
-    Ok(project_assets)
+    Ok(hm)
 }
 
 async fn commit_batch(
@@ -520,11 +663,28 @@ pub async fn post_install_store_assets(
 
     let batch_id = create_batch(&canister_call_params).await?;
 
+    // The "file" semaphore limits how much file data to load at once.  A given loaded file's data
+    // may be simultaneously encoded (gzip and so forth).
+    let file_semaphore = SharedSemaphore::new(true, MAX_SIMULTANEOUS_LOADED_MB);
+
+    // The create_chunk call semaphore limits the number of simultaneous
+    // agent.call()s to create_chunk.
+    let create_chunk_call_semaphore =
+        SharedSemaphore::new(true, MAX_SIMULTANEOUS_CREATE_CHUNK_CALLS);
+
+    // The create_chunk wait semaphore limits the number of simultaneous
+    // agent.wait() calls for outstanding create_chunk requests.
+    let create_chunk_wait_semaphore =
+        SharedSemaphore::new(true, MAX_SIMULTANEOUS_CREATE_CHUNK_WAITS);
+
     let project_assets = make_project_assets(
         &canister_call_params,
         &batch_id,
         asset_locations,
         &container_assets,
+        &file_semaphore,
+        &create_chunk_call_semaphore,
+        &create_chunk_wait_semaphore,
     )
     .await?;
 
