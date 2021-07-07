f43b9b7a9b34b8ed93065c0b80dbe055b501a56c
Gabor Greif <gabor@dfinity.org>
2021-07-07 13:08:48 +0200

    toying around with patching

--- e2e/assets/default_args/patch.bash
+++ e2e/assets/default_args/patch.bash
@@ -1,3 +1,16 @@
 #!/dev/null
 
+patch invalid.mo <<EOF
+@@ -1,3 +1,3 @@
+ actor {
+-
++bla
+ }
+EOF
+
+cat invalid.mo
+
+find .
+
 dfx config canisters/e2e_project/main invalid.mo
+dfx config defaults/build/args " --compacting-gc "
