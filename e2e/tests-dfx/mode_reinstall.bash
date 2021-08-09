37c9fef23f4e648c1a8346e26dbca78483f26603
Eric Swanson <eric.swanson@dfinity.org>
2021-08-09 16:54:52 -0700

    first 7 tests pass

--- e2e/tests-dfx/mode_reinstall.bash
+++ e2e/tests-dfx/mode_reinstall.bash
@@ -66,3 +66,40 @@ teardown() {
 
 # dfx deploy --mode=reinstall requires canister name
 # dfx deploy --mode=reinstall
+
+@test "deploy --mode=reinstall fails" {
+    [ "$USE_IC_REF" ] && skip "skipped for ic-ref"
+
+    dfx_start
+    assert_command_fail dfx deploy --mode=reinstall
+
+    assert_match "The --mode=reinstall is only valid when deploying a single canister, because reinstallation destroys all data in the canister."
+}
+
+@test "deploy --mode=reinstall succeeds when a canister name is provided" {
+    dfx_start
+    dfx deploy
+
+    # if the pipe is alone with assert_command, $stdout, $stderr etc will not be available,
+    # so all the assert_match calls will fail.  http://mywiki.wooledge.org/BashFAQ/024
+    echo yes | (
+        assert_command dfx deploy --mode=reinstall e2e_project
+
+        assert_match "YOU WILL LOSE ALL DATA IN THE CANISTER"
+        assert_match "Reinstalling code for canister e2e_project"
+    )
+}
+
+@test "deploy --mode=reinstall refused if not approved" {
+    dfx_start
+    dfx deploy
+
+    echo no | (
+        assert_command_fail dfx deploy --mode=reinstall e2e_project
+
+        assert_match "YOU WILL LOSE ALL DATA IN THE CANISTER"
+
+        assert_not_match "Installing code for canister"
+        assert_match "Refusing to reinstall canister without approval"
+    )
+}
