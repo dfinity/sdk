9f7e1d589a3c6e45170235107de2da28bc2faed3
Eric Swanson <eric.swanson@dfinity.org>
2021-08-09 16:24:09 -0700

    first 4 e2e tests pass

---
+++ e2e/tests-dfx/mode_reinstall.bash
@@ -0,0 +1,68 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+    # We want to work from a temporary directory, different for every test.
+    x=$(mktemp -d -t dfx-e2e-XXXXXXXX)
+    export DFX_CONFIG_ROOT="$x"
+    cd "$x" || exit
+    export RUST_BACKTRACE=1
+
+    dfx_new
+}
+
+teardown() {
+    dfx_stop
+    rm -rf "$DFX_CONFIG_ROOT"
+}
+
+@test "install --mode=reinstall --all fails" {
+    [ "$USE_IC_REF" ] && skip "skipped for ic-ref"
+
+    dfx_start
+    assert_command_fail dfx canister install --mode=reinstall --all
+
+    assert_match "The --mode=reinstall is only valid when specifying a single canister, because reinstallation destroys all data in the canister."
+}
+
+@test "install --mode=reinstall fails if no canister is provided" {
+    # This fails because clap protects against it.
+    [ "$USE_IC_REF" ] && skip "skipped for ic-ref"
+
+    dfx_start
+    assert_command_fail dfx canister install --mode=reinstall
+    assert_match "required arguments were not provided"
+    assert_match "--all"
+}
+
+@test "reinstall succeeds when a canister name is provided" {
+    dfx_start
+    dfx deploy
+
+    # if the pipe is alone with assert_command, $stdout, $stderr etc will not be available,
+    # so all the assert_match calls will fail.
+    echo yes | (
+        assert_command dfx canister install --mode=reinstall e2e_project
+
+        assert_match "YOU WILL LOSE ALL DATA IN THE CANISTER"
+        assert_match "Reinstalling code for canister e2e_project"
+    )
+}
+
+@test "install --mode=reinstall refused if not approved" {
+    dfx_start
+    dfx deploy
+
+    echo no | (
+        assert_command_fail dfx canister install --mode=reinstall e2e_project
+
+        assert_match "YOU WILL LOSE ALL DATA IN THE CANISTER"
+
+        assert_not_match "Installing code for canister"
+        assert_match "Refusing to reinstall canister without approval"
+    )
+}
+
+# dfx deploy --mode=reinstall requires canister name
+# dfx deploy --mode=reinstall
