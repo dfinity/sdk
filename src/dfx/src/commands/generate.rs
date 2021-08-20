fcbda38cccc30008bdfe324c4f56b5232f4d2bff
Linwei Shang <linwei.shang@dfinity.org>
2021-08-20 05:34:46 -0700

    Notify user to create canister before generate

--- src/dfx/src/commands/generate.rs
+++ src/dfx/src/commands/generate.rs
@@ -2,6 +2,7 @@ use crate::lib::builders::BuildConfig;
 use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 use crate::lib::models::canister::CanisterPool;
+use crate::lib::models::canister_id_store::CanisterIdStore;
 use crate::lib::provider::create_agent_environment;
 
 use clap::Clap;
@@ -31,6 +32,12 @@ pub fn exec(env: &dyn Environment, opts: GenerateOpts) -> DfxResult {
 
     // Get pool of canisters to build
     let canister_pool = CanisterPool::load(&env, false, &canister_names)?;
+
+    // This is just to display an error if trying to generate before creating the canister.
+    let store = CanisterIdStore::for_env(&env)?;
+    for canister in canister_pool.get_canister_list() {
+        store.get(canister.get_name())?;
+    }
 
     let build_config = BuildConfig::from_config(&config)?;
 
