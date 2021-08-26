ea61dea5fefce047eb96d128a524d73216aebff1
Eric Swanson <eric.swanson@dfinity.org>
2021-08-26 10:42:00 -0700

    Revert "try some retries when fetching root key"
    This reverts commit e9d22e542298c0e22359efa18a1bf3968ded5990.

--- src/dfx/src/lib/root_key.rs
+++ src/dfx/src/lib/root_key.rs
@@ -2,7 +2,6 @@ use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 
 use anyhow::anyhow;
-use garcon::{Delay, Waiter};
 
 pub async fn fetch_root_key_if_needed(env: &dyn Environment) -> DfxResult {
     let agent = env
@@ -14,24 +13,7 @@ pub async fn fetch_root_key_if_needed(env: &dyn Environment) -> DfxResult {
         .expect("no network descriptor")
         .is_ic
     {
-        let mut waiter = Delay::builder()
-            .timeout(std::time::Duration::from_secs(30))
-            .throttle(std::time::Duration::from_secs(1))
-            .build();
-        waiter.start();
-
-        loop {
-
-            let fetch_result = agent.fetch_root_key().await;
-            if fetch_result.is_ok() {
-                return Ok(());
-            };
-
-            let wait_result = waiter.wait();
-            if wait_result.is_err() {
-                fetch_result?;
-            };
-        }
+        agent.fetch_root_key().await?;
     }
     Ok(())
 }
