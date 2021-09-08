0a50498777378436a4967a535caf896b90dc0403
Jude Taylor <jude.taylor@dfinity.org>
2021-09-08 16:25:56 +0000

    update shellchecks

--- public/default.nix
+++ public/default.nix
@@ -13,9 +13,7 @@ let
     } ''
       echo "Running shellcheck..."
       shellcheck --shell=sh $src/*.sh \
-        --exclude SC2039 \
-        --exclude SC2154 \
-        --exclude SC2034
+        --exclude SC2039,SC3003,SC3014,SC3043,SC2154,SC2034
 
       echo "Running shfmt..."
       shfmt -d -p -i 4 -ci -bn -s $src/*.sh
