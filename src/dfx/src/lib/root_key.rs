c8242b6a14391ec08eb4fd1be0c008608a9b48ac
Eric Swanson <eric.swanson@dfinity.org>
2021-09-21 14:35:35 -0700

    attempt retries when fetching root key.  this should actually happen in the agent, but by starting here I can run e2e tests locally

--- src/dfx/src/lib/root_key.rs
+++ src/dfx/src/lib/root_key.rs
@@ -2,6 +2,7 @@ use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 
 use anyhow::anyhow;
+use garcon::{Delay, Waiter};
 
 pub async fn fetch_root_key_if_needed(env: &dyn Environment) -> DfxResult {
     let agent = env
@@ -13,7 +14,28 @@ pub async fn fetch_root_key_if_needed(env: &dyn Environment) -> DfxResult {
         .expect("no network descriptor")
         .is_ic
     {
-        agent.fetch_root_key().await?;
+        let mut waiter = Delay::builder()
+            .exponential_backoff(std::time::Duration::from_secs(1), 2.0)
+            .timeout(std::time::Duration::from_secs(60 * 5))
+            .build();
+        // let mut waiter = Delay::builder()
+        //     .timeout(std::time::Duration::from_secs(30))
+        //     .throttle(std::time::Duration::from_secs(1))
+        //     .build();
+        waiter.start();
+
+        loop {
+
+            let fetch_result = agent.fetch_root_key().await;
+            if fetch_result.is_ok() {
+                return Ok(());
+            };
+
+            let wait_result = waiter.wait();
+            if wait_result.is_err() {
+                fetch_result?;
+            };
+        }
     }
     Ok(())
 }
