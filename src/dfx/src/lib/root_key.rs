1da66737763097939e2e43f2779710ef30bb2ec7
Eric Swanson <eric.swanson@dfinity.org>
2021-09-22 12:02:32 -0700

    retest 1

--- src/dfx/src/lib/root_key.rs
+++ src/dfx/src/lib/root_key.rs
@@ -14,6 +14,7 @@ pub async fn fetch_root_key_if_needed(env: &dyn Environment) -> DfxResult {
         .expect("no network descriptor")
         .is_ic
     {
+        // retest (1)
         let mut waiter = Delay::builder()
             .exponential_backoff(std::time::Duration::from_secs(1), 2.0)
             .timeout(std::time::Duration::from_secs(60 * 5))
