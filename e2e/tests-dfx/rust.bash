3940f5e14547f932349331e75f2fbc252185f1b1
Linwei Shang <linwei.shang@dfinity.org>
2021-09-30 16:16:52 -0700

    Add e2e test for rust canister

---
+++ e2e/tests-dfx/rust.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+    standard_setup
+
+    dfx_new
+}
+
+teardown() {
+    dfx_stop
+
+    standard_teardown
+}
+
+@test "build uses default build args" {
+    install_asset rust
+    dfx_start
+    dfx canister --no-wallet create --all
+    assert_command dfx build print
+    assert_match "Finished"
+    assert_command dfx canister --no-wallet install print
+}
