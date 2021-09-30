d76c66e40cd15670e13ea001214c79cdf1807c38
Eric Swanson <eric.swanson@dfinity.org>
2021-09-30 15:58:57 -0700

    no need to wait for replica healthy after dfx start --background

--- e2e/utils/_.bash
+++ e2e/utils/_.bash
@@ -115,8 +115,6 @@ dfx_start() {
     timeout 5 sh -c \
         "until nc -z localhost ${port}; do echo waiting for replica; sleep 1; done" \
         || (echo "could not connect to replica on port ${port}" && exit 1)
-
-    wait_until_replica_healthy
 }
 
 wait_until_replica_healthy() {
@@ -174,8 +172,6 @@ dfx_start_replica_and_bootstrap() {
     timeout 5 sh -c \
         "until nc -z localhost ${replica_port}; do echo waiting for replica; sleep 1; done" \
         || (echo "could not connect to replica on port ${replica_port}" && exit 1)
-
-    wait_until_replica_healthy
 
     # This only works because we use the network by name
     #    (implicitly: --network local)
