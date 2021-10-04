a99d26cdafd8a87d53e8e8a378471bca02d69bbe
Eric Swanson <eric.swanson@dfinity.org>
2021-10-04 10:44:27 -0700

    add 25x copies of start.bash, with only the two tests that kill the replica

---
+++ e2e/tests-dfx/start-21.bash
@@ -0,0 +1,82 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+    standard_setup
+
+    dfx_new hello
+}
+
+teardown() {
+    dfx_stop
+
+    standard_teardown
+}
+
+@test "dfx restarts the replica 21" {
+    [ "$USE_IC_REF" ] && skip "skip for ic-ref"
+
+    dfx_start
+
+    install_asset greet
+    assert_command dfx deploy
+    assert_command dfx canister call hello greet '("Alpha")'
+    assert_eq '("Hello, Alpha!")'
+    dfx canister status hello
+
+    REPLICA_PID=$(cat .dfx/replica-configuration/replica-pid)
+
+    echo "replica pid is $REPLICA_PID"
+
+    kill -KILL "$REPLICA_PID"
+    assert_process_exits "$REPLICA_PID" 15s
+
+    timeout 15s sh -c \
+      'until dfx ping; do echo waiting for replica to restart; sleep 1; done' \
+      || (echo "replica did not restart" && ps aux && exit 1)
+    wait_until_replica_healthy
+
+    dfx canister status hello
+    assert_command dfx canister call hello greet '("Omega")'
+    assert_eq '("Hello, Omega!")'
+}
+
+@test "dfx restarts icx-proxy when the replica restarts 21" {
+    [ "$USE_IC_REF" ] && skip "skip for ic-ref"
+
+    dfx_start
+
+    install_asset greet
+    assert_command dfx deploy
+    assert_command dfx canister call hello greet '("Alpha")'
+    assert_eq '("Hello, Alpha!")'
+    dfx canister status hello
+
+    REPLICA_PID=$(cat .dfx/replica-configuration/replica-pid)
+    ICX_PROXY_PID=$(cat .dfx/icx-proxy-pid)
+
+    echo "replica pid is $REPLICA_PID"
+    echo "icx-proxy pid is $ICX_PROXY_PID"
+
+    kill -KILL "$REPLICA_PID"
+    assert_process_exits "$REPLICA_PID" 15s
+    assert_process_exits "$ICX_PROXY_PID" 15s
+
+    timeout 15s sh -c \
+      'until dfx ping; do echo waiting for replica to restart; sleep 1; done' \
+      || (echo "replica did not restart" && ps aux && exit 1)
+    wait_until_replica_healthy
+
+    dfx canister status hello
+    assert_command dfx canister call hello greet '("Omega")'
+    assert_eq '("Hello, Omega!")'
+
+    ID=$(dfx canister id hello_assets)
+
+    timeout 15s sh -c \
+      "until curl --fail http://localhost:$(cat .dfx/webserver-port)/sample-asset.txt?canisterId=$ID; do echo waiting for icx-proxy to restart; sleep 1; done" \
+      || (echo "icx-proxy did not restart" && ps aux && exit 1)
+
+    assert_command curl --fail http://localhost:"$(cat .dfx/webserver-port)"/sample-asset.txt?canisterId="$ID"
+}
