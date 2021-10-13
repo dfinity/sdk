7dbc5c35a206cb85699fc27938f56999ef03a11d
Eric Swanson <eric.swanson@dfinity.org>
2021-10-13 10:36:17 -0700

    nix-fmt

--- check-binaries.nix
+++ check-binaries.nix
@@ -2,35 +2,35 @@
 , dfx ? import ./dfx.nix { inherit pkgs; }
 }:
 let
-   lib = pkgs.lib;
+  lib = pkgs.lib;
 in
 pkgs.runCommandNoCCLocal "check-binaries" {
-      nativeBuildInputs = with pkgs; [
-        dfx.build
-      ] ++ lib.optional stdenv.isDarwin darwin.binutils;
-    } ''
-    echo "check the binaries!"
-    mkdir -p $out
-    export DFX_CONFIG_ROOT="$out"
-    dfx cache install
-    CACHE_DIR="$(dfx cache show)"
-    echo "Cache dir is $CACHE_DIR"
+  nativeBuildInputs = with pkgs; [
+    dfx.build
+  ] ++ lib.optional stdenv.isDarwin darwin.binutils;
+} ''
+  echo "check the binaries!"
+  mkdir -p $out
+  export DFX_CONFIG_ROOT="$out"
+  dfx cache install
+  CACHE_DIR="$(dfx cache show)"
+  echo "Cache dir is $CACHE_DIR"
 
-    if uname -a | grep Linux; then
-        echo "checking linux.."
-        for a in dfx replica ic-starter ic-ref;
-        do
-            echo "checking $a"
-            ldd "$CACHE_DIR/$a"
-        done
-    else
-        echo "checking osx.."
-        for a in dfx replica ic-starter ic-ref;
-        do
-            echo "checking $a"
-            # will have to find otool....
-            otool -L "$CACHE_DIR/$a"
-        done
-    fi
+  if uname -a | grep Linux; then
+      echo "checking linux.."
+      for a in dfx replica ic-starter ic-ref;
+      do
+          echo "checking $a"
+          ldd "$CACHE_DIR/$a"
+      done
+  else
+      echo "checking osx.."
+      for a in dfx replica ic-starter ic-ref;
+      do
+          echo "checking $a"
+          # will have to find otool....
+          otool -L "$CACHE_DIR/$a"
+      done
+  fi
 
 ''
