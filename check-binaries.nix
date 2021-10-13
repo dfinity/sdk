e9cde952f9bc238f1793a6ee2f997ce024f962ba
Eric Swanson <eric.swanson@dfinity.org>
2021-10-13 09:54:09 -0700

    test: check binaries (wip)

---
+++ check-binaries.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import ./nix {}
+, dfx ? import ./dfx.nix { inherit pkgs; }
+}:
+let
+   lib = pkgs.lib;
+in
+pkgs.runCommand "check-binaries" {
+      nativeBuildInputs = with pkgs; [
+        dfx.build
+      ];
+    } ''
+    echo "check the binaries!"
+    mkdir -p $out
+    export DFX_CONFIG_ROOT="$out"
+    dfx cache install
+    CACHE_DIR="$(dfx cache show)"
+    echo "Cache dir is $CACHE_DIR"
+
+    if [ uname -a | grep Linux ]; then
+        echo "checking linux.."
+        for a in dfx replica ic-starter ic-ref;
+        do
+            echo "checking $a"
+            ldd "$CACHE_DIR/$a"
+        done
+    else
+        echo "checking osx.."
+        for a in dfx replica ic-starter ic-ref;
+        do
+            echo "checking $a"
+            # will have to find otool....
+            otool -L "$CACHE_DIR/$a"
+        done
+    fi
+
+''
