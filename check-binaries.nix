bddc0d2cd4711809c8877dba35d149d380081f10
Eric Swanson <eric.swanson@dfinity.org>
2021-10-13 11:49:37 -0700

    DRY

--- check-binaries.nix
+++ check-binaries.nix
@@ -3,6 +3,9 @@
 }:
 let
   lib = pkgs.lib;
+
+  lib_list_tool = if pkgs.stdenv.isDarwin then "otool -L" else "ldd";
+
 in
 pkgs.runCommand "check-binaries" {
   nativeBuildInputs = with pkgs; [
@@ -10,28 +13,14 @@ pkgs.runCommand "check-binaries" {
   ] ++ lib.optional stdenv.isDarwin darwin.binutils
   ++ lib.optional stdenv.isLinux glibc.bin;
 } ''
-  echo "check the binaries!"
   mkdir -p $out
   export DFX_CONFIG_ROOT="$out"
   dfx cache install
   CACHE_DIR="$(dfx cache show)"
-  echo "Cache dir is $CACHE_DIR"
 
-  if uname -a | grep Linux; then
-      echo "checking linux.."
-      for a in dfx replica ic-starter ic-ref;
-      do
-          echo "checking $a"
-          ldd "$CACHE_DIR/$a"
-      done
-  else
-      echo "checking osx.."
-      for a in dfx replica ic-starter ic-ref;
-      do
-          echo "checking $a"
-          # will have to find otool....
-          otool -L "$CACHE_DIR/$a"
-      done
-  fi
-
+  for a in dfx ic-ref ic-starter icx-proxy mo-doc mo-ide moc replica;
+  do
+      echo
+      ${lib_list_tool} "$CACHE_DIR/$a"
+  done
 ''
