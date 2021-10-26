40d8b74f3a0e67d262cbe906fbdf2bf892119574
Eric Swanson <eric.swanson@dfinity.org>
2021-10-26 12:02:44 -0700

    Revert "Revert "tar --no-overwrite-dir in order to not make directories unwritable""
    This reverts commit 83eb47458443cbb849ddbae3faf906e8c7b599bb.

--- scripts/prepare-dfx-assets.sh
+++ scripts/prepare-dfx-assets.sh
@@ -81,7 +81,8 @@ download_tarball() {
 
     download_url_and_check_sha "$URL" "$SHA256" "$DOWNLOAD_PATH"
 
-    tar -xvf "$DOWNLOAD_PATH" -C "$BINARY_CACHE_TEMP_DIR"
+    # --no-overwrite-dir: since some archives contain r-x ".", do not overwrite directory metadata.
+    tar --no-overwrite-dir -xvf "$DOWNLOAD_PATH" -C "$BINARY_CACHE_TEMP_DIR"
 }
 
 download_ic_ref() {
