d0f6a33c6c1ef2563d7b5701015c3b4a33b070ab
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2021-10-26 13:23:38 -0700

    fix: use tar -k not tar --no-overwrite-dir for platform independence (#1888)
    It turns out `--no-overwrite-dir` is nonstandard, and anyway `-k` achieves the same thing since we do not expect to overwrite any files anyway.

--- scripts/prepare-dfx-assets.sh
+++ scripts/prepare-dfx-assets.sh
@@ -81,8 +81,9 @@ download_tarball() {
 
     download_url_and_check_sha "$URL" "$SHA256" "$DOWNLOAD_PATH"
 
-    # --no-overwrite-dir: since some archives contain r-x ".", do not overwrite directory metadata.
-    tar --no-overwrite-dir -xvf "$DOWNLOAD_PATH" -C "$BINARY_CACHE_TEMP_DIR"
+    # -k: some archives contain r-x ".", and on linux the default behavior is to overwrite the
+    # metadata.  We only want to extract new files anyway.
+    tar -xkvf "$DOWNLOAD_PATH" -C "$BINARY_CACHE_TEMP_DIR"
 }
 
 download_ic_ref() {
