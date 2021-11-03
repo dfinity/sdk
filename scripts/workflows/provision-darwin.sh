ecfede4c5bc11049f4bc466fa10dde46947a23db
Eric Swanson <eric.swanson@dfinity.org>
2021-11-03 15:32:29 -0700

    .

--- scripts/workflows/provision-darwin.sh
+++ scripts/workflows/provision-darwin.sh
@@ -7,17 +7,6 @@ export
 # Enter temporary directory.
 pushd /tmp
 
-# Install Homebrew
-#curl --location --output install-brew.sh "https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
-#bash install-brew.sh
-#rm install-brew.sh
-
-# Install Node.
-#version=14.15.4
-#curl --location --output node.pkg "https://nodejs.org/dist/v$version/node-v$version.pkg"
-#sudo installer -pkg node.pkg -store -target /
-#rm node.pkg
-
 # Install Bats.
 brew unlink bats
 brew install bats-core
--- scripts/workflows/provision-linux.sh
+++ scripts/workflows/provision-linux.sh
@@ -7,12 +7,6 @@ export
 # Enter temporary directory.
 pushd /tmp
 
-# Install Node.
-#wget --output-document install-node.sh "https://deb.nodesource.com/setup_14.x"
-#sudo bash install-node.sh
-#sudo apt-get install --yes nodejs
-#rm install-node.sh
-
 # Install Bats.
 sudo apt-get install --yes bats
 
