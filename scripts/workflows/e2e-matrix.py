8f33a52d8dd31dc9b1ff27891558d038d31982b4
Eric Swanson <eric.swanson@dfinity.org>
2021-11-03 14:10:04 -0700

    cleanup

--- scripts/workflows/e2e-matrix.py
+++ scripts/workflows/e2e-matrix.py
@@ -14,46 +14,6 @@ def test_scripts(prefix):
 
 test = sorted(test_scripts('dfx') + test_scripts('replica'))
 
-# test = [
-#     'dfx/assetscanister',
-#     'dfx/base',
-#     'dfx/basic-project',
-#     'dfx/bootstrap',
-#     'dfx/build',
-#     'dfx/build_granular',
-#     'dfx/call',
-#     'dfx/candid_ui',
-#     'dfx/certificate',
-#     'dfx/certified_info',
-#     'dfx/config',
-#     'dfx/create',
-#     'dfx/dfx_install',
-#     'dfx/frontend',
-#     'dfx/id',
-#     'dfx/identity',
-#     'dfx/identity_command',
-#     'dfx/install',
-#     'dfx/leak',
-#     'dfx/network',
-#     'dfx/new',
-#     'dfx/packtool',
-#     'dfx/ping',
-#     'dfx/print',
-#     'dfx/provider',
-#     'dfx/request_status',
-#     'dfx/secp256k1',
-#     'dfx/sign_send',
-#     'dfx/signals',
-#     'dfx/start',
-#     'dfx/update_settings',
-#     'dfx/upgrade',
-#     'dfx/usage',
-#     'dfx/usage_env',
-#     'dfx/wallet',
-#     'replica/deploy',
-#     'replica/lifecycle'
-# ]
-
 matrix = {
     'test': test,
     'backend': [ 'ic-ref', 'replica' ],
