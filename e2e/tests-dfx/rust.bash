427aacfbd87f106495c54ccc3578b7f275f27778
Linwei Shang <linwei.shang@dfinity.org>
2021-11-16 11:22:10 -0800

    e2e

--- e2e/tests-dfx/rust.bash
+++ e2e/tests-dfx/rust.bash
@@ -18,7 +18,10 @@ teardown() {
     dfx_start
     dfx canister --no-wallet create --all
     assert_command dfx build hello
-    assert_match "Finished"
+    assert_match "`ic-cdk-optimizer` not installed"
+    cargo install ic-cdk-optimizer
+    assert_command dfx build hello
+    assert_match "Executing: ic-cdk-optimizer"
     assert_command dfx canister --no-wallet install hello
     assert_command dfx canister --no-wallet call hello greet dfinity
     assert_match '("Hello, dfinity!")'
