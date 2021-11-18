c6dd6c270ec9f14ef004b9df81a6df8a7b0a8044
Eric Swanson <eric.swanson@dfinity.org>
2021-11-18 13:34:59 -0800

    look up advisory-db rev from nix/sources.json

---
+++ scripts/workflows/audit-matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import json
+import glob
+import os
+
+def get_cargo_audit_ref():
+    with open('nix/sources.json') as json_file:
+        data = json.load(json_file)
+        return data[ 'advisory-db']['rev']
+
+matrix = {
+    'rust': [ '1.55.0' ],
+    'os': [ 'macos-latest', 'ubuntu-latest' ],
+    'cargo-audit': [ '0.15.2' ],
+    'advisory-db-rev': [ get_cargo_audit_ref() ]
+}
+
+print(json.dumps(matrix))
