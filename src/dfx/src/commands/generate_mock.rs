d6f87abecc6971cd5a44460e07086ca707b58862
Severin Siffert <severin.siffert@dfinity.org>
2022-02-08 09:18:47 +0100

    slight progress

--- src/dfx/src/commands/generate_mock.rs
+++ src/dfx/src/commands/generate_mock.rs
@@ -1,24 +1,42 @@
-use crate::lib::builders::BuildConfig;
 use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
-use crate::lib::models::canister::CanisterPool;
-use crate::lib::models::canister_id_store::CanisterIdStore;
+use crate::lib::models::canister::{Canister, CanisterPool};
 use crate::lib::provider::create_agent_environment;
 
 use clap::Clap;
 
-/// Generate stubs for remote canisters from their .did declarations
+/// Generate .mo stubs for remote canisters from their .did declarations
 #[derive(Clap)]
 pub struct GenerateMockOpts {
-    /// Specifies the name of the canister to build.
-    /// If you do not specify a canister name, generates mocks for all canisters that have no implementation yet.
+    /// Specifies the name of the canister to generate stubs for.
+    /// If you do not specify a canister name, generates mocks for all canisters that don't have a main file.
     canister_name: Option<String>,
+
+    /// Overwrite main .mo file if it already exists.
+    #[clap(long)]
+    overwrite: bool,
 }
 
 pub fn exec(env: &dyn Environment, opts: GenerateMockOpts) -> DfxResult {
     let env = create_agent_environment(env, None)?;
 
+    // Read the config.
+    let config = env.get_config_or_anyhow()?;
+
+    //Option can be None which means generate stubs for all canisters.
+    let canister_names = config.get_config().get_canister_names_with_dependencies(opts.canister_name.as_deref())?;
+    let canister_pool = CanisterPool::load(&env, false, &canister_names)?;
+
+    for canister in canister_pool.get_canister_list() {
+        println!("Canisters: {:?}", canister.canister_id());
+    }
+    // filter such that only canisters without main.mo are left
     todo!();
 
     Ok(())
+}
+
+fn qualifies_for_generating(canister: &Canister) -> bool {
+    false
+    //todo!
 }
