7d49aeadb6ad99dca566cbb16f4ac2f826093951
Severin Siffert <severin.siffert@dfinity.org>
2022-02-10 09:27:08 +0100

    add e2e tests

--- assets.nix
+++ assets.nix
@@ -10,6 +10,7 @@ let
   replica-bin = pkgs.sources."replica-${pkgs.system}";
   canister-sandbox-bin = pkgs.sources."canister-sandbox-${pkgs.system}";
   starter-bin = pkgs.sources."ic-starter-${pkgs.system}";
+  didc-bin = pkgs.sources."didc-${pkgs.system}";
   looseBinaryCache = pkgs.runCommandNoCCLocal "loose-binary-cache" {} ''
     mkdir -p $out
 
@@ -17,11 +18,11 @@ let
     gunzip <${canister-sandbox-bin} >$out/canister_sandbox
     gunzip <${starter-bin} >$out/ic-starter
     cp -R ${pkgs.sources.motoko-base}/src $out/base
+    cp ${didc-bin} $out/didc
     cp ${pkgs.motoko}/bin/mo-doc $out
     cp ${pkgs.motoko}/bin/mo-ide $out
     cp ${pkgs.motoko}/bin/moc $out
     cp ${pkgs.ic-ref}/bin/* $out
-    cp ${pkgs.didc}/bin/didc $out
     cp ${icx-proxy-standalone}/bin/icx-proxy $out
   '';
 in
