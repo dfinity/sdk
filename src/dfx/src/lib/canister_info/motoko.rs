9adf0f668610a8e60f53146bf3c43e97048b4986
Yan Chen <yan.chen@dfinity.org>
2022-02-17 08:36:32 -0800

    fmt

--- src/dfx/src/lib/canister_info/motoko.rs
+++ src/dfx/src/lib/canister_info/motoko.rs
@@ -76,7 +76,7 @@ impl CanisterInfoFactory for MotokoCanisterInfo {
         } else {
             output_wasm_path.with_extension("did")
         };
-        let output_stable_path = output_wasm_path.with_extension("most");            
+        let output_stable_path = output_wasm_path.with_extension("most");
         let output_did_js_path = output_wasm_path.with_extension("did.js");
         let output_canister_js_path = output_wasm_path.with_extension("js");
         let output_assets_root = output_root.join("assets");
