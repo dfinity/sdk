406a3a49287c3499eb71db56f713bb4fff3771eb
Severin Siffert <severin.siffert@dfinity.org>
2022-02-17 10:52:19 +0100

    fix e2e tests

--- e2e/tests-dfx/remote_generate_binding.bash
+++ e2e/tests-dfx/remote_generate_binding.bash
@@ -11,9 +11,9 @@ teardown() {
 }
 
 @test "generate-remote-binding succeeds on a simple remote example" {
-    install_asset generate_remote_binding/basic
+    install_asset remote_generate_binding/basic
 
-    assert_command dfx generate-remote-binding --all
+    assert_command dfx remote generate-binding --all
 
     if [[ ! -f remote.mo ]]; then
         echo "remote.mo not created"
@@ -22,13 +22,13 @@ teardown() {
 }
 
 @test "generate-remote-binding --overwrite behaves as expected" {
-    install_asset generate_remote_binding/basic
+    install_asset remote_generate_binding/basic
 
     echo "wrong" > remote.mo
-    assert_command dfx generate-remote-binding --all
+    assert_command dfx remote generate-binding --all
     assert_match "already exists"
 
-    assert_command dfx generate-remote-binding --all --overwrite
+    assert_command dfx remote generate-binding --all --overwrite
     assert_neq "wrong" "$(cat remote.mo)"
 
 }
