1961528ee3503592ac42e8f2fc658aa4f66622ff
Severin Siffert <severin.siffert@dfinity.org>
2022-02-21 09:31:40 +0100

    refactor e2e tests

--- e2e/tests-dfx/remote_generate_binding.bash
+++ e2e/tests-dfx/remote_generate_binding.bash
@@ -15,25 +15,10 @@ teardown() {
 
     assert_command dfx remote generate-binding --all
 
-    if [[ ! -f remote.mo ]]; then
-        echo "remote.mo not created"
-        exit 1
-    fi
-
-    if [[ ! -f remote.rs ]]; then
-        echo "remote.rs not created"
-        exit 1
-    fi
-
-    if [[ ! -f remote.ts ]]; then
-        echo "remote.ts not created"
-        exit 1
-    fi
-
-    if [[ ! -f remote.js ]]; then
-        echo "remote.js not created"
-        exit 1
-    fi
+    assert_file_exists remote.mo
+    assert_file_exists remote.rs
+    assert_file_exists remote.ts
+    assert_file_exists remote.js
 }
 
 @test "remote generate-binding --overwrite succeeds for --all" {
@@ -44,15 +29,8 @@ teardown() {
     assert_command dfx remote generate-binding --overwrite --all
 
     # should generate if not present
-    if [[ ! -f remote.ts ]]; then
-        echo "remote.ts not created"
-        exit 1
-    fi
-
-    if [[ ! -f remote.js ]]; then
-        echo "remote.js not created"
-        exit 1
-    fi
+    assert_file_exists remote.js
+    assert_file_exists remote.ts
 
     # should overwrite if already present
     assert_neq "to_overwrite" "$(cat remote.mo)"
@@ -67,15 +45,8 @@ teardown() {
     assert_command dfx remote generate-binding --all
 
     # should generate if not present
-    if [[ ! -f remote.ts ]]; then
-        echo "remote.ts not created"
-        exit 1
-    fi
-
-    if [[ ! -f remote.js ]]; then
-        echo "remote.js not created"
-        exit 1
-    fi
+    assert_file_exists remote.ts
+    assert_file_exists remote.js
 
     # should not overwrite if already present
     assert_eq "to_overwrite" "$(cat remote.mo)"
@@ -87,25 +58,10 @@ teardown() {
 
     assert_command dfx remote generate-binding remote-rust
 
-    if [[ ! -f remote.rs ]]; then 
-        echo "remote.rs not created"
-        exit 1
-    fi
-
-    if [[ -f remote.mo ]]; then 
-        echo "remote.mo created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.ts ]]; then
-        echo "remote.ts created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.js ]]; then
-        echo "remote.js created when it shouldn't"
-        exit 1
-    fi
+    assert_file_exists remote.rs
+    assert_file_not_exists remote.mo
+    assert_file_not_exists remote.ts
+    assert_file_not_exists remote.js
 }
 
 @test "remote generate-binding succeeds for specific motoko canister" {
@@ -113,25 +69,10 @@ teardown() {
 
     assert_command dfx remote generate-binding remote-motoko
 
-    if [[ ! -f remote.mo ]]; then 
-        echo "remote.mo not created"
-        exit 1
-    fi
-
-    if [[ -f remote.rs ]]; then
-        echo "remote.rs created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.ts ]]; then
-        echo "remote.ts created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.js ]]; then
-        echo "remote.js created when it shouldn't"
-        exit 1
-    fi
+    assert_file_exists remote.mo
+    assert_file_not_exists remote.rs
+    assert_file_not_exists remote.ts
+    assert_file_not_exists remote.js
 }
 
 @test "remote generate-binding succeeds for specific javascript canister" {
@@ -139,25 +80,10 @@ teardown() {
 
     assert_command dfx remote generate-binding remote-javascript
 
-    if [[ ! -f remote.js ]]; then 
-        echo "remote.js not created"
-        exit 1
-    fi
-
-    if [[ -f remote.mo ]]; then 
-        echo "remote.mo created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.rs ]]; then
-        echo "remote.rs created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.ts ]]; then
-        echo "remote.ts created when it shouldn't"
-        exit 1
-    fi
+    assert_file_exists remote.js
+    assert_file_not_exists remote.mo
+    assert_file_not_exists remote.rs
+    assert_file_not_exists remote.ts
 }
 
 @test "remote generate-binding succeeds for specific typescript canister" {
@@ -165,25 +91,10 @@ teardown() {
 
     assert_command dfx remote generate-binding remote-typescript
 
-    if [[ ! -f remote.ts ]]; then 
-        echo "remote.ts not created"
-        exit 1
-    fi
-
-    if [[ -f remote.mo ]]; then 
-        echo "remote.mo created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.rs ]]; then
-        echo "remote.rs created when it shouldn't"
-        exit 1
-    fi
-
-    if [[ -f remote.js ]]; then
-        echo "remote.js created when it shouldn't"
-        exit 1
-    fi
+    assert_file_exists remote.ts
+    assert_file_not_exists remote.mo
+    assert_file_not_exists remote.rs
+    assert_file_not_exists remote.js
 }
 
 @test "remote generate-binding --overwrite succeeds for specific canister" {
