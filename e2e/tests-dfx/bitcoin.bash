3898a4e4112904e46d2aaa0844c89bfc46be8dd6
Eric Swanson <eric.swanson@dfinity.org>
2022-03-08 14:53:04 -0800

    test: provision e2e tests for bitcoin testing

---
+++ e2e/tests-dfx/bitcoin.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+    standard_setup
+
+    bitcoind -regtest -daemonwait
+}
+
+teardown() {
+    bitcoin-cli -regtest stop
+
+    standard_teardown
+}
+
+@test "noop" {
+    assert_command bitcoin-cli -regtest createwallet "test"
+    ADDRESS="$(bitcoin-cli -regtest getnewaddress)"
+    assert_command bitcoin-cli generatetoaddress 101 "$ADDRESS"
+}
--- scripts/workflows/provision-linux.sh
+++ scripts/workflows/provision-linux.sh
@@ -21,6 +21,17 @@ rm v$version.tar.gz
 if [ "$E2E_TEST" = "tests-dfx/certificate.bash" ]; then
     sudo apt-get install --yes mitmproxy
 fi
+if [ "$E2E_TEST" = "tests-dfx/bitcoin.bash" ]; then
+    BITCOIN_CORE_VERSION=22.0
+    (
+        cd "$(mktemp -d)"
+        BTC_FILENAME="bitcoin-core-$BITCOIN_CORE_VERSION/bitcoin-$BITCOIN_CORE_VERSION-x86_64-linux-gnu.tar.gz"
+        wget "https://bitcoin.org/bin/$BTC_FILENAME"
+        tar xzf "$BTC_FILENAME"
+        cd "bitcoin-$BITCOIN_CORE_VERSION/bin"
+        sudo install -m 0755 -o root -g root -t /usr/local/bin *
+    )
+fi
 
 # Set environment variables.
 BATS_SUPPORT="/usr/local/lib/bats-support"
