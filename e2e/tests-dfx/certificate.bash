d50496a65d477dc1a7cc2cdf25a312109e4d2309
Severin Siffert <severin.siffert@dfinity.org>
2022-03-25 10:55:25 +0100

    see if this retries to start mitmproxy

--- e2e/tests-dfx/certificate.bash
+++ e2e/tests-dfx/certificate.bash
@@ -14,15 +14,6 @@ setup() {
 
     BACKEND="$(jq -r .networks.local.bind dfx.json)"
 
-    # In github workflows, at the time of this writing, we get:
-    #     macos-latest: mitmproxy 7.0.4
-    #     ubuntu-latest: mitmproxy 4.x
-    if [ "$(mitmdump --version | grep Mitmproxy | cut -d ' ' -f 2 | cut -c 1-2)" = "4." ]; then
-        MODIFY_BODY_ARG="--replacements"
-    else
-        MODIFY_BODY_ARG="--modify-body"
-    fi
-
     # Sometimes, something goes wrong with mitmdump's initialization.
     # It reports that it is listening, and the `nc` call succeeds,
     # but it does not actually respond to requests.
@@ -40,14 +31,14 @@ setup() {
         # shellcheck disable=SC2094
         cat <<<"$(jq '.networks.local.bind="127.0.0.1:'"$MITM_PORT"'"' dfx.json)" >dfx.json
 
-        mitmdump -p "$MITM_PORT" --mode "reverse:http://$BACKEND"  "$MODIFY_BODY_ARG" '/~s/Hello,/Hullo,' &
+        mitmdump -p "$MITM_PORT" --mode "reverse:http://$BACKEND"  "--modify-body" '/~s/Hello,/Hullo,' &
         MITMDUMP_PID=$!
 
         timeout 5 sh -c \
             "until nc -z localhost $MITM_PORT; do echo waiting for mitmdump; sleep 1; done" \
             || (echo "mitmdump did not start on port $MITM_PORT" && exit 1)
 
-        if timeout 10 dfx ping; then
+        if nc -z localhost $MITM_PORT; then
             break
         fi
 
