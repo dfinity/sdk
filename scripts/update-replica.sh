40ec29542e96daf58c1ee0c817e393c9b067e92a
Severin Siffert <severin.siffert@dfinity.org>
2022-03-31 18:42:07 +0200

    chore: update to latest blessed replica (#2111)
    Also add update-replica.sh script

---
+++ scripts/update-replica.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+#   $1 not set   ||   not running in repo root
+if [ -z ${1+x} ] || [ ! -f ./scripts/write-dfx-asset-sources.sh ]; then
+    echo "Usage: run ./scripts/update-replica.sh <SHA-to-update-to> in repo root"
+    exit 1
+fi
+
+SHA=$1
+echo "Updating sources to rev ${SHA}"
+niv update ic-starter-x86_64-darwin -a rev=$SHA
+niv update ic-starter-x86_64-linux -a rev=$SHA
+niv update replica-x86_64-darwin -a rev=$SHA
+niv update replica-x86_64-linux -a rev=$SHA
+niv update canister-sandbox-x86_64-darwin -a rev=$SHA
+niv update canister-sandbox-x86_64-linux -a rev=$SHA
+
+echo "Writing asset sources"
+./scripts/write-dfx-asset-sources.sh
+
+echo "Done. Don't forget to update CHANGELOG.adoc"
