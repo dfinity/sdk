ef7988dd82b6e3037212d3c8c7964c14f37fa235
Kyle Peacock <kylpeacock@gmail.com>
2022-04-19 14:31:07 -0700

    chore: updates config and templates for node 18 compatibility

--- src/dfx/assets/new_project_node_files/webpack.config.js
+++ src/dfx/assets/new_project_node_files/webpack.config.js
@@ -107,7 +107,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "/api",
