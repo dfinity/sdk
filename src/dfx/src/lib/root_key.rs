3150d8624bc617fc820f75f9d7715c045dd55f0c
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2022-05-23 12:16:06 -0700

    refactor: Environment.get_network_descriptor() returns a ref, not an option (#2220)
    # Description
    
    This eliminates a bunch of unwrap() and expect() calls that can never trigger, because the Environment in question is always an AgentEnvironment anyway.  In practice, Environment::get_network_descriptor can never be called, so it now calls `unreachable!()`.
    
    This is preliminary work for system-wide dfx start
    
    # How Has This Been Tested?
    
    e2e tests will cover it
    
    # Checklist:
    
    - [x] The title of this PR complies with [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] (not needed) I have edited the CHANGELOG accordingly.
    - [x] (not needed) I have made corresponding changes to the documentation.

--- src/dfx/src/lib/root_key.rs
+++ src/dfx/src/lib/root_key.rs
@@ -10,11 +10,7 @@ pub async fn fetch_root_key_if_needed(env: &dyn Environment) -> DfxResult {
         .get_agent()
         .ok_or_else(|| anyhow!("Cannot get HTTP client from environment."))?;
 
-    if !env
-        .get_network_descriptor()
-        .expect("no network descriptor")
-        .is_ic
-    {
+    if !env.get_network_descriptor().is_ic {
         agent
             .fetch_root_key()
             .await
@@ -31,11 +27,7 @@ pub async fn fetch_root_key_or_anyhow(env: &dyn Environment) -> DfxResult {
         .get_agent()
         .ok_or_else(|| anyhow!("Cannot get HTTP client from environment."))?;
 
-    if !env
-        .get_network_descriptor()
-        .expect("no network descriptor")
-        .is_ic
-    {
+    if !env.get_network_descriptor().is_ic {
         agent
             .fetch_root_key()
             .await
