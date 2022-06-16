364b46d36bf9908d49684225c4ac7d7e98ed3e42
Adam Spofford <adam.spofford@dfinity.org>
2022-06-16 12:52:59 -0700

    sigh

--- src/dfx/src/lib/sign/signed_message.rs
+++ src/dfx/src/lib/sign/signed_message.rs
@@ -180,4 +180,20 @@ impl SignedMessageV1 {
     }
 }
 
-time::serde::format_description!(date_time_utc, OffsetDateTime, "[year repr:full padding:zero]-[month repr:numerical padding:zero]-[day padding:zero] [hour repr:24 padding:zero]:[minute padding:zero]:[second padding:zero] UTC");
+mod date_time_utc {
+    time::serde::format_description!(date_time, PrimitiveDateTime, "[year repr:full padding:zero]-[month repr:numerical padding:zero]-[day padding:zero] [hour repr:24 padding:zero]:[minute padding:zero]:[second padding:zero] UTC");
+    
+    use serde::{Serializer, Deserializer};
+    use time::{OffsetDateTime, PrimitiveDateTime, UtcOffset};
+
+    pub fn serialize<S: Serializer>(datetime: &OffsetDateTime, s: S) -> Result<S::Ok, S::Error> {
+        let utc = datetime.to_offset(UtcOffset::UTC);
+        let date = utc.date();
+        let time = utc.time();
+        date_time::serialize(&PrimitiveDateTime::new(date, time), s)
+    }
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<OffsetDateTime, D::Error> {
+        let primitive = date_time::deserialize(d)?;
+        Ok(primitive.assume_utc())
+    }
+}
