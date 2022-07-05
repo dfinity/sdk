10ea85ac17226a91b7d97f92d6cd6d8ca80413ea
ielashi <ielashi@users.noreply.github.com>
2022-07-05 17:13:17 +0000

    Deploying to public-manifest from  @ 733f7da1677b01501ac14c3a858a8a7946c73da2 🚀

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="07c7afee087e863bbffe1888791516f632bec491"
+SCRIPT_COMMIT_DESC="733f7da1677b01501ac14c3a858a8a7946c73da2"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
