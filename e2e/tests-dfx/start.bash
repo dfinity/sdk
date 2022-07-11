b2e426f34b1947bd174f409bc748ee1d5c7ee662
Severin Siffert <severin.siffert@dfinity.org>
2022-07-11 10:49:03 +0200

    more fixes

--- e2e/tests-dfx/start.bash
+++ e2e/tests-dfx/start.bash
@@ -96,7 +96,7 @@ teardown() {
 
     install_asset greet
     assert_command dfx deploy
-    assert_command dfx canister call hello greet '("Alpha")'
+    assert_command dfx canister call hello_backend greet '("Alpha")'
     assert_eq '("Hello, Alpha!")'
 
     REPLICA_PID=$(get_replica_pid)
@@ -118,10 +118,10 @@ teardown() {
     #     IC0304: Attempt to execute a message on canister <>> which contains no Wasm module
     # but the condition clears.
     timeout 30s sh -c \
-      "until dfx canister call hello greet '(\"wait\")'; do echo waiting for any canister call to succeed; sleep 1; done" \
+      "until dfx canister call hello_backend greet '(\"wait\")'; do echo waiting for any canister call to succeed; sleep 1; done" \
       || (echo "canister call did not succeed") # but continue, for better error reporting
 
-    assert_command dfx canister call hello greet '("Omega")'
+    assert_command dfx canister call hello_backend greet '("Omega")'
     assert_eq '("Hello, Omega!")'
 
     ID=$(dfx canister id hello_frontend)
