42ce32255116ca607dd590b43bc81ad97386a7b9
Adam Spofford <adam.spofford@dfinity.org>
2022-07-12 08:52:31 -0700

    the whole patch is unnecessary

--- e2e/assets/canister_args/patch.bash
+++ e2e/assets/canister_args/patch.bash
@@ -1,16 +1,4 @@
 #!/dev/null
 
-patch dfx.json <<EOF
-@@ -4,7 +4,7 @@
-   "canisters": {
-     "e2e_project_backend": {
-       "type": "motoko",
--      "main": "src/e2e_project_backend/main.mo"
-+      "main": "src/e2e_project_backend/main.mo",
-     },
-     "e2e_project_frontend": {
-       "type": "assets",
-EOF
-
 cat <<<"$(jq '.canisters.e2e_project_backend.args="--compacting-gcY"' dfx.json)" >dfx.json
 cat <<<"$(jq '.defaults.build.args="--compacting-gcX"' dfx.json)" >dfx.json
