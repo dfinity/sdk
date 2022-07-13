08747eaeeda786e8742f8feaa4ff5c7e939fe0bf
Adam Spofford <adam.spofford@dfinity.org>
2022-07-13 06:54:17 -0700

    sigh

--- e2e/utils/_.bash
+++ e2e/utils/_.bash
@@ -7,7 +7,7 @@ install_asset() {
     cp -R $ASSET_ROOT/* .
 
     [ -f ./patch.bash ] && source ./patch.bash
-    [ -f ./Cargo.toml ] && cargo update
+    [ ! -f ./Cargo.toml ] || cargo update
 }
 
 standard_setup() {
