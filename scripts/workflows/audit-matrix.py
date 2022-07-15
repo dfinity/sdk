565838a1a77c6595e3ade41efc215c5f6678814c
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-07-15 14:02:28 +0200

    unlint

--- scripts/workflows/audit-matrix.py
+++ scripts/workflows/audit-matrix.py
@@ -4,16 +4,15 @@ import json
 
 
 def get_cargo_audit_ref():
-    with open("nix/sources.json") as json_file:
+    with open('nix/sources.json') as json_file:
         data = json.load(json_file)
-        return data["advisory-db"]["rev"]
-
+        return data[ 'advisory-db']['rev']
 
 matrix = {
-    "rust": ["1.60.0"],
-    "os": ["macos-latest", "ubuntu-latest"],
-    "cargo-audit": ["0.15.2"],
-    "advisory-db-rev": [get_cargo_audit_ref()],
+    'rust': [ '1.60.0' ],
+    'os': [ 'macos-latest', 'ubuntu-latest' ],
+    'cargo-audit': [ '0.15.2' ],
+    'advisory-db-rev': [ get_cargo_audit_ref() ]
 }
 
 print(json.dumps(matrix))
