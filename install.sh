1596ebf6c4545ae44dc98e3c6979834ddc4b5213
smallstepman <marcin.liebiediew@dfinity.org>
2022-07-18 18:58:36 +0000

    Deploying to public-manifest from  @ 71906dc9ae6d74ba783415119d68d1604afb2c54 🚀

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="13340381e59fc39fd9c7d4a233b2a06dc8570140"
+SCRIPT_COMMIT_DESC="71906dc9ae6d74ba783415119d68d1604afb2c54"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
