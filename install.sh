0cd84c70d2b34a089658741dd7ed68dc90150557
MurielPinho <Muriel.apinho@gmail.com>
2022-07-20 15:35:29 +0000

    Deploying to public-manifest from  @ 4c646b69a32e032caa65658e98e38f80b6cf7647 🚀

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="6f41f253a1271b75539eb2c2dc69dbb048c370af"
+SCRIPT_COMMIT_DESC="4c646b69a32e032caa65658e98e38f80b6cf7647"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
