10228c005b2b20703b4abc3d438019190d2887de
rvanasa <ryanvandersmith@gmail.com>
2022-07-26 20:33:31 +0000

    Deploying to public-manifest from  @ 18627d4fbb3463832c1887c024525adab8472a31 🚀

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="5e72d2126d2bf6d3c93c9f9bcc146d19053ffe24"
+SCRIPT_COMMIT_DESC="18627d4fbb3463832c1887c024525adab8472a31"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
