6529e35bbb5033297d0ccea90b2a7a5f02f27233
Eric Swanson <eric.swanson@dfinity.org>
2022-08-10 15:56:33 -0700

    fix ledger test - define shared local network as system subnet type

--- e2e/tests-dfx/ledger.bash
+++ e2e/tests-dfx/ledger.bash
@@ -6,6 +6,7 @@ setup() {
     [ "$USE_IC_REF" ] && skip "skipped for ic-ref"
     standard_setup
     install_asset ledger
+    install_shared_asset ledger_shared
 
     dfx identity import --disable-encryption alice alice.pem
     dfx identity import --disable-encryption bob bob.pem
