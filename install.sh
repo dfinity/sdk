3a11bed908d4bfde37f1dc24549ff03ec5e2392d
krpeacock <kylpeacock@gmail.com>
2022-08-12 20:39:54 +0000

    Deploying to public-manifest from  @ 9e3694d39c4ff8792601e28935dc779df24a60cb 🚀

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="cd8e1e915d751926bf6079f58d6e63577bb24552"
+SCRIPT_COMMIT_DESC="9e3694d39c4ff8792601e28935dc779df24a60cb"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
