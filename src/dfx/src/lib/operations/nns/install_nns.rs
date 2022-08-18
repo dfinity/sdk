395fdeba0ffe10cc2521752eab253a0219acfe58
Max <max@dfinity.org>
2022-08-18 04:08:33 +0200

    Verify that the local replica type is system

--- src/dfx/src/lib/operations/nns/install_nns.rs
+++ src/dfx/src/lib/operations/nns/install_nns.rs
@@ -1,4 +1,5 @@
-use crate::DfxResult;
+use crate::{DfxResult};
+use crate::config::dfinity::{Config, ConfigNetwork, ReplicaSubnetType};
 
 use anyhow::{bail, Context};
 use fn_error_context::context;
@@ -29,4 +30,26 @@ pub async fn install_nns(
         bail!("ic-nns-init call failed");
     }
     Ok(())
+}
+
+/// Gets the local replica type.
+fn local_replica_type() -> Result<ReplicaSubnetType, &'static str> {
+	let dfx_config = Config::from_current_dir().map_err(|_| "Could not get config from dfx.json.")?.ok_or("No config in dfx.json")?;
+    let network = dfx_config.get_config().get_network("local").ok_or("'local' network is not defined in dfx.json.")?;
+    let local_network = if let ConfigNetwork::ConfigLocalProvider(local_network) = network {
+            local_network
+        } else {
+            return Err("In dfx.json, 'local' is not a local provider.");
+        };
+    let local_replica_config = local_network.replica.as_ref().expect("In dfx.json, 'local' network has no replica setting.");
+    local_replica_config.subnet_type.ok_or("Replica type is not defined for 'local' network.")
+}
+/// Asserts that the local replica type is 'system'.
+/// Note: At present dfx runs a single local replica and the replica type is taken from dfx.json.  It is unfortunate that the subnet type is forced
+/// on the other canisters, however in practice this is unlikely to be a huge problem in the short term.
+fn assert_local_replica_type_is_system() {
+    match local_replica_type() {
+        Ok(ReplicaSubnetType::System) => (),
+        other => panic!("In dfx.json networks.local.replica.subnet_type needs to be \"system\" to run NNS canisters.  Current value: {:?}", other),
+    }   
 }
