82b898b2ef8f59f98bba8f7d7eea351b73acd0eb
Max <max@dfinity.org>
2022-08-18 13:56:24 +0200

    ++

--- src/dfx/src/lib/operations/nns/install_nns.rs
+++ src/dfx/src/lib/operations/nns/install_nns.rs
@@ -35,16 +35,19 @@ pub async fn install_nns(
     }
     */
     assert_local_replica_type_is_system();
-    
+
     download_nns_wasms().await.unwrap();
-    /*
     let ic_nns_init_opts = IcNnsInitOpts {
         wasm_dir: NNS_WASM_DIR.to_string(),
-        nns_url: provider_url.to_string(),
-        test_accounts: Some("5b315d2f6702cb3a27d826161797d7b2c2e131cd312aece51d4d5574d1247087".to_string()),
+        nns_url: get_replica_url().unwrap(),
+        test_accounts: Some(
+            "5b315d2f6702cb3a27d826161797d7b2c2e131cd312aece51d4d5574d1247087".to_string(),
+        ),
     };
-    ic_nns_init(ic_nns_init_path, &ic_nns_init_opts).await.unwrap();
-    */
+    ic_nns_init(ic_nns_init_path, &ic_nns_init_opts)
+        .await
+        .unwrap();
+
     Ok(())
 }
 
@@ -82,24 +85,30 @@ pub fn assert_local_replica_type_is_system() {
 
 const NNS_WASM_DIR: &'static str = "wasm/nns";
 /// Downloads wasm file
-pub async fn download_wasm(wasm_name: &str, ic_commit: &str, wasm_dir: &str) -> anyhow::Result<()> {
+pub async fn download_wasm(
+    target_name: &str,
+    src_name: &str,
+    ic_commit: &str,
+    wasm_dir: &str,
+) -> anyhow::Result<()> {
     fs::create_dir_all(wasm_dir)?;
-    let final_path = Path::new(wasm_dir).join(format!("{wasm_name}.wasm"));
+    let final_path = Path::new(wasm_dir).join(format!("{target_name}.wasm"));
+    println!("{final_path:?}");
     if final_path.exists() {
         return Ok(());
     }
 
     let url_str =
-        format!("https://download.dfinity.systems/ic/{ic_commit}/canisters/{wasm_name}.wasm.gz");
+        format!("https://download.dfinity.systems/ic/{ic_commit}/canisters/{src_name}.wasm.gz");
     let url = reqwest::Url::parse(&url_str)?;
     let mut response = reqwest::get(url.clone()).await?.bytes().await?;
     let mut decoder = Decoder::new(&response[..])?;
     let mut buffer = Vec::new();
     decoder.read_to_end(&mut buffer).unwrap();
 
-    let tmp_dir = tempfile::Builder::new().prefix(wasm_name).tempdir()?;
+    let tmp_dir = tempfile::Builder::new().prefix(target_name).tempdir()?;
     let downloaded_filename = {
-        let filename = tmp_dir.path().join(wasm_name);
+        let filename = tmp_dir.path().join(target_name);
         let mut file = fs::File::create(&filename)?;
         file.write_all(&buffer);
         filename
@@ -109,10 +118,31 @@ pub async fn download_wasm(wasm_name: &str, ic_commit: &str, wasm_dir: &str) -> 
 }
 pub async fn download_nns_wasms() -> anyhow::Result<()> {
     let ic_commit = "3982db093a87e90cbe0595877a4110e4f37ac740"; // TODO: Where should this commit come from?
-    for wasm_name in ["registry-canister", "governance-canister_test", "governance-canister_test", "ledger-canister_notify-method", "root-canister", "cycles-minting-canister", "lifeline", "sns-wasm-canister", "genesis-token-canister", "identity-canister", "nns-ui-canister"] {
-      download_wasm(wasm_name, ic_commit, NNS_WASM_DIR).await.unwrap();
+    for (src_name, target_name) in [
+        ("registry-canister", "registry-canister"),
+        ("governance-canister", "governance-canister_test"),
+        ("ledger-canister", "ledger-canister_notify-method"),
+        ("ic-icrc1-ledger", "ic-icrc1-ledger"),
+        ("root-canister", "root-canister"),
+        ("cycles-minting-canister", "cycles-minting-canister"),
+        ("lifeline", "lifeline"),
+        ("sns-wasm-canister", "sns-wasm-canister"),
+        ("genesis-token-canister", "genesis-token-canister"),
+        ("identity-canister", "identity-canister"),
+        ("nns-ui-canister", "nns-ui-canister"),
+    ] {
+        download_wasm(src_name, target_name, ic_commit, NNS_WASM_DIR)
+            .await
+            .unwrap();
     }
     Ok(())
+}
+
+/// get the replica URL.  Note: This is not the same as the provider URL.
+fn get_replica_url() -> Result<String, io::Error> {
+    let port = fs::read_to_string(".dfx/replica-configuration/replica-1.port")
+        .map(|string| string.trim().to_string())?;
+    Ok(format!("http://localhost:{port}"))
 }
 
 /// Arguments for the ic-nns-init command line function.
