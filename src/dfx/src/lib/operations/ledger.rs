e594b43af64164530ae24757d69f3a6525f3a8ba
Adam Spofford <adam.spofford@dfinity.org>
2022-08-19 17:11:58 -0400

    Verify cert

--- src/dfx/src/lib/operations/ledger.rs
+++ src/dfx/src/lib/operations/ledger.rs
@@ -1,6 +1,8 @@
-use candid::Principal;
-use ic_agent::Agent;
+use anyhow::{bail, ensure, Context};
+use candid::{Encode, Principal};
+use ic_agent::{hash_tree::LookupResult, ic_types::HashTree, lookup_value, Agent};
 use ic_utils::{call::SyncCall, Canister};
+use openssl::sha::sha256;
 
 use crate::lib::{
     error::DfxResult,
@@ -45,6 +47,41 @@ pub async fn xdr_permyriad_per_icp(agent: &Agent) -> DfxResult<u64> {
         .build()
         .call()
         .await?;
-    //todo check certificate
+    // check certificate, this is a query call
+    let cert = serde_cbor::from_slice(&certified_rate.certificate)?;
+    agent
+        .verify(&cert, MAINNET_CYCLE_MINTER_CANISTER_ID, false)
+        .context(
+            "The origin of the certificate for the XDR <> ICP exchange rate could not be verified",
+        )?;
+    // we can trust the certificate
+    let witness = lookup_value(
+        &cert,
+        [
+            "canister".into(),
+            MAINNET_CYCLE_MINTER_CANISTER_ID.into(),
+            "certified_data".into(),
+        ],
+    )
+    .context("The IC's certificate for the XDR <> ICP exchange rate could not be verified")?;
+    let tree = serde_cbor::from_slice::<HashTree>(&certified_rate.hash_tree)?;
+    ensure!(
+        tree.digest() == witness,
+        "The CMC's certificate for the XDR <> ICP exchange rate did not match the IC's certificate"
+    );
+    // we can trust the hash tree
+    let lookup = tree.lookup_path([&"ICP_XDR_CONVERSION_RATE".into()]);
+    let certified_hash = if let LookupResult::Found(hash) = lookup {
+        hash
+    } else {
+        bail!("The CMC's certificate did not contain the XDR <> ICP exchange rate");
+    };
+    let encoded_data = Encode!(&certified_rate.data)?;
+    let expected_hash = sha256(&encoded_data);
+    ensure!(
+        certified_hash == expected_hash,
+        "The CMC's certificate for the XDR <> ICP exchange rate did not match the provided rate"
+    );
+    // we can trust the exchange rate
     Ok(certified_rate.data.xdr_permyriad_per_icp)
 }
