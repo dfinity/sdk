62688e9d8cd9a270b9131417b8599c35d0d6fa5a
Adam Spofford <adam.spofford@dfinity.org>
2022-08-22 12:15:40 -0400

    Squashed commit of the following:
    commit ed42db16db16c8e654ca0ccedc03916d1f86eae1
    Author: Adam Spofford <adam.spofford@dfinity.org>
    Date:   Mon Aug 22 12:04:52 2022 -0400
    
        It was right there the whole time
    
    commit 74d31f948a89e2635d9dd913281e21645a58679c
    Author: Adam Spofford <adam.spofford@dfinity.org>
    Date:   Mon Aug 22 11:13:26 2022 -0400
    
        hash the domain separator
    
    commit e594b43af64164530ae24757d69f3a6525f3a8ba
    Author: Adam Spofford <adam.spofford@dfinity.org>
    Date:   Fri Aug 19 17:11:58 2022 -0400
    
        Verify cert

--- src/dfx/src/lib/operations/ledger.rs
+++ src/dfx/src/lib/operations/ledger.rs
@@ -1,18 +1,15 @@
-use candid::Principal;
-use ic_agent::Agent;
+use anyhow::{bail, ensure, Context};
+use candid::{Encode, Principal};
+use ic_agent::{hash_tree::LookupResult, ic_types::HashTree, lookup_value, Agent};
 use ic_utils::{call::SyncCall, Canister};
 
-use crate::{
-    lib::{
-        error::DfxResult,
-        ledger_types::{
-            AccountBalanceArgs, IcpXdrConversionRateCertifiedResponse,
-            MAINNET_CYCLE_MINTER_CANISTER_ID, MAINNET_LEDGER_CANISTER_ID,
-        },
-        nns_types::{account_identifier::AccountIdentifier, icpts::ICPTs},
-        waiter::waiter_with_timeout,
+use crate::lib::{
+    error::DfxResult,
+    ledger_types::{
+        AccountBalanceArgs, IcpXdrConversionRateCertifiedResponse,
+        MAINNET_CYCLE_MINTER_CANISTER_ID, MAINNET_LEDGER_CANISTER_ID,
     },
-    util::expiry_duration,
+    nns_types::{account_identifier::AccountIdentifier, icpts::ICPTs},
 };
 
 const ACCOUNT_BALANCE_METHOD: &str = "account_balance_dfx";
@@ -45,10 +42,44 @@ pub async fn xdr_permyriad_per_icp(agent: &Agent) -> DfxResult<u64> {
         .with_canister_id(MAINNET_CYCLE_MINTER_CANISTER_ID)
         .build()?;
     let (certified_rate,): (IcpXdrConversionRateCertifiedResponse,) = canister
-        .update_("get_icp_xdr_conversion_rate")
+        .query_("get_icp_xdr_conversion_rate")
         .build()
-        .call_and_wait(waiter_with_timeout(expiry_duration()))
+        .call()
         .await?;
-    //todo check certificate so this can be a query call
+    // check certificate, this is a query call
+    let cert = serde_cbor::from_slice(&certified_rate.certificate)?;
+    agent
+        .verify(&cert, MAINNET_CYCLE_MINTER_CANISTER_ID, false)
+        .context(
+            "The origin of the certificate for the XDR <> ICP exchange rate could not be verified",
+        )?;
+    // we can trust the certificate
+    let witness = lookup_value(
+        &cert,
+        [
+            "canister".into(),
+            MAINNET_CYCLE_MINTER_CANISTER_ID.into(),
+            "certified_data".into(),
+        ],
+    )
+    .context("The IC's certificate for the XDR <> ICP exchange rate could not be verified")?;
+    let tree = serde_cbor::from_slice::<HashTree>(&certified_rate.hash_tree)?;
+    ensure!(
+        tree.digest() == witness,
+        "The CMC's certificate for the XDR <> ICP exchange rate did not match the IC's certificate"
+    );
+    // we can trust the hash tree
+    let lookup = tree.lookup_path([&"ICP_XDR_CONVERSION_RATE".into()]);
+    let certified_data = if let LookupResult::Found(hash) = lookup {
+        hash
+    } else {
+        bail!("The CMC's certificate did not contain the XDR <> ICP exchange rate");
+    };
+    let encoded_data = Encode!(&certified_rate.data)?;
+    ensure!(
+        certified_data == encoded_data,
+        "The CMC's certificate for the XDR <> ICP exchange rate did not match the provided rate"
+    );
+    // we can trust the exchange rate
     Ok(certified_rate.data.xdr_permyriad_per_icp)
 }
