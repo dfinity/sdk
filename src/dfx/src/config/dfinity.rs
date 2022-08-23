51174117c27c176f1e8d83f478031fddca8b5657
Max <max@dfinity.org>
2022-08-23 10:55:40 +0200

    This is enough for now

--- src/dfx/src/config/dfinity.rs
+++ src/dfx/src/config/dfinity.rs
@@ -568,10 +568,7 @@ impl ConfigInterface {
         network
     )]
     pub fn is_remote_canister(&self, canister: &str, network: &str) -> DfxResult<bool> {
-        Ok(self
-            .get_remote_canister_id(canister, network)
-            .unwrap_or_default()
-            .is_some())
+        Ok(self.get_remote_canister_id(canister, network)?.is_some())
     }
 
     #[context("Failed to get compute allocation for '{}'.", canister_name)]
