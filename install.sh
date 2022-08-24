f9125bc2fed67b6c912bd34563e37c9bdeeeb889
bitdivine <bitdivine@users.noreply.github.com>
2022-08-24 02:59:12 +0000

    Deploying to public-manifest from  @ b4f04a8645fb339d95d3527254f94b99ef21f1fb 🚀

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="9c603b02cf1c0e58036bafc806ed1efd05550c94"
+SCRIPT_COMMIT_DESC="b4f04a8645fb339d95d3527254f94b99ef21f1fb"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
