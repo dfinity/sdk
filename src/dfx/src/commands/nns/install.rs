7c98a2f678537b729a53e7dccf926284d0a1917e
Max <max@dfinity.org>
2022-08-26 06:40:05 +0200

    help++

--- src/dfx/src/commands/nns/install.rs
+++ src/dfx/src/commands/nns/install.rs
@@ -5,8 +5,21 @@ use crate::lib::nns::install_nns::{get_replica_url, get_with_retries, install_nn
 use crate::lib::root_key::fetch_root_key_if_needed;
 use clap::Parser;
 
-/// Installs the nns canisters
+/// Installs the NNS canisters, Internet Identity and the NNS frontend dapp
+/// 
+/// - The core network nervous system canisters are nns-registry, nns-governance, nns-ledger, nns-root, nns-cycles-minting,
+///   nns-lifeline, nns-genesis-token and nns-sns-wasm.
+///   Source code is at <https://github.com/dfinity/ic/tree/master/rs/nns#network-nervous-system-nns>.
+/// 
+/// 
+/// - internet_identity is a login service.
+///   Source code is at <https://github.com/dfinity/internet-identity>.
+///   This frontend is typically served at: <http://qaa6y-5yaaa-aaaaa-aaafa-cai.localhost:8080>.
+/// 
+/// - nns-dapp is a voting app and wallet. Source code is at <https://github.com/dfinity/nns-dapp>.
+///   This frontend is typically served at: <http://qhbym-qaaaa-aaaaa-aaafq-cai.localhost:8080>.
 #[derive(Parser)]
+#[clap(about)]
 pub struct InstallOpts {}
 
 pub async fn exec(env: &dyn Environment, _opts: InstallOpts) -> DfxResult {
