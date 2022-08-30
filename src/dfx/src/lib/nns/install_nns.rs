606aeb1e988edf218fb63b48963970256a8a04db
Max <max@dfinity.org>
2022-08-30 10:59:12 +0200

    Pretty printout

--- src/dfx/src/lib/nns/install_nns.rs
+++ src/dfx/src/lib/nns/install_nns.rs
@@ -31,190 +31,10 @@ use std::process::{self, Command};
 use std::time::Duration;
 
 use self::canisters::{
-    IcNnsInitCanister, SnsCanisterInstallation, StandardCanister, NNS_CORE, SNS_CANISTERS,
+    IcNnsInitCanister, SnsCanisterInstallation, StandardCanister, NNS_CORE, SNS_CANISTERS, NNS_FRONTEND
 };
 
-pub mod canisters {
-    //! Information about canisters deployed via `ic nns install`.
-
-    /// Configuration for an NNS canister installation as performed by `ic-nns-init`.
-    ///
-    /// Note: Other deployment methods may well use different settings.
-    pub struct IcNnsInitCanister {
-        /// The name of the canister as typically entered in dfx.json or used in `dfx canister id NAME`.
-        pub canister_name: &'static str,
-        /// The basename of the wasm file.
-        pub wasm_name: &'static str,
-        /// ic-nns-init uses test wasms for some canisters but still requires the standard wasm to be present.  For unknown reasons.
-        pub test_wasm_name: Option<&'static str>,
-        /// The id of the canister when installed by `dfx nns install`.
-        pub canister_id: &'static str,
-    }
-    /// Canister to keep a record of hardware and resource providers.
-    pub const NNS_REGISTRY: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-registry",
-        wasm_name: "registry-canister.wasm",
-        test_wasm_name: None,
-        canister_id: "rwlgt-iiaaa-aaaaa-aaaaa-cai",
-    };
-    /// Canister used to hold referanda and execute NNS proposals.
-    pub const NNS_GOVERNANCE: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-governance",
-        test_wasm_name: Some("governance-canister_test.wasm"),
-        wasm_name: "governance-canister.wasm",
-        canister_id: "rrkah-fqaaa-aaaaa-aaaaq-cai",
-    };
-    /// Canister that holds ICP account balances.
-    pub const NNS_LEDGER: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-ledger",
-        wasm_name: "ledger-canister_notify-method.wasm",
-        test_wasm_name: None,
-        canister_id: "ryjl3-tyaaa-aaaaa-aa&aba-cai",
-    };
-    /// Canister that controls all NNS canisters.
-    pub const NNS_ROOT: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-root",
-        wasm_name: "root-canister.wasm",
-        test_wasm_name: None,
-        canister_id: "r7inp-6aaaa-aaaaa-aaabq-cai",
-    };
-    /// Canister that exchanges ICP for cycles.
-    pub const NNS_CYCLES_MINTING: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-cycles-minting",
-        wasm_name: "cycles-minting-canister.wasm",
-        test_wasm_name: None,
-        canister_id: "rkp4c-7iaaa-aaaaa-aaaca-cai",
-    };
-    /// Canister used to restore functionality in an emergency.
-    pub const NNS_LIFELINE: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-lifeline",
-        wasm_name: "lifeline.wasm",
-        test_wasm_name: None,
-        canister_id: "rno2w-sqaaa-aaaaa-aaacq-cai",
-    };
-    /// Canister used to store genesis tokens.
-    pub const NNS_GENESIS_TOKENS: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-genesis-token",
-        wasm_name: "genesis-token-canister.wasm",
-        test_wasm_name: None,
-        canister_id: "renrk-eyaaa-aaaaa-aaada-cai",
-    };
-    /// Canister that spawns SNS canister groups.
-    pub const NNS_SNS_WASM: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-sns-wasm",
-        wasm_name: "sns-wasm-canister.wasm",
-        test_wasm_name: None,
-        canister_id: "qvhpv-4qaaa-aaaaa-aaagq-cai",
-    };
-    /// Placeholder for the Internet Identity.  Not used.
-    pub const NNS_IDENTITY: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-identity",
-        wasm_name: "identity-canister.wasm",
-        test_wasm_name: None,
-        canister_id: "",
-    };
-    /// Placeholder for the nns-dapp.  Not used.
-    pub const NNS_UI: IcNnsInitCanister = IcNnsInitCanister {
-        canister_name: "nns-ui",
-        wasm_name: "nns-ui-canister.wasm",
-        test_wasm_name: None,
-        canister_id: "",
-    };
-    /// Minimum data needed to download and deploy a standard canister via dfx deploy NAME.
-    pub struct StandardCanister {
-        /// The typical name of the canister, as seen in dfx.json or used in `dfx canister id NAME`.
-        pub canister_name: &'static str,
-        /// The basename of the wasm file.
-        pub wasm_name: &'static str,
-        /// The URL from which to download the wasm file.
-        pub wasm_url: &'static str,
-    }
-    /// A canister that provides login as a service for other dapps.
-    pub const INTERNET_IDENTITY: StandardCanister = StandardCanister {
-        canister_name: "internet_identity",
-        wasm_name: "internet_identity_dev.wasm",
-        wasm_url: "https://github.com/dfinity/internet-identity/releases/download/release-2022-07-11/internet_identity_dev.wasm"
-    };
-    /// Frontend dapp for voting and managing neurons.
-    pub const NNS_DAPP: StandardCanister = StandardCanister {
-        canister_name: "nns-dapp",
-        wasm_name: "nns-dapp_local.wasm",
-        wasm_url: "https://github.com/dfinity/nns-dapp/releases/download/tip/nns-dapp_local.wasm",
-    };
-    /// Backend canisters deployed by `ic nns init`.
-    pub const NNS_CORE: &[&IcNnsInitCanister; 10] = &[
-        &NNS_REGISTRY,
-        &NNS_GOVERNANCE,
-        &NNS_LEDGER,
-        &NNS_ROOT,
-        &NNS_CYCLES_MINTING,
-        &NNS_LIFELINE,
-        &NNS_GENESIS_TOKENS,
-        &NNS_SNS_WASM,
-        &NNS_IDENTITY,
-        &NNS_UI,
-    ];
-    /// Frontend canisters deployed by `ic nns init`.  The deployment is normal, like any other canister.
-    pub const NNS_FRONTEND: [&StandardCanister; 2] = [&INTERNET_IDENTITY, &NNS_DAPP];
-
-    /// Information required for WASMs uploaded to the nns-sns-wasm canister.
-    ///
-    /// Note:  These wasms are not deployed by `ic nns install` but later by developers
-    pub struct SnsCanisterInstallation {
-        /// The name of the canister as typically added to dfx.json or used in `dfx cansiter id NAME`
-        pub canister_name: &'static str,
-        /// The name used when uploading to the nns-sns-wasm canister.
-        pub upload_name: &'static str,
-        /// The basename of the wasm file.
-        pub wasm_name: &'static str,
-    }
-    /// The controller of all the canisters in a given SNS project.
-    pub const SNS_ROOT: SnsCanisterInstallation = SnsCanisterInstallation {
-        canister_name: "sns-root",
-        upload_name: "root",
-        wasm_name: "sns-root-canister.wasm",
-    };
-    /// The governance canister for an SNS project.
-    pub const SNS_GOVERNANCE: SnsCanisterInstallation = SnsCanisterInstallation {
-        canister_name: "sns-governance",
-        upload_name: "governance",
-        wasm_name: "sns-governance-canister.wasm",
-    };
-    /// Manages the decentralisation of an SNS project, exchanging stake in the mainnet for stake in the project.
-    pub const SNS_SWAP: SnsCanisterInstallation = SnsCanisterInstallation {
-        canister_name: "sns-swap",
-        upload_name: "swap",
-        wasm_name: "sns-governance-canister.wasm",
-    };
-    /// Stores account balances for an SNS project.
-    pub const SNS_LEDGER: SnsCanisterInstallation = SnsCanisterInstallation {
-        canister_name: "sns-ledger",
-        upload_name: "ledger",
-        wasm_name: "ic-icrc1-ledger.wasm",
-    };
-    /// Stores ledger data; needed to preserve data if an SNS ledger canister is upgraded.
-    pub const SNS_LEDGER_ARCHIVE: SnsCanisterInstallation = SnsCanisterInstallation {
-        canister_name: "sns-ledger-archive",
-        upload_name: "archive",
-        wasm_name: "ic-icrc1-archive.wasm",
-    };
-    /// SNS wasm files hosted by the nns-sns-wasms canister.
-    ///
-    /// Note:  Sets of these canisters are deployed on request, so one network will
-    /// typically have many sets of these canisters, one per project decentralized
-    /// with the SNS toolchain.
-    pub const SNS_CANISTERS: [&SnsCanisterInstallation; 5] = [
-        &SNS_ROOT,
-        &SNS_GOVERNANCE,
-        &SNS_SWAP,
-        &SNS_LEDGER,
-        &SNS_LEDGER_ARCHIVE,
-    ];
-
-    /// Test account with well known public & private keys, used in NNS_LEDGER, NNS_DAPP and third party projects.
-    pub const TEST_ACCOUNT: &str =
-        "5b315d2f6702cb3a27d826161797d7b2c2e131cd312aece51d4d5574d1247087";
-}
+pub mod canisters;
 
 /// Installs NNS canisters on a local dfx server.
 /// # Notes:
@@ -267,6 +87,7 @@ pub async fn install_nns(
         canister_id_store.add(canister_name, canister_id)?;
     }
 
+    println!("Uploading NNS data...");
     upload_nns_sns_wasms_canister_wasms(env)?;
 
     // Install the GUI canisters:
@@ -274,16 +95,34 @@ pub async fn install_nns(
         wasm_url,
         wasm_name,
         canister_name,
-    } in canisters::NNS_FRONTEND
+    } in NNS_FRONTEND
     {
         let local_wasm_path = nns_wasm_dir(env).join(wasm_name);
         download(&Url::parse(wasm_url)?, &local_wasm_path).await?;
         install_canister(env, agent, canister_name, &local_wasm_path).await?;
     }
     // ... and configure the backend NNS canisters:
+    println!("Configuring the NNS...");
     set_xdr_rate(1234567, &nns_url)?;
     set_cmc_authorized_subnets(&nns_url, &subnet_id.to_string())?;
 
+    let canister_id_store = CanisterIdStore::for_env(env)?;
+    println!(r#"
+
+######################################
+# NNS CANISTER INSTALLATION COMPLETE #
+######################################
+
+Backend canisters:
+{}
+
+Frontend canisters:
+{}
+
+"#,
+  NNS_CORE.iter().map(|canister| format!("{:20}  {}\n", canister.canister_name, canister.canister_id)).collect::<Vec<String>>().join(""),
+  // TODO:  Variable port
+  NNS_FRONTEND.iter().map(|canister| format!("{:20}  http://{}.localhost:8080\n", canister.canister_name, canister_id_store.get(canister.canister_name).map(|principal| principal.to_string()).unwrap_or_default())).collect::<Vec<String>>().join(""));
     Ok(())
 }
 
