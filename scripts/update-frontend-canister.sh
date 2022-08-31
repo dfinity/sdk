dd8f57e20944e7775183a7cbb7e01d361ae861f4
Linwei Shang <linwei.shang@dfinity.org>
2022-08-31 17:03:59 -0400

    rename cont

--- scripts/update-frontend-canister.sh
+++ scripts/update-frontend-canister.sh
@@ -9,12 +9,12 @@ die() {
 cargo --version >/dev/null || die "Must have cargo installed."
 
 export RUSTFLAGS="--remap-path-prefix=\"${PWD}\"=./ --remap-path-prefix=\"${HOME}\"=_/"
-cargo build -p certified-assets --release --target wasm32-unknown-unknown
+cargo build -p ic-assets-canister --release --target wasm32-unknown-unknown
 
 BUILD_DIR="target/wasm32-unknown-unknown/release"
-ic-wasm --output $BUILD_DIR/certified_assets.wasm $BUILD_DIR/certified_assets.wasm metadata --file src/canisters/frontend/ic-certified-assets/assets.did --visibility public candid:service
-ic-wasm --output $BUILD_DIR/certified_assets.wasm $BUILD_DIR/certified_assets.wasm shrink
-gzip --best --keep --force --no-name $BUILD_DIR/certified_assets.wasm
+ic-wasm --output $BUILD_DIR/ic_assets_canister.wasm $BUILD_DIR/ic_assets_canister.wasm metadata --file src/canisters/frontend/ic-certified-assets/assets.did --visibility public candid:service
+ic-wasm --output $BUILD_DIR/ic_assets_canister.wasm $BUILD_DIR/ic_assets_canister.wasm shrink
+gzip --best --keep --force --no-name $BUILD_DIR/ic_assets_canister.wasm
 
-cp -f $BUILD_DIR/certified_assets.wasm.gz src/distributed/assetstorage.wasm.gz
+cp -f $BUILD_DIR/ic_assets_canister.wasm.gz src/distributed/assetstorage.wasm.gz
 cp -f src/canisters/frontend/ic-certified-assets/assets.did src/distributed/assetstorage.did
