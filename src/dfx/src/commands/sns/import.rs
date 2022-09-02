e33bacc5a16cf083443c9367704f74c0cb7a05c4
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2022-09-02 10:12:55 -0700

    feat: add dfx nns import, dfx sns import commands (#2515)
    * feat: add dfx nns import, dfx ssn import commands
    
    Also added dfx project import, which can import from anywhere, but is used in e2e tests.
    
    All of these are hidden for the time being, because the nns/sns support is still work in progress, and it's unclear what 'project import' should really look like.
    
    * shellcheck
    
    * move new project command under a top-level beta command
    
    * fmt
    
    * Fix some doc comments
    
    Co-authored-by: Linwei Shang <linwei.shang@dfinity.org>

---
+++ src/dfx/src/commands/sns/import.rs
@@ -0,0 +1,37 @@
+use crate::lib::error::DfxResult;
+use crate::lib::project::import::import_canister_definitions;
+use crate::lib::project::network_mappings::get_network_mappings;
+use crate::Environment;
+
+use clap::Parser;
+use tokio::runtime::Runtime;
+
+/// Imports the sns canisters
+#[derive(Parser)]
+pub struct SnsImportOpts {
+    /// Networks to import canisters ids for.
+    ///   --network-mapping <network name in both places>
+    ///   --network-mapping <network name here>=<network name in project being imported>
+    /// Examples:
+    ///   --network-mapping ic
+    ///   --network-mapping ic=mainnet
+    #[clap(long, default_value = "ic=mainnet", multiple_occurrences(true))]
+    network_mapping: Vec<String>,
+}
+
+pub fn exec(env: &dyn Environment, opts: SnsImportOpts) -> DfxResult {
+    let config = env.get_config_or_anyhow()?;
+    let mut config = config.as_ref().clone();
+
+    let network_mappings = get_network_mappings(&opts.network_mapping)?;
+
+    let runtime = Runtime::new().expect("Unable to create a runtime");
+    runtime.block_on(import_canister_definitions(
+        env.get_logger(),
+        &mut config,
+        "https://raw.githubusercontent.com/dfinity/ic/master/rs/sns/cli/dfx.json",
+        None,
+        None,
+        &network_mappings,
+    ))
+}
