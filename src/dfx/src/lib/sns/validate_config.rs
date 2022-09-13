8d5dc960932d1a6411c7f5cbf6119345406c42a2
Max <max@dfinity.org>
2022-09-14 01:54:41 +0200

    Code

--- src/dfx/src/lib/sns/validate_config.rs
+++ src/dfx/src/lib/sns/validate_config.rs
@@ -1,9 +1,41 @@
+use anyhow::{anyhow, Context};
 use fn_error_context::context;
 use std::path::Path;
+use std::process::{self, Command};
 
 use crate::lib::error::DfxResult;
+use crate::Environment;
 
 #[context("Failed to validate sns config at {}.", path.display())]
 pub fn validate_config(path: &Path) -> DfxResult {
-    todo!()
+    let cli_name = "sns";
+    let sns_cli = env
+        .get_cache()
+        .get_binary_command_path(cli_name)
+        .with_context(|| format!("Could not find bundled binary '{cli_name}'."))?;
+    let mut command = Command::new(sns_cli);
+    command
+        .arg("init-config-file")
+        .arg("--init-config-file-path")
+        .arg(path)
+        .arg("validate");
+    command
+        .stdin(process::Stdio::null())
+        .output()
+        .map_err(anyhow::Error::from)
+        .and_then(|output| {
+            if output.status.success() {
+                Ok(())
+            } else {
+                Err(anyhow!(
+                    "Failed to validate SNS configuration.\n{:?} {:?}\nStdout:\n{:?}\n\nStderr:\n{:?}",
+                    command.get_program(),
+                    command.get_args(),
+                    String::from_utf8_lossy(&output.stdout),
+                    String::from_utf8_lossy(&output.stderr)
+                ))
+            }
+        })?;
+    Ok(())
+
 }
