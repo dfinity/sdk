4b7605dc2cf3bb167315d267863339af9853137d
Max <max@dfinity.org>
2022-09-14 01:17:15 +0200

    Add tests for dfx sns config validate

--- e2e/tests-dfx/sns.bash
+++ e2e/tests-dfx/sns.bash
@@ -13,6 +13,9 @@ teardown() {
     standard_teardown
 }
 
+# The location of the SNS init config.
+SNS_CONFIG_FILE_NAME="sns.yml"
+
 @test "sns config create and validate fail outside of a project" {
     assert_command_fail dfx sns config create
     assert_match 'Cannot find dfx configuration file in the current working directory'
@@ -29,8 +32,13 @@ teardown() {
 }
 
 @test "sns config validate approves a valid configuration" {
+    cp "${BATS_TEST_DIRNAME}/../assets/sns/valid_sns_init_config.yaml" "$SNS_CONFIG_FILE_NAME"
     dfx_new
+    dfx sns config validate
+    assert_match 'No errors found'
+}
+@test "sns config validate identifies a missing key" {
+    grep -v token_name "${BATS_TEST_DIRNAME}/../assets/sns/valid_sns_init_config.yaml" > "$SNS_CONFIG_FILE_NAME"
     assert_command_fail dfx sns config validate
-    # todo
-    assert_match 'not yet implemented'
+    assert_match token_name
 }
