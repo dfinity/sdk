ab453a948731ea144170abd5fd5dbc4f058239fc
Max <max@dfinity.org>
2022-09-14 02:51:23 +0200

    ++

--- src/dfx/src/lib/sns/mod.rs
+++ src/dfx/src/lib/sns/mod.rs
@@ -1,4 +1,5 @@
 pub mod create_config;
 pub mod validate_config;
+pub mod sns_cli;
 
 pub const CONFIG_FILE_NAME: &str = "sns.yml";
--- src/dfx/src/lib/sns/validate_config.rs
+++ src/dfx/src/lib/sns/validate_config.rs
@@ -1,41 +1,14 @@
-use anyhow::{anyhow, Context};
 use fn_error_context::context;
+use std::ffi::OsString;
 use std::path::Path;
-use std::process::{self, Command};
 
 use crate::lib::error::DfxResult;
+use crate::lib::sns::sns_cli::call_sns_cli;
 use crate::Environment;
 
 ///
 #[context("Failed to validate SNS config at {}.", path.display())]
 pub fn validate_config(env: &dyn Environment, path: &Path) -> DfxResult {
-    let cli_name = "sns";
-    let sns_cli = env
-        .get_cache()
-        .get_binary_command_path(cli_name)
-        .with_context(|| format!("Could not find bundled binary '{cli_name}'."))?;
-    let mut command = Command::new(sns_cli);
-    command
-        .arg("init-config-file")
-        .arg("--init-config-file-path")
-        .arg(path)
-        .arg("validate");
-    command
-        .stdin(process::Stdio::null())
-        .output()
-        .map_err(anyhow::Error::from)
-        .and_then(|output| {
-            if output.status.success() {
-                Ok(())
-            } else {
-                Err(anyhow!(
-                    "Failed to validate SNS configuration.\n{:?} {:?}\nStdout:\n{:?}\n\nStderr:\n{:?}",
-                    command.get_program(),
-                    command.get_args(),
-                    String::from_utf8_lossy(&output.stdout),
-                    String::from_utf8_lossy(&output.stderr)
-                ))
-            }
-        })?;
-    Ok(())
+    let args = vec![OsString::from("init-config-file"), OsString::from("--init-config-file-path"), OsString::from(path), OsString::from("validate")];
+    call_sns_cli(env, &args)
 }
