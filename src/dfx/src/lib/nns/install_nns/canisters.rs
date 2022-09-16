cef9cfae14bc3484111ae4fd0fbc38aa232353f3
Max <max@dfinity.org>
2022-09-16 13:44:28 +0200

    Bump canister IDs by one

--- src/dfx/src/lib/nns/install_nns/canisters.rs
+++ src/dfx/src/lib/nns/install_nns/canisters.rs
@@ -97,14 +97,14 @@ pub struct StandardCanister {
 /// A canister that provides login as a service for other dapps.
 pub const INTERNET_IDENTITY: StandardCanister = StandardCanister {
     canister_name: "internet_identity",
-    canister_id: "qaa6y-5yaaa-aaaaa-aaafa-cai",
+    canister_id: "qhbym-qaaaa-aaaaa-aaafq-cai",
     wasm_name: "internet_identity_dev.wasm",
     wasm_url: "https://github.com/dfinity/internet-identity/releases/download/release-2022-07-11/internet_identity_dev.wasm"
 };
 /// Frontend dapp for voting and managing neurons.
 pub const NNS_DAPP: StandardCanister = StandardCanister {
     canister_name: "nns-dapp",
-    canister_id: "qhbym-qaaaa-aaaaa-aaafq-cai",
+    canister_id: "qsgjb-riaaa-aaaaa-aaaga-cai",
     wasm_name: "nns-dapp_local.wasm",
     wasm_url: "https://github.com/dfinity/nns-dapp/releases/download/tip/nns-dapp_local.wasm",
 };
