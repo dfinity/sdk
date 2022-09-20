32b1ee2622b502d2d955f08d095f04b3c6c37d63
Max <max@dfinity.org>
2022-09-20 13:27:40 +0200

    fmt + clippy

--- src/dfx/src/commands/sns/import.rs
+++ src/dfx/src/commands/sns/import.rs
@@ -1,4 +1,5 @@
 use crate::lib::error::DfxResult;
+use crate::lib::info::replica_rev;
 use crate::lib::project::import::import_canister_definitions;
 use crate::lib::project::network_mappings::get_network_mappings;
 use crate::Environment;
@@ -26,7 +27,7 @@ pub fn exec(env: &dyn Environment, opts: SnsImportOpts) -> DfxResult {
     let network_mappings = get_network_mappings(&opts.network_mapping)?;
 
     let runtime = Runtime::new().expect("Unable to create a runtime");
-    let ic_commit = std::env::var("DFX_IC_REF").unwrap_or_else(|| replica_rev());
+    let ic_commit = std::env::var("DFX_IC_REF").unwrap_or_else(|_| replica_rev().to_string());
     let their_dfx_json_location =
         format!("https://raw.githubusercontent.com/dfinity/ic/{ic_commit}/rs/sns/cli/dfx.json");
     runtime.block_on(import_canister_definitions(
