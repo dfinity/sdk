3ce9c404bf6c3b8b9f17060fb9162dead0a3bf8b
Max <max@dfinity.org>
2022-10-12 23:57:26 +0200

    Interpret and pass on --network flag

--- src/dfx/src/lib/sns/deploy.rs
+++ src/dfx/src/lib/sns/deploy.rs
@@ -5,13 +5,29 @@ use std::path::Path;
 
 use crate::lib::call_bundled::call_bundled;
 use crate::lib::error::DfxResult;
+use crate::lib::provider::create_agent_environment;
 use crate::Environment;
 
 /// Creates an SNS.  This requires funds but no proposal.
 #[context("Failed to deploy SNS with config: {}", path.display())]
-pub fn deploy_sns(env: &dyn Environment, path: &Path) -> DfxResult<String> {
+pub fn deploy_sns(
+    env: &dyn Environment,
+    path: &Path,
+    network: Option<String>,
+) -> DfxResult<String> {
+    let agent_environment = create_agent_environment(env, network)?;
+    let network_descriptor = agent_environment.get_network_descriptor();
+    let network_str = if network_descriptor.is_ic {
+        "ic"
+    } else if let Ok(url) = network_descriptor.first_provider() {
+        url
+    } else {
+        "local"
+    };
     let args = vec![
         OsString::from("deploy"),
+        OsString::from("--network"),
+        OsString::from(network_str),
         OsString::from("--init-config-file"),
         OsString::from(path),
     ];
