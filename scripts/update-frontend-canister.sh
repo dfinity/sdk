26e853b2e6734bcc2afdbc5cc4e2f48e93d8d1c9
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2022-10-17 20:20:02 +0200

    disable everything besides `cargo build`

--- scripts/update-frontend-canister.sh
+++ scripts/update-frontend-canister.sh
@@ -8,26 +8,26 @@ die() {
 
 cargo --version >/dev/null || die "Must have cargo installed."
 
-SCRIPT=$(readlink -f "${0}")
-SCRIPT_DIR=$(dirname "${SCRIPT}")
-cd ${SCRIPT_DIR}/..
+# SCRIPT=$(readlink -f "${0}")
+# SCRIPT_DIR=$(dirname "${SCRIPT}")
+# cd ${SCRIPT_DIR}/..
 
-if [ -z "${CARGO_HOME}" ]
-then
-  export CARGO_HOME="${HOME}/.cargo"
-fi
+# if [ -z "${CARGO_HOME}" ]
+# then
+#   export CARGO_HOME="${HOME}/.cargo"
+# fi
 
-export RUSTFLAGS="--remap-path-prefix $(readlink -f ${SCRIPT_DIR}/..)=/build --remap-path-prefix ${CARGO_HOME}/bin=/cargo/bin --remap-path-prefix ${CARGO_HOME}/git=/cargo/git"
-for l in $(ls ${CARGO_HOME}/registry/src/)
-do
-  export RUSTFLAGS="--remap-path-prefix ${CARGO_HOME}/registry/src/${l}=/cargo/registry/src/github ${RUSTFLAGS}"
-done
+# export RUSTFLAGS="--remap-path-prefix $(readlink -f ${SCRIPT_DIR}/..)=/build --remap-path-prefix ${CARGO_HOME}/bin=/cargo/bin --remap-path-prefix ${CARGO_HOME}/git=/cargo/git"
+# for l in $(ls ${CARGO_HOME}/registry/src/)
+# do
+#   export RUSTFLAGS="--remap-path-prefix ${CARGO_HOME}/registry/src/${l}=/cargo/registry/src/github ${RUSTFLAGS}"
+# done
 cargo build -p ic-frontend-canister --release --target wasm32-unknown-unknown --locked
 
 BUILD_DIR="target/wasm32-unknown-unknown/release"
 # ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm metadata --file src/canisters/frontend/ic-certified-assets/assets.did --visibility public candid:service
 # ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm shrink
-gzip --best --keep --force --no-name $BUILD_DIR/ic_frontend_canister.wasm
+# gzip --best --keep --force --no-name $BUILD_DIR/ic_frontend_canister.wasm
 
-cp -f $BUILD_DIR/ic_frontend_canister.wasm.gz src/distributed/assetstorage.wasm.gz
+cp -f $BUILD_DIR/ic_frontend_canister.wasm src/distributed/assetstorage.wasm.gz
 cp -f src/canisters/frontend/ic-certified-assets/assets.did src/distributed/assetstorage.did
