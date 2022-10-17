915a090be09c0e1c6d4e26d0153246b73e0258eb
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2022-10-17 20:16:04 +0200

    back to ubuntu, trying to disable ic-wasm

--- scripts/update-frontend-canister.sh
+++ scripts/update-frontend-canister.sh
@@ -8,7 +8,7 @@ die() {
 
 cargo --version >/dev/null || die "Must have cargo installed."
 
-SCRIPT=$(greadlink -f "${0}")
+SCRIPT=$(readlink -f "${0}")
 SCRIPT_DIR=$(dirname "${SCRIPT}")
 cd ${SCRIPT_DIR}/..
 
@@ -17,7 +17,7 @@ then
   export CARGO_HOME="${HOME}/.cargo"
 fi
 
-export RUSTFLAGS="--remap-path-prefix $(greadlink -f ${SCRIPT_DIR}/..)=/build --remap-path-prefix ${CARGO_HOME}/bin=/cargo/bin --remap-path-prefix ${CARGO_HOME}/git=/cargo/git"
+export RUSTFLAGS="--remap-path-prefix $(readlink -f ${SCRIPT_DIR}/..)=/build --remap-path-prefix ${CARGO_HOME}/bin=/cargo/bin --remap-path-prefix ${CARGO_HOME}/git=/cargo/git"
 for l in $(ls ${CARGO_HOME}/registry/src/)
 do
   export RUSTFLAGS="--remap-path-prefix ${CARGO_HOME}/registry/src/${l}=/cargo/registry/src/github ${RUSTFLAGS}"
@@ -25,8 +25,8 @@ done
 cargo build -p ic-frontend-canister --release --target wasm32-unknown-unknown --locked
 
 BUILD_DIR="target/wasm32-unknown-unknown/release"
-ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm metadata --file src/canisters/frontend/ic-certified-assets/assets.did --visibility public candid:service
-ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm shrink
+# ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm metadata --file src/canisters/frontend/ic-certified-assets/assets.did --visibility public candid:service
+# ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm shrink
 gzip --best --keep --force --no-name $BUILD_DIR/ic_frontend_canister.wasm
 
 cp -f $BUILD_DIR/ic_frontend_canister.wasm.gz src/distributed/assetstorage.wasm.gz
