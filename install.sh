c080014d091f5a00ea28a46efd2e97a10ad84519
dfx-json <98767015+dfx-json@users.noreply.github.com>
2022-10-25 07:27:03 +0000

    Deploying to public-manifest from  @ 091932f1c3dcecd8a27c39357a33bd8444c953b6 🚀

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="caf1b332b12cfafea06978bb682a1cf2292806c0"
+SCRIPT_COMMIT_DESC="091932f1c3dcecd8a27c39357a33bd8444c953b6"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
