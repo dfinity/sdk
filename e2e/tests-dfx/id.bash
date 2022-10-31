067d011ca8d5848aee9129c42d0ece33a2211c47
Max <max@dfinity.org>
2022-10-31 17:18:27 +0100

    Test++

--- e2e/tests-dfx/id.bash
+++ e2e/tests-dfx/id.bash
@@ -61,13 +61,14 @@ teardown() {
     dfx build
     jq '.canisters.e2e_project_backend.remote.id.__default = "rkp4c-7iaaa-aaaaa-aaaca-cai"' dfx.json | sponge dfx.json
     jq '.canisters.e2e_project_backend.remote.id.namedremote = "va76m-bqaaa-aaaaa-aaayq-cai"' dfx.json | sponge dfx.json
+    jq '.somethingelse= {"type": "persistent", "providers": ["http://somethingelse"]}' "$E2E_NETWORKS_JSON" | sponge "$E2E_NETWORKS_JSON"
     # The local dfx canister ID should not be affected:
     assert_command dfx canister id e2e_project_backend
     assert_match "$(jq -r .e2e_project_backend.local < .dfx/local/canister_ids.json)"
     # Named remotes should be unaffected:
     assert_command dfx canister --network namedremote id e2e_project_backend
     assert_match "va76m-bqaaa-aaaaa-aaayq-cai"
-    # Other remotes should use teh default entry:
+    # Other remotes should use the default entry:
     assert_command dfx canister --network somethingelse id e2e_project_backend
     assert_match "rkp4c-7iaaa-aaaaa-aaaca-cai"
 }
