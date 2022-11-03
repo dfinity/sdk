fcc5e8e943269520cbf0d382e4921eff31b31c9f
Eric Swanson <eric.swanson@dfinity.org>
2022-11-02 17:23:21 -0700

    fmt

--- src/dfx/src/commands/canister/delete.rs
+++ src/dfx/src/commands/canister/delete.rs
@@ -34,6 +34,7 @@ use std::time::Duration;
 #[allow(deprecated)]
 const DANK_PRINCIPAL: Principal =
     Principal::from_slice(&[0, 0, 0, 0, 0, 0xe0, 1, 0x11, 0x01, 0x01]); // Principal: aanaa-xaaaa-aaaah-aaeiq-cai
+
 // "Couldn't send message" when deleting a canister: increase WITHDRAWAL_COST
 const WITHDRAWAL_COST: u128 = 10_101_000_000; // Observed ok locally @10_100_066_000
 const MAX_MEMORY_ALLOCATION: u64 = 8589934592;
