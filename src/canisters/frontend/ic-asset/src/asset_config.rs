8ac3104adeb79f68fdc51d10563c47ba75596ff7
Severin Siffert <severin.siffert@dfinity.org>
2022-11-11 10:50:09 +0100

    Update src/canisters/frontend/ic-asset/src/asset_config.rs
    Co-authored-by: Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>

--- src/canisters/frontend/ic-asset/src/asset_config.rs
+++ src/canisters/frontend/ic-asset/src/asset_config.rs
@@ -47,6 +47,7 @@ pub struct AssetConfigRule {
     headers: Maybe<HeadersConfig>,
     #[serde(skip_serializing_if = "Option::is_none")]
     ignore: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     enable_aliasing: Option<bool>,
     #[serde(skip_serializing)]
     used: bool,
