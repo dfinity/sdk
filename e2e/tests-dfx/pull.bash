2f8a388bd5decf0c50a6a6e45fe890d109901df0
Linwei Shang <linwei.shang@dfinity.org>
2022-11-17 21:45:18 -0500

    e2e

---
+++ e2e/tests-dfx/pull.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+    standard_setup
+}
+
+teardown() {
+    dfx_stop
+
+    standard_teardown
+}
+
+@test "dfx build can write required metadata for pull" {
+    dfx_new
+    install_asset pull
+
+    dfx_start
+    
+    dfx canister create --all
+    assert_command dfx build
+    assert_command ic-wasm .dfx/local/canisters/e2e_project_backend/e2e_project_backend.wasm metadata
+    assert_match "icp:public candid:service"
+    assert_match "icp:public dfx:deps"
+    assert_match "icp:public dfx:init"
+    assert_match "icp:public dfx:wasm_url"
+
+    assert_command ic-wasm .dfx/local/canisters/e2e_project_backend/e2e_project_backend.wasm metadata dfx:deps
+    assert_match "dep1:rrkah-fqaaa-aaaaa-aaaaq-cai;dep2:ryjl3-tyaaa-aaaaa-aaaba-cai;"
+
+    assert_command ic-wasm .dfx/local/canisters/e2e_project_backend/e2e_project_backend.wasm metadata dfx:init
+    assert_match "NA"
+
+    assert_command ic-wasm .dfx/local/canisters/e2e_project_backend/e2e_project_backend.wasm metadata dfx:wasm_url
+    assert_match "example.com/e2e_project.wasm"
+}
