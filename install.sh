a6088dc14a1dd453ecbf387da92b6f624c0c15ea
sesi200 <severin.siffert@dfinity.org>
2022-11-18 07:05:35 +0000

    Deploying to public-manifest from  @ 7134cdfe72a770a251fcef6c5413b541d6463e5e 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION:-}"
-SCRIPT_COMMIT_DESC="3d91c1be7f1a508f9506e10f666584634a2f85a3"
+SCRIPT_COMMIT_DESC="7134cdfe72a770a251fcef6c5413b541d6463e5e"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
