70c6b245395c557aa522ed01a83efad81c444bd6
Severin Siffert <severin.siffert@dfinity.org>
2022-11-24 14:02:55 +0100

    fix: give more cycles margin when deleting canisters

--- src/dfx/src/commands/canister/delete.rs
+++ src/dfx/src/commands/canister/delete.rs
@@ -36,7 +36,7 @@ const DANK_PRINCIPAL: Principal =
     Principal::from_slice(&[0, 0, 0, 0, 0, 0xe0, 1, 0x11, 0x01, 0x01]); // Principal: aanaa-xaaaa-aaaah-aaeiq-cai
 
 // "Couldn't send message" when deleting a canister: increase WITHDRAWAL_COST
-const WITHDRAWAL_COST: u128 = 10_303_000_000; // 2% higher than a value observed ok locally
+const WITHDRAWAL_COST: u128 = 10_606_030_000; // 5% higher than a value observed ok locally
 const MAX_MEMORY_ALLOCATION: u64 = 8589934592;
 
 /// Deletes a currently stopped canister.
