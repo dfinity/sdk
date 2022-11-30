8265bce4f3fa3a6a8d02db15c5af08d569812f7e
Linwei Shang <linwei.shang@dfinity.org>
2022-11-30 15:19:54 -0500

    prepare pullable canisters in e2e test

--- e2e/tests-dfx/pull.bash
+++ e2e/tests-dfx/pull.bash
@@ -35,3 +35,26 @@ teardown() {
     assert_command ic-wasm .dfx/local/canisters/e2e_project_backend/e2e_project_backend.wasm metadata dfx:wasm_url
     assert_match "https://example.com/e2e_project.wasm"
 }
+
+@test "dfx pull can resolve dependencies from on-chain canister metadata" {
+    # system-wide local replica
+    dfx_start
+
+    # prepare canisters to be pulled 
+    mkdir onchain
+    cd onchain
+    install_asset pullable
+
+    echo -n -e \\x00asm\\x01\\x00\\x00\\x00 > src/onchain_a/main.wasm
+
+    echo -n -e \\x00asm\\x01\\x00\\x00\\x00 > src/onchain_b/empty.wasm
+    cd src/onchain_b
+    ic-wasm empty.wasm -o main.wasm metadata "dfx:deps" -d "onchain_a:rrkah-fqaaa-aaaaa-aaaaq-cai;" -v public
+    cd ../../
+    dfx deploy
+
+    assert_command dfx canister metadata ryjl3-tyaaa-aaaaa-aaaba-cai dfx:deps
+    assert_match "onchain_a:rrkah-fqaaa-aaaaa-aaaaq-cai;"
+
+    cd ../
+}
