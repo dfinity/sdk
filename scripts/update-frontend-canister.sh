5798cef1c626d8a6442daf39b5b7dbe83a61b397
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2022-12-13 12:57:43 +0100

    fix script

--- scripts/update-frontend-canister.sh
+++ scripts/update-frontend-canister.sh
@@ -31,19 +31,11 @@ case ${1---help} in
   --development-build | -d)
     cargo --version >/dev/null || die "Must have cargo installed."
     ic-wasm --version >/dev/null || die "Must have ic-wasm installed."
-    if [ -z "${CARGO_HOME}" ]
-    then
-    export CARGO_HOME="${HOME}/.cargo"
-    fi
-
-    export RUSTFLAGS="--remap-path-prefix $(readlink -f ${SCRIPT_DIR}/..)=/build --remap-path-prefix ${CARGO_HOME}/bin=/cargo/bin --remap-path-prefix ${CARGO_HOME}/git=/cargo/git"
-    for l in $(ls ${CARGO_HOME}/registry/src/)
-    do
-    export RUSTFLAGS="--remap-path-prefix ${CARGO_HOME}/registry/src/${l}=/cargo/registry/src/github ${RUSTFLAGS}"
-    done
-    cargo build -p ic-frontend-canister --release --target wasm32-unknown-unknown
 
     BUILD_DIR="target/wasm32-unknown-unknown/release"
+
+    cargo build -p ic-frontend-canister --release --target wasm32-unknown-unknown
+
     ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm metadata --file src/canisters/frontend/ic-certified-assets/assets.did --visibility public candid:service
     ic-wasm --output $BUILD_DIR/ic_frontend_canister.wasm $BUILD_DIR/ic_frontend_canister.wasm shrink
     gzip --best --keep --force --no-name $BUILD_DIR/ic_frontend_canister.wasm
