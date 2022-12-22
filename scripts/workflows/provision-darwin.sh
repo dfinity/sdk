1e8bfdf96dbb153343bd65089ae855455ee33575
Eric Swanson <eric.swanson@dfinity.org>
2022-12-21 17:10:37 -0800

    try: install openssl@3 (would require at minimum documentation changes)

--- scripts/workflows/provision-darwin.sh
+++ scripts/workflows/provision-darwin.sh
@@ -7,7 +7,7 @@ export
 # Enter temporary directory.
 pushd /tmp
 
-brew install coreutils moreutils
+brew install coreutils moreutils openssl@3
 
 # Install Bats.
 if [ "$(uname -r)" = "19.6.0" ]; then
