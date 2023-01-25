ba661fd178bc5d7ce6e329795bd91452f701ac79
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2023-01-25 07:16:49 -0800

    refactor: concrete error types for get_canister_config (#2920)
    Preparation to move to dfx-core crate

--- src/dfx-core/src/error/dfx_config.rs
+++ src/dfx-core/src/error/dfx_config.rs
@@ -13,4 +13,13 @@ pub enum DfxConfigError {
 
     #[error("Failed to get canisters with their dependencies (for {}): {1}", _0.as_deref().unwrap_or("all canisters"))]
     GetCanistersWithDependenciesFailed(Option<String>, Box<DfxConfigError>),
+
+    #[error("Failed to get compute allocation for canister '{0}': {1}")]
+    GetComputeAllocationFailed(String, Box<DfxConfigError>),
+
+    #[error("Failed to get freezing threshold for canister '{0}': {1}")]
+    GetFreezingThresholdFailed(String, Box<DfxConfigError>),
+
+    #[error("Failed to get memory allocation for canister '{0}': {1}")]
+    GetMemoryAllocationFailed(String, Box<DfxConfigError>),
 }
