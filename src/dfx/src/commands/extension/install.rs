3fb2f94f6446f705e1bcbe69334b93831b5db95c
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2023-01-30 16:21:50 +0100

    replace `spinners` implementation

--- src/dfx/src/commands/extension/install.rs
+++ src/dfx/src/commands/extension/install.rs
@@ -2,7 +2,6 @@ use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 
 use clap::Parser;
-use spinners::{Spinner, Spinners};
 
 #[derive(Parser)]
 pub struct InstallOpts {
@@ -11,12 +10,9 @@ pub struct InstallOpts {
 }
 
 pub fn exec(env: &dyn Environment, opts: InstallOpts) -> DfxResult<()> {
-    let mut sp = Spinner::new(
-        Spinners::Dots9,
-        format!("Installing extension: {}", opts.name),
-    );
+    let spinner = env.new_spinner(format!("Installing extension: {}", opts.name).into());
     let mgr = env.new_extension_manager()?;
     mgr.install_extension(&opts.name)?;
-    sp.stop();
+    spinner.finish_with_message(format!("Extension '{}' installed successfully", opts.name).into());
     Ok(())
 }
