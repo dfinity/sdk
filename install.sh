b3062e9ccbfd663d606c6e1087bc657c66f2b9ba
lwshang <github@lwshang.com>
2023-03-01 20:10:21 +0000

    Deploying to public-manifest from  @ b4a337f8ce25042894983fe47b2e3241b35cd453 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
-SCRIPT_COMMIT_DESC="7d6a2c755733a871bfd8c1780d65a155e31e77b7"
+SCRIPT_COMMIT_DESC="b4a337f8ce25042894983fe47b2e3241b35cd453"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
