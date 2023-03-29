cd6a119ebd43047bcc0019557789975a6bdd0053
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2023-03-29 19:00:57 +0200

    CI, are you still with us?

--- src/dfx-core/src/error/uri.rs
+++ src/dfx-core/src/error/uri.rs
@@ -6,15 +6,19 @@ use thiserror::Error;
 pub enum UriError {
     #[error(transparent)]
     NetworkConfigError(#[from] crate::error::network_config::NetworkConfigError),
+
     #[error(transparent)]
     FsError(#[from] crate::error::fs::FsError),
+
     #[error("Failed to read port value from '{0}': {1}")]
     PortReadError(String, ParseIntError),
-    // next error should accept a url::ParseError
+
     #[error("Failed to parse url '{0}': {1}")]
     UrlParseError(String, url::ParseError),
+
     #[error("Failed to determine replica urls: {0}")]
     ReplicaUrlsError(String),
+
     #[error("Failed to get providers for network '{0}': {1}")]
     ProvidersError(String, Box<Self>),
 }
