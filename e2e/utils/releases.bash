6fe63b99813ad16283b98d3170bc8c1548a1479e
Ognjen Maric <ognjen.maric@dfinity.org>
2023-03-31 20:05:14 +0200

    Please shellcheck

--- e2e/utils/releases.bash
+++ e2e/utils/releases.bash
@@ -1,20 +1,20 @@
 # Print the latest dfx version available on GitHub releases
 get_latest_dfx_version() {
-    latest_version=curl -s "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tag_name"
-    echo $latest_version
+    latest_version=$(curl -sL "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tag_name")
+    echo "$latest_version"
 }
 
 # Extract a particular file from the latest release tarball, and save it to the specified destination
 # Usage: get_from_latest_release_tarball <file_path> <destination>
 get_from_latest_release_tarball() {
-    local file_path=$1
-    local destination=$2
-    local tarball_url=$(curl -sL "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tarball_url")
+    local -r file_path=$1
+    local -r destination=$2
+    local -r tarball_url=$(curl -sL "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tarball_url")
 
-    local temp_dir=$(mktemp -d)
-    curl -sL "$tarball_url" -o "$temp_dir/release.tar.gz"
+    local -r temp_dir=$(mktemp -d)
+    curl -sL "$tarball_url" -o "${temp_dir}/release.tar.gz"
 
     tar -xzf "$temp_dir/release.tar.gz" -C "$temp_dir" "*/$file_path"
-    extracted_file=$(find "$temp_dir" -type f -name $(basename "$file_path"))
+    local -r extracted_file=$(find "$temp_dir" -type f -name $(basename "$file_path"))
     mv "$extracted_file" "$destination"
 }
