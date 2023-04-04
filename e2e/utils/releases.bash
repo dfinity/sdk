b368f676b0e52c7c27f7863707a28dc4765ff53e
Ognjen Maric <ognjen.maric@dfinity.org>
2023-04-04 18:30:34 +0200

    Use curl's built-in retry mechanism

--- e2e/utils/releases.bash
+++ e2e/utils/releases.bash
@@ -1,17 +1,6 @@
-# Usage: with_retries <nr_retries> <command>
-with_retries() {
-    local -r nr_retries=$1
-    shift
-    local -r cmd=$@
-    for i in $(seq 1 $nr_retries); do
-        ($cmd) && break;
-        sleep 2;
-    done
-}
-
 # Print the latest dfx version available on GitHub releases
 get_latest_dfx_version() {
-    latest_version=$(with_retries 5 (curl -sL "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tag_name"))
+    latest_version=$(curl -sL --retry 5 "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tag_name")
     echo "$latest_version"
 }
 
@@ -20,10 +9,10 @@ get_latest_dfx_version() {
 get_from_latest_release_tarball() {
     local -r file_path=$1
     local -r destination=$2
-    local -r tarball_url=$(with_retries 5 (curl -sL "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tarball_url"))
+    local -r tarball_url=$(curl -sL --retry 5 "https://api.github.com/repos/dfinity/sdk/releases/latest" | jq -r ".tarball_url")
 
     local -r temp_dir=$(mktemp -d)
-    with_retries 5 (curl -sL "$tarball_url" -o "${temp_dir}/release.tar.gz")
+    curl -sL --retry 5 "$tarball_url" -o "${temp_dir}/release.tar.gz"
 
     if [ "$(uname)" == "Darwin" ]; then
         tar -xzf "$temp_dir/release.tar.gz" -C "$temp_dir" "*/$file_path"
