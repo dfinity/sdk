bd9a3a9c4be8a21a3f274f740aa5b7055afd145b
Severin Siffert <severin.siffert@dfinity.org>
2023-04-18 17:58:11 +0200

    feat: make confirmation dialogue more accepting (#3092)
    Confirmation dialogues are no longer case sensitive and accept 'y' in addition to 'yes'.
    
    Fixes [SDK-956](https://dfinity.atlassian.net/browse/SDK-956)

--- src/dfx-core/src/cli/mod.rs
+++ src/dfx-core/src/cli/mod.rs
@@ -10,8 +10,8 @@ pub fn ask_for_consent(message: &str) -> Result<(), UserConsent> {
     stdin()
         .read_line(&mut input_string)
         .map_err(UserConsent::ReadError)?;
-    let input_string = input_string.trim_end();
-    if input_string != "yes" {
+    let input_string = input_string.trim_end().to_lowercase();
+    if input_string != "yes" && input_string != "y" {
         return Err(UserConsent::Declined);
     }
     Ok(())
