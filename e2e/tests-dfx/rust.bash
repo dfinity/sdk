302fc75cf4ff73145aa381bde5df61c13be86d78
Linwei Shang <linwei.shang@dfinity.org>
2023-05-11 14:22:04 -0400

    fix rust e2e

--- e2e/tests-dfx/rust.bash
+++ e2e/tests-dfx/rust.bash
@@ -18,8 +18,8 @@ teardown() {
 
     dfx_start
     dfx canister create --all
-    assert_command dfx build hello_backend
-    assert_match "Shrink WASM module size."
+    assert_command dfx build hello_backend -vvv
+    assert_match "Shrinking WASM"
     assert_command dfx canister install hello_backend
     assert_command dfx canister call hello_backend greet dfinity
     assert_match '("Hello, dfinity!")'
@@ -27,7 +27,6 @@ teardown() {
     # dfx sets the candid:service metadata
     dfx canister metadata hello_backend candid:service >installed.did
     assert_command diff src/hello_backend/hello_backend.did installed.did
-
 }
 
 @test "rust canister can resolve dependencies" {
