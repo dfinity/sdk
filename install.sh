472544ec8e0e6f5eec4ba2b144273e6b1611c970
dprats <diego.prats@gmail.com>
2023-05-18 22:22:35 +0000

    Deploying to public-manifest from  @ 6f32576d4a7cb4fec4a8f0e69ddde8d5250dc511 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
-SCRIPT_COMMIT_DESC="fd6e41c4d6595a98413da379245c9fc5d1028fd7"
+SCRIPT_COMMIT_DESC="6f32576d4a7cb4fec4a8f0e69ddde8d5250dc511"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
