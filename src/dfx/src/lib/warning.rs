5dfc9262fcfa5f85a067b2478d561856fa975a15
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2023-06-29 13:32:02 -0700

    feat: add DFX_WARNING mainnet_plaintext_identity (#3208)
    Make it possible to disable this warning:
    > The {} identity is not stored securely. Do not use it to control a lot of cycles/ICP. Create a new identity with `dfx identity new` and use it in mainnet-facing commands with the `--identity` flag
    
    Also added a documentation section for the `DFX_WARNING` environment variable to dfx-envvars.md.
    
    Fixes https://dfinity.atlassian.net/browse/SDK-962

---
+++ src/dfx/src/lib/warning.rs
@@ -0,0 +1,17 @@
+pub enum DfxWarning {
+    VersionCheck,
+    MainnetPlainTextIdentity,
+}
+
+pub fn is_warning_disabled(warning: DfxWarning) -> bool {
+    let warning = match warning {
+        DfxWarning::VersionCheck => "version_check",
+        DfxWarning::MainnetPlainTextIdentity => "mainnet_plaintext_identity",
+    };
+    // By default, warnings are all enabled.
+    let env_warnings = std::env::var("DFX_WARNING").unwrap_or_else(|_| "".to_string());
+    env_warnings
+        .split(',')
+        .filter(|w| w.starts_with('-'))
+        .any(|w| w.chars().skip(1).collect::<String>().eq(warning))
+}
