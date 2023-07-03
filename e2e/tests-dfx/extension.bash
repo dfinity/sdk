7af22e01abbc928bdfb78cf21b8c16cddebabf83
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2023-07-03 15:21:33 +0200

    add e2e tests

---
+++ e2e/tests-dfx/extension.bash
@@ -0,0 +1,99 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+    standard_setup
+    use_test_specific_cache_root
+}
+
+teardown() {
+    dfx_stop
+
+    standard_teardown
+}
+
+@test "install extension from official registry" {
+    assert_command_fail dfx snsx
+
+    assert_command dfx extension list
+    assert_match 'No extensions installed'
+
+    assert_command dfx extension install sns --install-as snsx
+    # TODO: how to capture spinner message?
+    # assert_match 'Successfully installed extension'
+
+    assert_command dfx extension list
+    assert_match 'snsx'
+
+    assert_command dfx --help
+    assert_match 'snsx.*Toolkit for'
+
+    assert_command dfx snsx --help
+
+    assert_command dfx extension uninstall snsx
+    # TODO: how to capture spinner message?
+    # assert_match 'Successfully uninstalled extension'
+
+    assert_command dfx extension list
+    assert_match 'No extensions installed'
+}
+
+@test "manually create extension" {
+    CACHE_DIR=$(dfx cache show)
+
+    assert_command dfx extension list
+    assert_match 'No extensions installed'
+
+    mkdir -p $CACHE_DIR/extensions/test_extension
+    echo "#!/usr/bin/env bash" > $CACHE_DIR/extensions/test_extension/test_extension
+    echo "echo test" > $CACHE_DIR/extensions/test_extension/test_extension
+    chmod +x $CACHE_DIR/extensions/test_extension/test_extension
+
+    assert_command dfx extension list
+    assert_match 'test_extension'
+
+    assert_command dfx extension run test_extension
+    assert_match 'test'
+
+    assert_command dfx test_extension
+    assert_match 'test'
+
+    assert_command dfx --help
+    assert_match "Missing \'extension.json\' file for extension \'test_extension\' \(exact location:.*/extensions/test_extension/extension.json\)"
+
+    assert_command dfx test_extension --help
+    assert_match "Missing \'extension.json\' file for extension \'test_extension\' \(exact location:.*/extensions/test_extension/extension.json\)"
+
+    echo "{}" > $CACHE_DIR/extensions/test_extension/extension.json
+
+    assert_command dfx --help
+    assert_match "Malformed extension manifest: Failed to parse contents of.*/extensions/test_extension/extension.json"
+
+    assert_command dfx test_extension --help
+    assert_match "Malformed extension manifest: Failed to parse contents of.*/extensions/test_extension/extension.json"
+
+    echo '{
+  "name": "test_extension",
+  "version": "0.1.0",
+  "homepage": "https://github.com/dfinity/dfx-extensions",
+  "authors": "DFINITY",
+  "summary": "Test extension for e2e purposes.",
+  "categories": [],
+  "keywords": []
+}' > $CACHE_DIR/extensions/test_extension/extension.json
+
+    assert_command dfx --help
+    assert_match "test_extension.*Test extension for e2e purposes."
+
+    assert_command dfx test_extension --help
+    assert_match "Test extension for e2e purposes..*Usage: dfx test_extension"
+
+    assert_command dfx extension uninstall test_extension
+    # TODO: how to capture spinner message?
+    # assert_match 'Successfully uninstalled extension'
+
+    assert_command dfx extension list
+    assert_match 'No extensions installed'
+}
+
