13aae2468338cdd1d141eecf219934e7fc3d0dec
adamspofford-dfinity <93943719+adamspofford-dfinity@users.noreply.github.com>
2023-08-08 17:42:35 +0000

    Deploying to public-manifest from  @ 32e13086870a2a95a821df9dcccefe72ed2aabe1 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
-SCRIPT_COMMIT_DESC="133ac66a7daec782e8033168ff873a224bd3127c"
+SCRIPT_COMMIT_DESC="32e13086870a2a95a821df9dcccefe72ed2aabe1"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
