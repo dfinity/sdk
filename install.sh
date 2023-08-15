f078b32b44d9dd55fa66d09afe79fa231af52ec4
mraszyk <31483726+mraszyk@users.noreply.github.com>
2023-08-15 09:04:58 +0000

    Deploying to public-manifest from  @ a31765c0eca0bb1094836d229956f52a044dcb6f 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
-SCRIPT_COMMIT_DESC="ae9344973ebce533762a58753c53ac209a43c7d3"
+SCRIPT_COMMIT_DESC="a31765c0eca0bb1094836d229956f52a044dcb6f"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
