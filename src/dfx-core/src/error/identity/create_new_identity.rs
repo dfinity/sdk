9b8aee2479e8cbb80a6b1155d36b6945b71aa592
Eric Swanson <eric.swanson@dfinity.org>
2023-08-17 13:13:32 -0700

    refactor: CreateNewIdentityError

---
+++ src/dfx-core/src/error/identity/create_new_identity.rs
@@ -0,0 +1,57 @@
+use crate::error::fs::FsError;
+use crate::error::identity::IdentityError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum CreateNewIdentityError {
+    #[error("Cannot create an anonymous identity.")]
+    CannotCreateAnonymousIdentity(),
+
+    #[error("Failed to clean up previous creation attempts: {0}")]
+    CleanupPreviousCreationAttemptsFailed(FsError),
+
+    #[error("Failed to create identity config: {0}")]
+    ConvertMnemonicToKeyFailed(IdentityError),
+
+    #[error("Convert secret key to sec1 Pem failed: {0}")]
+    ConvertSecretKeyToSec1PemFailed(Box<sec1::Error>),
+
+    #[error("Failed to create identity config: {0}")]
+    CreateIdentityConfigFailed(IdentityError),
+
+    #[error("Failed to create mnemonic from phrase: {0}")]
+    CreateMnemonicFromPhraseFailed(String),
+
+    #[error("Failed to create temporary identity directory: {0}")]
+    CreateTemporaryIdentityDirectoryFailed(FsError),
+
+    #[error("Failed to generate key: {0}")]
+    GenerateKeyFailed(IdentityError),
+
+    #[error("Identity already exists.")]
+    IdentityAlreadyExists(),
+
+    #[error("Failed to load pem file: {0}")]
+    LoadPemFromFileFailed(IdentityError),
+
+    #[error("Failed to remove identity: {0}")]
+    RemoveIdentityFailed(IdentityError),
+
+    #[error("Failed to rename temporary directory to permanent identity directory: {0}")]
+    RenameTemporaryIdentityDirectoryFailed(FsError),
+
+    #[error("Failed to save identity configuration: {0}")]
+    SaveIdentityConfigurationFailed(IdentityError),
+
+    #[error("Failed to save pem: {0}")]
+    SavePemFailed(IdentityError),
+
+    #[error("Failed to switch back over to the identity you're replacing: {0}")]
+    SwitchBackToIdentityFailed(IdentityError),
+
+    #[error("Failed to temporarily switch over to anonymous identity: {0}")]
+    SwitchToAnonymousIdentityFailed(IdentityError),
+
+    #[error("Failed to validate pem file: {0}")]
+    ValidatePemFileFailed(IdentityError),
+}
