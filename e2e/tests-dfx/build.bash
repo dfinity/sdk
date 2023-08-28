f4f698e9dca188f0aee20105e88835eae11bd2f5
Kento Sugama <kento.sugama@dfinity.org>
2023-08-28 16:10:10 -0400

    e2e test

--- e2e/tests-dfx/build.bash
+++ e2e/tests-dfx/build.bash
@@ -106,6 +106,15 @@ teardown() {
     assert_match "10World"
 }
 
+@test "build supports auto-generated idl for management canister imports in motoko" {
+    install_asset motoko_management
+    dfx_start
+    dfx canister create --all
+    assert_command dfx build
+    dfx deploy
+    assert_command dfx canister call e2e_project_backend rand
+}
+
 @test "build succeeds on default project" {
     dfx_start
     dfx canister create --all
