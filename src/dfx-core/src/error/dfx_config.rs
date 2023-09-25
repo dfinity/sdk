88164b52fa5bc1ef7dee5b8335c9f3fe8eefe90c
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2023-09-25 11:51:29 -0700

    refactor: split up DfxConfigError into method-specific error types (#3384)
    Eliminates some boxing and makes it more clear what errors are possible for any given method.

--- src/dfx-core/src/error/dfx_config.rs
+++ src/dfx-core/src/error/dfx_config.rs
@@ -2,31 +2,58 @@ use candid::Principal;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum DfxConfigError {
+pub enum AddDependenciesError {
     #[error("Circular canister dependencies: {}", _0.join(" -> "))]
     CanisterCircularDependency(Vec<String>),
 
     #[error("Canister '{0}' not found in dfx.json.")]
     CanisterNotFound(String),
+}
 
+#[derive(Error, Debug)]
+pub enum GetCanisterConfigError {
     #[error("No canisters in the configuration file.")]
     CanistersFieldDoesNotExist(),
 
-    #[error("Failed to get canisters with their dependencies (for {}): {1}", _0.as_deref().unwrap_or("all canisters"))]
-    GetCanistersWithDependenciesFailed(Option<String>, Box<DfxConfigError>),
+    #[error("Canister '{0}' not found in dfx.json.")]
+    CanisterNotFound(String),
+}
 
+#[derive(Error, Debug)]
+pub enum GetCanisterNamesWithDependenciesError {
+    #[error("No canisters in the configuration file.")]
+    CanistersFieldDoesNotExist(),
+
+    #[error("Failed to add dependencies for canister '{0}': {1}")]
+    AddDependenciesFailed(String, AddDependenciesError),
+}
+
+#[derive(Error, Debug)]
+pub enum GetComputeAllocationError {
     #[error("Failed to get compute allocation for canister '{0}': {1}")]
-    GetComputeAllocationFailed(String, Box<DfxConfigError>),
+    GetComputeAllocationFailed(String, GetCanisterConfigError),
+}
 
+#[derive(Error, Debug)]
+pub enum GetFreezingThresholdError {
     #[error("Failed to get freezing threshold for canister '{0}': {1}")]
-    GetFreezingThresholdFailed(String, Box<DfxConfigError>),
+    GetFreezingThresholdFailed(String, GetCanisterConfigError),
+}
 
+#[derive(Error, Debug)]
+pub enum GetMemoryAllocationError {
     #[error("Failed to get memory allocation for canister '{0}': {1}")]
-    GetMemoryAllocationFailed(String, Box<DfxConfigError>),
+    GetMemoryAllocationFailed(String, GetCanisterConfigError),
+}
 
-    #[error("Failed to figure out if canister '{0}' has a remote id on network '{1}': {2}")]
-    GetRemoteCanisterIdFailed(Box<String>, Box<String>, Box<DfxConfigError>),
-
+#[derive(Error, Debug)]
+pub enum GetPullCanistersError {
     #[error("Pull dependencies '{0}' and '{1}' have the same canister ID: {2}")]
     PullCanistersSameId(String, String, Principal),
+}
+
+#[derive(Error, Debug)]
+pub enum GetRemoteCanisterIdError {
+    #[error("Failed to figure out if canister '{0}' has a remote id on network '{1}': {2}")]
+    GetRemoteCanisterIdFailed(Box<String>, Box<String>, GetCanisterConfigError),
 }
