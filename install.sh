7e3b68d9a70fae07c2baeb4b62065eb480409d63
chenyan-dfinity <48968912+chenyan-dfinity@users.noreply.github.com>
2024-01-19 23:59:07 +0000

    Deploying to public-manifest from  @ c99e0d9b3de279686d6792a22da664fa2225c07e 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
-SCRIPT_COMMIT_DESC="f936852861662b235287cd274c96f7e9779690cd"
+SCRIPT_COMMIT_DESC="c99e0d9b3de279686d6792a22da664fa2225c07e"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
