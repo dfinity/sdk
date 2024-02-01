25440cacffc7a3e15ad0e77b0bf0e5129bd8f93e
ericswanson-dfinity <64809312+ericswanson-dfinity@users.noreply.github.com>
2024-02-01 19:18:24 +0000

    Deploying to public-manifest from  @ fb2e4b0a37eb8adc092a06b1c53bfb1c591e5fa9 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
-SCRIPT_COMMIT_DESC="92e3dfd6ce7f4072cb526a867adfaa3b05c6ae91"
+SCRIPT_COMMIT_DESC="fb2e4b0a37eb8adc092a06b1c53bfb1c591e5fa9"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
