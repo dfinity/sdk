34c208006384a160a3ee17a9e6640fa59abe4395
ericswanson-dfinity <64809312+ericswanson-dfinity@users.noreply.github.com>
2024-02-07 10:44:01 +0000

    Deploying to public-manifest from  @ 54b812543abf93237c7a11b08ea5d56d9424070a 🚀

--- install.sh
+++ install.sh
@@ -239,7 +239,7 @@ DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="0b1f94ea4ee9a6c449127d49baf7d635bd369996"
+SCRIPT_COMMIT_DESC="54b812543abf93237c7a11b08ea5d56d9424070a"
 
 # Get the version of a tag from the manifest JSON file.
 # Arguments:
