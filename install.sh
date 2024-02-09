97e9dcc156e009bbea16100171d1251f623e1b9c
lwshang <github@lwshang.com>
2024-02-09 22:27:30 +0000

    Deploying to dfxvm-install-script from  @ fb848ae0326f9bd378234e1cddd97c118ba64e64 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="c7fa9267950836fb2e8af2f03a6531ead9289f81"
+SCRIPT_COMMIT_DESC="fb848ae0326f9bd378234e1cddd97c118ba64e64"
 
 download_and_install() {
     SHASUM="$1"
