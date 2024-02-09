d44ad61844e205d06f77b6e0aff8d2f15283cc83
sesi200 <severin.siffert@dfinity.org>
2024-02-09 09:25:28 +0000

    Deploying to dfxvm-install-script from  @ c7fa9267950836fb2e8af2f03a6531ead9289f81 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="bd137299103744166082b77766d845f7b3e7e9f0"
+SCRIPT_COMMIT_DESC="c7fa9267950836fb2e8af2f03a6531ead9289f81"
 
 download_and_install() {
     SHASUM="$1"
