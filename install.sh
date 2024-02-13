35070cf33e3a71109c9dbfa3222d4fdce5fef2b5
krpeacock <kylpeacock@gmail.com>
2024-02-13 00:38:23 +0000

    Deploying to public-manifest from  @ f87e6afad5c4dc57806144d7c2e7a26353f54182 🚀

--- install.sh
+++ install.sh
@@ -239,7 +239,7 @@ DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="fb848ae0326f9bd378234e1cddd97c118ba64e64"
+SCRIPT_COMMIT_DESC="f87e6afad5c4dc57806144d7c2e7a26353f54182"
 
 # Get the version of a tag from the manifest JSON file.
 # Arguments:
