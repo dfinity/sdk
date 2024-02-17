2fe080c51a8e07dc3ac542691dfd248fd2f95ac1
jessiemongeon1 <133128541+jessiemongeon1@users.noreply.github.com>
2024-02-17 16:46:44 +0000

    Deploying to public-manifest from  @ bf7322741520f31a4d3484e391dbfc359829ef43 🚀

--- install.sh
+++ install.sh
@@ -239,7 +239,7 @@ DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="df4b6282444975a9bd8c7a74ebb219ce0a1f8c9b"
+SCRIPT_COMMIT_DESC="bf7322741520f31a4d3484e391dbfc359829ef43"
 
 # Get the version of a tag from the manifest JSON file.
 # Arguments:
