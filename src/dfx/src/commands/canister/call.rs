5afaf80c6e117b6823af27241fd794c83ec61484
Adam Spofford <adam.spofford@dfinity.org>
2024-02-21 15:07:26 -0800

    Add better error message for canister-only methods

--- src/dfx/src/commands/canister/call.rs
+++ src/dfx/src/commands/canister/call.rs
@@ -8,7 +8,7 @@ use crate::util::{
     arguments_from_file, blob_from_arguments, fetch_remote_did_file, get_candid_type,
     print_idl_blob,
 };
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, bail, Context};
 use candid::Principal as CanisterId;
 use candid::{CandidType, Decode, Deserialize, Principal};
 use candid_parser::utils::CandidSource;
@@ -211,6 +211,12 @@ pub fn get_effective_canister_id(
                 let in_args = Decode!(arg_value, In)
                     .context("Argument is not valid for InstallChunkedCode")?;
                 Ok(in_args.target_canister)
+            }
+            MgmtMethod::BitcoinGetBalance | MgmtMethod::BitcoinGetBalanceQuery
+            | MgmtMethod::BitcoinGetUtxos | MgmtMethod::BitcoinGetUtxosQuery
+            | MgmtMethod::BitcoinSendTransaction | MgmtMethod::BitcoinGetCurrentFeePercentiles
+            | MgmtMethod::EcdsaPublicKey | MgmtMethod::SignWithEcdsa => {
+                bail!("Management canister method {method_name} can only be run from canisters");
             }
         }
     } else {
