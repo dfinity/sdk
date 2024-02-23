4275f55ab57054be2d764acda3eb1d08d5aa65cd
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-02-23 14:49:22 +0100

    fix formatting bytes

--- src/dfx/src/commands/canister/tail.rs
+++ src/dfx/src/commands/canister/tail.rs
@@ -21,18 +21,25 @@ fn format_canister_logs(logs: FetchCanisterLogsResponse) -> Vec<String> {
         .map(|r| {
             let time = chrono::NaiveDateTime::from_timestamp_nanos(r.timestamp_nanos as i64)
                 .expect("invalid timestamp");
+            fn format_bytes(bytes: &[u8]) -> String {
+                format!(
+                    "[{}]",
+                    bytes
+                        .iter()
+                        .map(|&v| format!("{}", v))
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                )
+            }
             let message = match String::from_utf8(r.content.clone()) {
-                Ok(s) => s,
-                Err(_) => {
-                    format!(
-                        "[{}]",
-                        r.content
-                            .iter()
-                            .map(|v| format!("{}", v))
-                            .collect::<Vec<_>>()
-                            .join(", ")
-                    )
+                Ok(s) => {
+                    if format!("{s:?}").contains("\\u{") {
+                        format_bytes(&r.content)
+                    } else {
+                        s
+                    }
                 }
+                Err(_) => format_bytes(&r.content),
             };
             format!("[{}. {}]: {}", r.idx, time, message)
         })
@@ -47,12 +54,12 @@ fn test_format_canister_logs() {
         canister_log_records: vec![
             CanisterLogRecord {
                 idx: 42,
-                timestamp_nanos: 1620328630000000000,
+                timestamp_nanos: 1620328630000000001,
                 content: b"some text message".to_vec(),
             },
             CanisterLogRecord {
                 idx: 43,
-                timestamp_nanos: 1620328630000123456,
+                timestamp_nanos: 1620328630000000002,
                 content: vec![1, 2, 3],
             },
         ],
@@ -60,8 +67,8 @@ fn test_format_canister_logs() {
     assert_eq!(
         format_canister_logs(logs),
         vec![
-            "[42. 2021-05-06 19:17:10.000000000]: some text message".to_string(),
-            "[43. 2021-05-06 19:17:10.000123456]: \u{1}\u{2}\u{3}".to_string(),
+            "[42. 2021-05-06 19:17:10.000000001]: some text message".to_string(),
+            "[43. 2021-05-06 19:17:10.000000002]: [1, 2, 3]".to_string(),
         ],
     );
 }
