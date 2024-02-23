245849a588353561ad79e14f9da1e67787f0643e
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-02-23 14:22:11 +0100

    add format_canister_logs

--- src/dfx/src/commands/canister/tail.rs
+++ src/dfx/src/commands/canister/tail.rs
@@ -5,6 +5,7 @@ use crate::lib::root_key::fetch_root_key_if_needed;
 use candid::Principal;
 use clap::Parser;
 use dfx_core::identity::CallSender;
+use ic_utils::interfaces::management_canister::FetchCanisterLogsResponse;
 use slog::info;
 
 /// Get the canister logs.
@@ -12,6 +13,57 @@ use slog::info;
 pub struct TailOpts {
     /// Specifies the name or id of the canister to get its canister information.
     canister: String,
+}
+
+fn format_canister_logs(logs: FetchCanisterLogsResponse) -> Vec<String> {
+    logs.canister_log_records
+        .into_iter()
+        .map(|r| {
+            let time = chrono::NaiveDateTime::from_timestamp_nanos(r.timestamp_nanos as i64)
+                .expect("invalid timestamp");
+            let message = match String::from_utf8(r.content.clone()) {
+                Ok(s) => s,
+                Err(_) => {
+                    format!(
+                        "[{}]",
+                        r.content
+                            .iter()
+                            .map(|v| format!("{}", v))
+                            .collect::<Vec<_>>()
+                            .join(", ")
+                    )
+                }
+            };
+            format!("[{}. {}]: {}", r.idx, time, message)
+        })
+        .collect()
+}
+
+#[test]
+fn test_format_canister_logs() {
+    use ic_utils::interfaces::management_canister::CanisterLogRecord;
+
+    let logs = FetchCanisterLogsResponse {
+        canister_log_records: vec![
+            CanisterLogRecord {
+                idx: 42,
+                timestamp_nanos: 1620328630000000000,
+                content: b"some text message".to_vec(),
+            },
+            CanisterLogRecord {
+                idx: 43,
+                timestamp_nanos: 1620328630000123456,
+                content: vec![1, 2, 3],
+            },
+        ],
+    };
+    assert_eq!(
+        format_canister_logs(logs),
+        vec![
+            "[42. 2021-05-06 19:17:10.000000000]: some text message".to_string(),
+            "[43. 2021-05-06 19:17:10.000123456]: \u{1}\u{2}\u{3}".to_string(),
+        ],
+    );
 }
 
 pub async fn exec(env: &dyn Environment, opts: TailOpts, call_sender: &CallSender) -> DfxResult {
@@ -27,7 +79,7 @@ pub async fn exec(env: &dyn Environment, opts: TailOpts, call_sender: &CallSende
 
     let logs = canister::get_canister_logs(env, canister_id, call_sender).await?;
 
-    info!(log, "{logs}");
+    info!(log, "{}", format_canister_logs(logs).join("\n"));
 
     Ok(())
 }
