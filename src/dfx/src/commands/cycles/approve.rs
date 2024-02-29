6e93b0c3d514364a6032b84b77393d92a75a0bb4
Severin Siffert <severin.siffert@dfinity.org>
2024-02-29 14:48:57 +0100

    Use the right Subaccount type in dfx cycles commands

--- src/dfx/src/commands/cycles/approve.rs
+++ src/dfx/src/commands/cycles/approve.rs
@@ -1,11 +1,11 @@
-use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
-use crate::lib::nns_types::account_identifier::Subaccount;
 use crate::lib::operations::cycles_ledger;
 use crate::lib::root_key::fetch_root_key_if_needed;
 use crate::util::clap::parsers::cycle_amount_parser;
+use crate::{lib::environment::Environment, util::clap::parsers::icrc_subaccount_parser};
 use candid::Principal;
 use clap::Parser;
+use icrc_ledger_types::icrc1::account::Subaccount;
 use slog::warn;
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -20,11 +20,11 @@ pub struct ApproveOpts {
     amount: u128,
 
     /// Allow this subaccount to spend cycles.
-    #[arg(long)]
+    #[arg(long, value_parser = icrc_subaccount_parser)]
     spender_subaccount: Option<Subaccount>,
 
     /// Approve cycles to be spent from this subaccount.
-    #[arg(long)]
+    #[arg(long, value_parser = icrc_subaccount_parser)]
     from_subaccount: Option<Subaccount>,
 
     /// The number of previously approved cycles.
@@ -58,15 +58,13 @@ pub async fn exec(env: &dyn Environment, opts: ApproveOpts) -> DfxResult {
             .as_nanos() as u64,
     );
 
-    let spender_subaccount = opts.spender_subaccount.map(|x| x.0);
-    let from_subaccount = opts.from_subaccount.map(|x| x.0);
     let result = cycles_ledger::approve(
         agent,
         env.get_logger(),
         opts.amount,
         opts.spender,
-        spender_subaccount,
-        from_subaccount,
+        opts.spender_subaccount,
+        opts.from_subaccount,
         opts.expected_allowance,
         opts.expires_at,
         created_at_time,
