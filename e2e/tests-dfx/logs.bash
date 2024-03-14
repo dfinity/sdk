85ec908efc19608314078e41f797aa6381ff354b
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-03-14 14:26:49 +0000

    add logs grep/tail tests

--- e2e/tests-dfx/logs.bash
+++ e2e/tests-dfx/logs.bash
@@ -27,3 +27,31 @@ teardown() {
   assert_contains "Hello, Alice!"
   assert_contains "Hello, Bob!"
 }
+
+@test "fetching canister logs is grepable" {
+  install_asset logs
+  dfx_start
+  dfx canister create --all
+  dfx build
+  dfx canister install e2e_project
+  dfx canister call e2e_project hello Alice
+  dfx canister call e2e_project hello Bob
+  sleep 2
+  assert_command dfx canister logs e2e_project | grep Alice
+  assert_contains "Hello, Alice!"
+  assert_not_contains "Hello, Bob!"
+}
+
+@test "fetching canister logs is tailable" {
+  install_asset logs
+  dfx_start
+  dfx canister create --all
+  dfx build
+  dfx canister install e2e_project
+  dfx canister call e2e_project hello Alice
+  dfx canister call e2e_project hello Bob
+  sleep 2
+  assert_command dfx canister logs e2e_project | tail -n 1
+  assert_not_contains "Hello, Alice!"
+  assert_contains "Hello, Bob!"
+}
