6fe5b6d13373303a5f1b9ad45d1d693a08e0a0d6
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-03-14 19:54:12 +0000

    fix e2e tests

--- e2e/tests-dfx/logs.bash
+++ e2e/tests-dfx/logs.bash
@@ -28,21 +28,11 @@ teardown() {
   assert_contains "Hello, Bob!"
 }
 
-@test "fetching canister logs is grepable" {
-  install_asset logs
-  dfx_start
-  dfx canister create --all
-  dfx build
-  dfx canister install e2e_project
-  dfx canister call e2e_project hello Alice
-  dfx canister call e2e_project hello Bob
-  sleep 2
-  assert_command dfx canister logs e2e_project | grep Alice
-  assert_contains "Hello, Alice!"
-  assert_not_contains "Hello, Bob!"
+dfx_canister_logs_grep_Alice() {
+  dfx canister logs e2e_project | grep Alice
 }
 
-@test "fetching canister logs is tailable" {
+@test "fetching canister logs output is grep-able" {
   install_asset logs
   dfx_start
   dfx canister create --all
@@ -51,7 +41,25 @@ teardown() {
   dfx canister call e2e_project hello Alice
   dfx canister call e2e_project hello Bob
   sleep 2
-  assert_command dfx canister logs e2e_project | tail -n 1
-  assert_not_contains "Hello, Alice!"
-  assert_contains "Hello, Bob!"
+  assert_command dfx_canister_logs_grep_Alice
+  assert_contains "Alice"
+  assert_not_contains "Bob"
+}
+
+dfx_canister_logs_tail_n_1() {
+  echo "$(dfx canister logs e2e_project)" | tail -n 1
+}
+
+@test "fetching canister logs output is tail-able" {
+  install_asset logs
+  dfx_start
+  dfx canister create --all
+  dfx build
+  dfx canister install e2e_project
+  dfx canister call e2e_project hello Alice
+  dfx canister call e2e_project hello Bob
+  sleep 2
+  assert_command dfx_canister_logs_tail_n_1
+  assert_not_contains "Alice"
+  assert_contains "Bob"
 }
