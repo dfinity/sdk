2d0c6cb952cd81fce45db114ee925d8662c66700
Kyle Peacock <kylpeacock@gmail.com>
2024-03-27 14:09:03 -0700

    parsed identity delegation from string

--- src/dfx/src/commands/identity/login.rs
+++ src/dfx/src/commands/identity/login.rs
@@ -4,14 +4,12 @@ use clap::Parser;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::{FuzzySelect, Input};
 use ic_agent::identity::Delegation;
-use serde::{Deserialize, Serialize};
-use std::str::FromStr;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum LoginError {
-    #[error("An error occurred while managing identities: {0}")]
-    IdentityManagerError(String),
+    // #[error("An error occurred while managing identities: {0}")]
+    // IdentityManagerError(String),
     #[error("An error occurred while parsing the identity: {0}")]
     IdentityError(String),
 }
@@ -51,37 +49,92 @@ pub fn exec(env: &dyn Environment, _opts: LoginOpts) -> DfxResult {
     Ok(())
 }
 
-//  //   baseIdentity: "default",
-//   delegations: [
-//     {
-//       delegation: {
-//         expiration: '1655f29d787c0000',
-//         pubkey: '302a...',
-//         targets: [ '00000000002000030101' ]
-//       },
-//       signature: 'ba46e...'
-//     }
-//   ],
 
-
-
-#[derive(Parser, Serialize, Deserialize, Debug)]
+#[derive(Debug, candid::Deserialize, serde::Serialize)]
 struct IdentityDelegation {
     base_identity: String,
     delegations: Vec<Delegation>,
 }
 
-impl FromStr for IdentityDelegation {
-    type Err = std::num::ParseIntError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split(',').collect();
-        Ok(Delegation {
-            expiration: parts[0].to_string(),
-            // hex decode
-            pubkey: hex::decode(parts[1]).unwrap(),
-            targets: parts[2].split(';').map(|s| s.to_string()).collect(),
-            signature: parts[3].to_string(),
+#[derive(Debug, candid::Deserialize, serde::Serialize)]
+struct JSONDelegation {
+    expiration: String,
+    pubkey: String,
+}
+
+#[derive(Debug, candid::Deserialize, serde::Serialize)]
+struct SignedJSONDelegation {
+   delegation: JSONDelegation,
+    signature: String,
+}
+#[derive(Debug, candid::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+struct JSONDelegationChain {
+    delegations: Vec<SignedJSONDelegation>,
+    public_key: String,
+}
+
+impl JSONDelegationChain {
+    pub fn to_identity_delegation(&self) -> Result<IdentityDelegation, LoginError> {
+        let base_identity = self.public_key.clone();
+        let delegations = self.delegations.iter().map(|d| {
+            // validate signature
+
+
+
+            // convert from string to u64
+            let expiration = u64::from_str_radix(&d.delegation.expiration, 16)
+                .map_err(|err| LoginError::IdentityError(err.to_string()))?;
+            // check if expiration is a valid timestamp (not in the past, not too far in the future)
+
+            let now_in_nanos = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .map_err(|err| LoginError::IdentityError(err.to_string()))?
+                .as_nanos();
+
+            if expiration < now_in_nanos as u64 {
+                return Err(LoginError::IdentityError("Invalid delegation. This delegation has expired. Please request a fresh delegation and try again".to_string()));
+            }
+
+            let pubkey = hex::decode(&d.delegation.pubkey)
+                .map_err(|err| LoginError::IdentityError(err.to_string()))?;
+
+            let delegation = Delegation {
+                expiration: expiration,
+                pubkey: pubkey,
+                targets: Option::None,
+            };
+            Ok(delegation)
+        }).collect::<Result<Vec<Delegation>, LoginError>>()?;
+
+        Ok(IdentityDelegation {
+            base_identity,
+            delegations,
         })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::commands::identity::login::JSONDelegationChain;
+
+
+    #[test]
+    fn test_parse_identity_delegation() {
+        let s =  r#"{
+        "delegations":[{
+            "delegation": {
+                    "expiration":"17c106823a0b91b8","pubkey":"3059301306072a8648ce3d020106082a8648ce3d030107034200046bba01230defcec1c4ecc15d57d81410acfd8000fdec74b72fc09849fb2a6a26d309f8eadb06fd8a51f23408ac9563b81d0ef6568127fb239cfb0790a9c20c68"
+            },
+            "signature":"d9d9f7a26b63657274696669636174655901d7d9d9f7a2647472656583018301830183024863616e697374657283018301830183024a000000000000000701018301830183024e6365727469666965645f64617461820358206e3432e09b691812eb8e34c73535666e2ab2a2fa8acba5e468f269c1963574fc82045820eeebdca966b95b97cecdf624e005758a11b5d031b574818565faaa89fc5a258b820458205d905b85682bddd934e155c02a115b5ce2efe0aacb2b56c181a9f6b27c62194182045820af7540e4390f6a39c132b0c0044b0f2c3b5acf74e08b80029e575a7c38fd88a682045820fc22fa3e1a4a8ca24dc3cb14e9eed256b3881abb3cd0542e58dce0bafe7dcd6e8204582087e17fd396c410957630bf1a1f0afbfd29e3713361db1f5938508b4a7812c0cc820458200a5a0c8dc5ca2755078b3ab0d7a7b18fde686f4ce8af86625e4c780b266b383882045820fc291a2424a75f805247f7981ebc6bea10b0e52ce1f09f522903aa083eba6982830182045820ce754d5cf162883acf90ed966f98487c35f9abec8c4df7b1760adea872d1b00583024474696d65820349b8d9a3fddcfdade017697369676e61747572655830832f346060e5940b061e93491cb98eb5f5434eb912b39fddf8b4e4162d0f6f75260768c0156c227e9fdd03e239fbc53b64747265658301820458208581a09c85459df0ba918296790d18fddbf8df6143499b218add0da08876b3cd830243736967830258201cd61b12d26b0d78181d86049fef261832d6a790b9c2ca09b69d8318c3a04ec58302582079a54b5de0621b02e88214e9631a8e189e3a3d8a6909cd786286a56638da534a820340"
+        }],
+        "publicKey":"303c300c060a2b0601040183b8430102032c000a0000000000000007010103b7e1806206cc258fab4f8ac57ede1a4946026308cf92f28f4597243351e776"}"#;
+
+        let chain: Result<JSONDelegationChain, serde_json::Error> = serde_json::from_str(s);
+
+        let id = chain.unwrap().to_identity_delegation().unwrap();
+        println!("{:?}", id);
     }
 }
