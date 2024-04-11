143a822f4a251823d951531c4a0b9699f41a958a
lwshang <github@lwshang.com>
2024-04-11 01:03:59 +0000

    Deploying to public-manifest from  @ ddd01458bc6cae61402de37930dd4bca48183aac 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="2f3da799f1fe803664dccfb968cd9fe8d4d41b1b"
+SCRIPT_COMMIT_DESC="ddd01458bc6cae61402de37930dd4bca48183aac"
 
 download_and_install() {
     SHASUM="$1"
