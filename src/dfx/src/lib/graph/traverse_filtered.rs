f6c0849f73e28e19084a0b53f82705d1ef2be811
Victor Porton <porton.victor@gmail.com>
2024-04-18 05:05:35 +0300

    removed unneeded code

--- src/dfx/src/lib/graph/traverse_filtered.rs
+++ src/dfx/src/lib/graph/traverse_filtered.rs
@@ -1,40 +1,7 @@
 use std::iter::once;
 
-// TODO: Somebody, adopt this code to `petgraph`.
-use petgraph::{data::DataMap, visit::{Bfs, Dfs, IntoNeighbors, VisitMap}};
-
-#[allow(unused)]
-pub struct DfsFiltered<NodeId, VM> {
-    base: Dfs<NodeId, VM>,
-    // node_filter: P,
-}
-
-impl<NodeId, VM> DfsFiltered<NodeId, VM> {
-    #[allow(unused)]
-    pub fn new(base: Dfs<NodeId, VM>) -> Self {
-        Self {
-            base
-        }
-    }
-
-    #[allow(unused)]
-    pub fn traverse<G, P, C, NodeWeight>(&mut self, graph: G, mut predicate: P, mut call: C)
-    where C: FnMut(&NodeId, &NodeId) -> (),
-          G: IntoNeighbors<NodeId = NodeId> + DataMap<NodeWeight = NodeWeight>,
-          P: FnMut(&NodeId) -> bool,
-          NodeId: Copy + Eq,
-          VM: VisitMap<NodeId>,
-    {
-        while let Some(source_item_id) = &self.base.next(graph) {
-            if (&mut predicate)(source_item_id) {
-                let source_parent_id = self.base.stack.iter().map(|e| *e).rev().find(&mut predicate); // FIXME: `rev()` here?
-                if let Some(source_parent_id) = &source_parent_id {
-                    (&mut call)(source_parent_id, &source_item_id);
-                }
-            }
-        }
-    }
-}
+// TODO: Somebody, adopt this code (and DFS) to `petgraph`.
+use petgraph::{data::DataMap, visit::{Bfs, IntoNeighbors, VisitMap}};
 
 pub struct BfsFiltered<NodeId, VM> {
     base: Bfs<NodeId, VM>,
