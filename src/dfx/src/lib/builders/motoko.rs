2620184b17c314fde2a466e36e0b5222e409ca23
Victor Porton <porton.victor@gmail.com>
2024-04-21 23:52:29 +0300

    removed old version of a function

--- src/dfx/src/lib/builders/motoko.rs
+++ src/dfx/src/lib/builders/motoko.rs
@@ -13,9 +13,7 @@ use anyhow::Context;
 use candid::Principal as CanisterId;
 use dfx_core::config::cache::Cache;
 use dfx_core::config::model::dfinity::{MetadataVisibility, Profile};
-use dfx_core::fs::metadata;
 use fn_error_context::context;
-use petgraph::visit::Bfs;
 use slog::{info, o, trace, warn, Logger};
 use std::collections::BTreeMap;
 use std::convert::TryFrom;
@@ -40,117 +38,6 @@ impl MotokoBuilder {
             cache: env.get_cache(),
         })
     }
-
-    // FIXME: Remove the function from here, because it has been copied to `lib::builders`
-    fn should_build(
-        &self,
-        pool: &CanisterPool,
-        canister_info: &CanisterInfo,
-    ) -> DfxResult<bool> {
-        let motoko_info = canister_info.as_info::<MotokoCanisterInfo>()?; // TODO: Remove.
-        let output_wasm_path = canister_info.get_output_wasm_path();
-
-        // from principal to name:
-        let rev_id_map: BTreeMap<String, String> = pool
-            .get_canister_list()
-            .iter()
-            .map(|c| (c.canister_id().to_text(), c.get_name().to_string()))
-            .collect();
-
-        let cache = &self.cache;
-
-        add_imports(cache.as_ref(), canister_info, &mut *pool.imports.borrow_mut(), pool)?;
-
-        // Check that one of the dependencies is newer than the target:
-        if let Ok(wasm_file_metadata) = metadata(output_wasm_path) {
-            let wasm_file_time = wasm_file_metadata.modified()?;
-            let mut imports = pool.imports.borrow_mut();
-            let start = if let Some(node_index) = imports.nodes.get(&Import::Canister(canister_info.get_name().to_string())) {
-                *node_index
-            } else {
-                let node = Import::Relative(motoko_info.get_main_path().to_path_buf());
-                let node_index = imports.graph.add_node(node.clone());
-                imports.nodes.insert(node, node_index);
-                node_index
-            };
-            let mut import_iter = Bfs::new(&imports.graph, start);
-            loop {
-                if let Some(import) = import_iter.next(&imports.graph) {
-                    let imported_file = match &imports.graph[import] {
-                        Import::Canister(canister_name) => { // duplicate code
-                            if let Some(canister) = pool.get_first_canister_with_name(canister_name.as_str()) {
-                                let main_file = canister.get_info().get_main_file();
-                                if let Some(main_file) = main_file {
-                                    Some(main_file.to_owned())
-                                } else {
-                                    None
-                                }
-                            } else {
-                                None
-                            }
-                        }
-                        Import::Ic(canister_id) => {
-                            if let Some(canister_name) = rev_id_map.get(canister_id.as_str()) {
-                                if let Some(canister) = pool.get_first_canister_with_name(canister_name) {
-                                    if let Some(main_file) = canister.get_info().get_main_file() {
-                                        Some(main_file.to_owned())
-                                    } else {
-                                        None
-                                    }
-                                } else {
-                                    None
-                                }
-                            } else {
-                                None
-                            }
-                        }
-                        Import::Lib(_path) => {
-                            // Skip libs, all changes by package managers don't modify existing directories but create new ones.
-                            continue;
-                        //     let i = path.find('/');
-                        //     let pre_path = if let Some(i) = i {
-                        //         let expanded = Path::new(
-                        //             package_arguments_map.get(&path[..i]).ok_or_else(|| anyhow!("nonexisting package"))?
-                        //         );
-                        //         expanded.join(&path[i+1..])
-                        //     } else {
-                        //         Path::new(path.as_str()).to_owned()
-                        //     };
-                        //     let path2 = pre_path.to_str().unwrap().to_owned() + ".mo";
-                        //     let path2 = path2.to_string();
-                        //     let path2 = Path::new(&path2);
-                        //     if path2.exists() { // TODO: Is it correct order of two variants?
-                        //         Some(Path::new(path2).to_owned())
-                        //     } else {
-                        //         let path3 = pre_path.join(Path::new("lib.mo"));
-                        //         if path3.exists() {
-                        //             Some(path3.to_owned())
-                        //         } else {
-                        //             bail!("source file has been deleted");
-                        //         }
-                        //     }
-                        }
-                        Import::Relative(path) => {
-                            Some(Path::new(&path).to_owned())
-                        }
-                    };
-                    if let Some(imported_file) = imported_file {
-                        let imported_file_metadata = metadata(imported_file.as_ref())?;
-                        let imported_file_time = imported_file_metadata.modified()?;
-                        if imported_file_time > wasm_file_time {
-                            break;
-                        };
-                    };
-                } else {
-                    trace!(self.logger, "Canister {} already compiled.", canister_info.get_name());
-                    return Ok(false);
-                }
-            }
-        };
-    
-        Ok(true)
-    }
-
 }
 
 /// Add imports originating from canister `info` to the graph `imports` of dependencies.
@@ -259,7 +146,7 @@ impl CanisterBuilder for MotokoBuilder {
         let input_path = motoko_info.get_main_path();
         let output_wasm_path = canister_info.get_output_wasm_path();
 
-        if !self.should_build(pool, canister_info)? {
+        if !self.should_build(pool, canister_info, self.cache.as_ref())? {
             return Ok(BuildOutput { // duplicate code
                 canister_id: canister_info
                     .get_canister_id()
