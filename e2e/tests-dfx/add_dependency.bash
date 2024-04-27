c42605caac38f77bd4c6ea7ceb2622b97414f291
Victor Porton <porton.victor@gmail.com>
2024-04-27 18:20:06 +0300

    added (failing) test add_dependency.bash

---
+++ e2e/tests-dfx/add_dependency.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bats
+
+load ../utils/_
+
+setup() {
+  standard_setup
+
+  install_asset add_dependency
+}
+
+teardown() {
+  dfx_stop
+
+  standard_teardown
+}
+
+@test "compiles after correcting dependency" {
+  install_asset base
+
+  dfx_start
+
+  # fails
+  dfx deploy || true
+
+  cp dfx_corrected.json dfx.json
+
+  assert_command dfx deploy
+}
