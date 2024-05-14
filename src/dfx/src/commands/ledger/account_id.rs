e76a38b376d630ebe2a94b34d5b14e2557840001
Martin Raszyk <martin.raszyk@dfinity.org>
2024-05-14 13:30:43 +0200

    use conflicts_with

--- src/dfx/src/commands/ledger/account_id.rs
+++ src/dfx/src/commands/ledger/account_id.rs
@@ -1,7 +1,7 @@
 use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 use crate::lib::nns_types::account_identifier::{AccountIdentifier, Subaccount};
-use anyhow::{anyhow, Context};
+use anyhow::Context;
 use candid::Principal;
 use clap::Parser;
 
@@ -12,7 +12,7 @@ pub struct AccountIdOpts {
     /// Principal controlling the account.
     pub of_principal: Option<Principal>,
 
-    #[arg(long, value_name = "ALIAS")]
+    #[arg(long, value_name = "ALIAS", conflicts_with("of_principal"))]
     /// Alias or principal of the canister controlling the account.
     pub of_canister: Option<String>,
 
@@ -20,18 +20,17 @@ pub struct AccountIdOpts {
     /// Subaccount identifier (64 character long hex string).
     pub subaccount: Option<Subaccount>,
 
-    #[arg(long, value_name = "SUBACCOUNT_FROM_PRINCIPAL")]
+    #[arg(
+        long,
+        value_name = "SUBACCOUNT_FROM_PRINCIPAL",
+        conflicts_with("subaccount")
+    )]
     /// Principal from which the subaccount identifier is derived.
     pub subaccount_from_principal: Option<Principal>,
 }
 
 pub async fn exec(env: &dyn Environment, opts: AccountIdOpts) -> DfxResult {
     let principal = if let Some(principal) = opts.of_principal {
-        if opts.of_canister.is_some() {
-            return Err(anyhow!(
-                "You can specify at most one of of-principal and of-canister arguments."
-            ));
-        }
         principal
     } else if let Some(alias) = opts.of_canister {
         let canister_id_store = env.get_canister_id_store()?;
@@ -41,11 +40,6 @@ pub async fn exec(env: &dyn Environment, opts: AccountIdOpts) -> DfxResult {
             .context("No identity is selected")?
     };
     let subaccount = if let Some(subaccount) = opts.subaccount {
-        if opts.subaccount_from_principal.is_some() {
-            return Err(anyhow!(
-                "You can specify at most one of subaccount and subaccount-from-principal arguments."
-            ));
-        }
         Some(subaccount)
     } else {
         opts.subaccount_from_principal
