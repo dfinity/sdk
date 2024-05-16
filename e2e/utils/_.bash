a243e6471c6cc65d4658c90218721537745af142
Adam Spofford <adam.spofford@dfinity.org>
2024-05-16 07:25:20 -0700

    .

--- e2e/utils/_.bash
+++ e2e/utils/_.bash
@@ -141,7 +141,7 @@ dfx_start() {
 
     # By default, start on random port for parallel test execution
     add_default_parameter "--host" "127.0.0.1:0"
-    if [[ ! "$USE_POCKETIC" ]]
+    if [[ ! "$USE_POCKETIC" ]]; then
         add_default_parameter "--artificial-delay" "100"
     fi
 
