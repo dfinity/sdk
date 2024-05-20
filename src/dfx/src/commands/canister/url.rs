67121e53357724e884e1efb3f2510346b7f8b6c2
Vincent Zhang <vincent.zhang@dfinity.org>
2024-05-20 13:16:54 +0800

    Handle the remote canisters, also run the formatter.

--- src/dfx/src/commands/canister/url.rs
+++ src/dfx/src/commands/canister/url.rs
@@ -1,3 +1,4 @@
+use super::super::deploy;
 use crate::lib::canister_info::CanisterInfo;
 use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
@@ -6,7 +7,6 @@ use candid::Principal;
 use clap::Parser;
 use dfx_core::config::model::canister_id_store::CanisterIdStore;
 use dfx_core::network::provider::{create_network_descriptor, LocalBindDetermination};
-use super::super::deploy;
 
 // Prints the url of a canister.
 #[derive(Parser)]
@@ -29,32 +29,39 @@ pub async fn exec(env: &dyn Environment, opts: CanisterUrlOpts) -> DfxResult {
         LocalBindDetermination::AsConfigured,
     )?;
 
-    let canister_id_store = 
+    let canister_id_store =
         CanisterIdStore::new(env.get_logger(), &network_descriptor, env.get_config()?)?;
 
-    let canister_name = opts.canister.as_str();
-    let canister_id = 
-        Principal::from_text(canister_name).or_else(|_| canister_id_store.get(canister_name))?;
+    // Get canister id, try to parse first as users can input canister id or canister name.
+    let canister_arg = opts.canister.as_str();
+    let canister_id =
+        Principal::from_text(canister_arg).or_else(|_| canister_id_store.get(canister_arg))?;
 
     let canister_id_text = Principal::to_text(&canister_id);
-
-    if let Some(name) = canister_id_store.get_name(&canister_id_text) {
+    if let Some(canister_name) = canister_id_store.get_name(&canister_id_text) {
         if let Some(canisters) = &config.get_config().canisters {
-            if let Some(canister_config) = canisters.get(name) {
-                // TODO : Handle remote.
+            if let Some(canister_config) = canisters.get(canister_name) {
+                // Ignore if it's remote canister.
+                if config
+                    .get_config()
+                    .is_remote_canister(&canister_name, &network_descriptor.name)?
+                {
+                    return Ok(());
+                }
 
-                let canister_info = CanisterInfo::load(&config, name, Some(canister_id))?;
+                let canister_info = CanisterInfo::load(&config, canister_name, Some(canister_id))?;
 
-                let is_assets = canister_info.is_assets() || canister_config.frontend.is_some();
-
-                if is_assets {
-                    let (canister_url, url2) = deploy::construct_frontend_url(&network_descriptor, &canister_id)?;
+                // Display as frontend if it's an asset canister, or custome type with a frontend.
+                if canister_info.is_assets() || canister_config.frontend.is_some() {
+                    let (canister_url, url2) =
+                        deploy::construct_frontend_url(&network_descriptor, &canister_id)?;
                     println!("{}", canister_url);
                     if let Some(canister_url2) = url2 {
                         println!("{}", canister_url2);
                     }
                 }
 
+                // Display as backend.
                 if !canister_info.is_assets() {
                     let url = deploy::construct_ui_canister_url(env, &canister_id)?;
                     if let Some(canister_url) = url {
