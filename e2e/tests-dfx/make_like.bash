568cd6bf23d4006d93ea368402e187db68b07681
Victor Porton <porton.victor@gmail.com>
2024-05-26 07:56:40 +0300

    corrected test

--- e2e/tests-dfx/make_like.bash
+++ e2e/tests-dfx/make_like.bash
@@ -19,77 +19,77 @@ teardown() {
     dfx_start
 
     assert_command dfx deploy -vv dependent
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_contains 'Installing code for canister dependent'
     assert_contains 'Installing code for canister dependency'
 
     touch src/dependent.mo
     assert_command dfx deploy -vv dependent
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_not_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_not_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     touch src/dependency.mo
     assert_command dfx deploy -vv dependent
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     touch src/dependency.mo
     assert_command dfx deploy -vv dependency
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_not_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     assert_command dfx deploy -vv dependent
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_not_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_not_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     touch src/lib.mo
     assert_command dfx deploy -vv dependent
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     touch src/lib.mo
     assert_command dfx deploy -vv dependency
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_not_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     touch src/lib.mo
     assert_command dfx deploy -vv
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     touch src/dependency.mo
     assert_command dfx deploy -vv
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     touch src/dependent.mo
     assert_command dfx deploy -vv
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_not_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_not_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 
     cp src/dependency_altered.mo src/dependency.mo
     assert_command dfx deploy -vv
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
     assert_contains 'Upgrading code for canister dependent'
     assert_contains 'Upgrading code for canister dependency'
 }
@@ -100,57 +100,57 @@ teardown() {
     assert_command dfx canister create dependency
     assert_command dfx canister create dependent
     assert_command dfx build -vv dependent
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     touch src/dependent.mo
     assert_command dfx build -vv dependent
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_not_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_not_contains "/src/dependency.mo"
 
     touch src/dependency.mo
     assert_command dfx build -vv dependent
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     touch src/dependency.mo
     assert_command dfx build -vv dependency
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     assert_command dfx build -vv dependent
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_not_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_not_contains "/src/dependency.mo"
 
     touch src/lib.mo
     assert_command dfx build -vv dependent
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     touch src/lib.mo
     assert_command dfx build -vv dependency
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     touch src/lib.mo
     assert_command dfx build -vv
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     touch src/dependency.mo
     assert_command dfx build -vv
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     touch src/dependent.mo
     assert_command dfx build -vv
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_not_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_not_contains "/src/dependency.mo"
 
     cp src/dependency_altered.mo src/dependency.mo
     assert_command dfx build -vv
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 }
 
 @test "mix build and deploy" {
@@ -159,12 +159,12 @@ teardown() {
     assert_command dfx canister create dependency
     assert_command dfx canister create dependent
     assert_command dfx build -vv
-    assert_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_contains '/src/dependent.mo"'
+    assert_contains "/src/dependency.mo"
 
     assert_command dfx deploy -vv dependent
-    assert_not_contains '"moc-wrapper" "src/dependent.mo"'
-    assert_not_contains '"moc-wrapper" "src/dependency.mo"'
+    assert_not_contains '/src/dependent.mo"'
+    assert_not_contains "/src/dependency.mo"
     assert_contains 'Installing code for canister dependent'
     assert_contains 'Installing code for canister dependency'
 }
