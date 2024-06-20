66f2b87e94f2fde67f64ba8e38ee2ea9047a909e
Max Murphy <max@winning.black>
2024-06-20 18:47:36 +0200

    ++

--- src/canisters/frontend/ic-asset/src/upload.rs
+++ src/canisters/frontend/ic-asset/src/upload.rs
@@ -5,6 +5,7 @@ use crate::batch_upload::{
     operations::AssetDeletionReason,
     plumbing::{make_project_assets, AssetDescriptor, ChunkUploader},
 };
+use crate::canister_api::methods::batch::propose_commit_batch;
 use crate::canister_api::methods::{
     api_version::api_version,
     batch::{commit_batch, create_batch},
@@ -69,6 +70,61 @@ pub async fn upload(
             commit_batch(canister, commit_batch_args_v0).await
         }
         BATCH_UPLOAD_API_VERSION.. => commit_batch(canister, commit_batch_args).await,
+    }
+    .map_err(CommitBatchFailed)
+}
+
+
+/// Upload the specified files
+pub async fn upload_and_propose(
+    canister: &Canister<'_>,
+    files: HashMap<String, PathBuf>,
+    logger: &Logger,
+) -> Result<(), UploadError> {
+    let asset_descriptors: Vec<AssetDescriptor> = files
+        .iter()
+        .map(|x| AssetDescriptor {
+            source: x.1.clone(),
+            key: x.0.clone(),
+            config: AssetConfig::default(),
+        })
+        .collect();
+
+    let canister_assets = list_assets(canister).await.map_err(ListAssetsFailed)?;
+
+    info!(logger, "Starting batch.");
+
+    let batch_id = create_batch(canister).await.map_err(CreateBatchFailed)?;
+
+    info!(logger, "Staging contents of new and changed assets:");
+
+    let chunk_upload_target = ChunkUploader::new(canister.clone(), batch_id.clone());
+
+    let project_assets = make_project_assets(
+        Some(&chunk_upload_target),
+        asset_descriptors,
+        &canister_assets,
+        logger,
+    )
+    .await?;
+
+    let commit_batch_args = batch_upload::operations::assemble_commit_batch_arguments(
+        project_assets,
+        canister_assets,
+        AssetDeletionReason::Incompatible,
+        HashMap::new(),
+        batch_id,
+    );
+
+    let canister_api_version = api_version(canister).await;
+    info!(logger, "Committing batch.");
+    match canister_api_version {
+        0 => {
+            let commit_batch_args_v0 = v0::CommitBatchArguments::try_from(commit_batch_args)
+                .map_err(DowngradeV1TOV0Failed)?;
+            propose_commit_batch(canister, commit_batch_args_v0).await
+        }
+        BATCH_UPLOAD_API_VERSION.. => propose_commit_batch(canister, commit_batch_args).await,
     }
     .map_err(CommitBatchFailed)
 }
