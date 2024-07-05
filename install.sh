ef62ae01bf90ff125b5471937b0879e50c5b7886
eichhorl <99166915+eichhorl@users.noreply.github.com>
2024-07-05 09:44:50 +0000

    Deploying to public-manifest from  @ d8c1ede88d1978653d2e7d7005095fe38499e023 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="e599de6fed0b860b5cccb0b4a233854d561f75fc"
+SCRIPT_COMMIT_DESC="d8c1ede88d1978653d2e7d7005095fe38499e023"
 
 download_and_install() {
     SHASUM="$1"
