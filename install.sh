784dce5c80d7a61fcbe2da910497f8ad3a8f75aa
THLO <thomas.locher@dfinity.org>
2024-07-17 08:39:02 +0000

    Deploying to public-manifest from  @ d14581385e31f3f6e8228407e815f62629a852d1 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="b8286eb667ce842323d45c9e94f29496642c5140"
+SCRIPT_COMMIT_DESC="d14581385e31f3f6e8228407e815f62629a852d1"
 
 download_and_install() {
     SHASUM="$1"
