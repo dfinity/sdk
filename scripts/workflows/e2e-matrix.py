ee44f000e661812d279e173ee6f4e4118f8a93fa
Eric Swanson <eric.swanson@dfinity.org>
2024-08-20 09:58:12 -0700

    64

--- scripts/workflows/e2e-matrix.py
+++ scripts/workflows/e2e-matrix.py
@@ -15,9 +15,14 @@ test = sorted(test_scripts("dfx") + test_scripts("replica") + test_scripts("icx-
 
 matrix = {
     "test": [ "dfx/canister_http", "dfx/bitcoin"],
-    "iterations": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
     "backend": ["pocketic", "replica"],
     "os": ["macos-12", "ubuntu-20.04"],
+    "iterations": [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+        33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+        49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64
+    ],
     "exclude": [
         {
             "backend": "pocketic",
