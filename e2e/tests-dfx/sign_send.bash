fe6d9d0218417f8c23f0746a75e02d1af82b0333
Adam Spofford <adam.spofford@dfinity.org>
2024-08-22 15:58:33 -0700

    .

--- e2e/tests-dfx/sign_send.bash
+++ e2e/tests-dfx/sign_send.bash
@@ -37,7 +37,7 @@ teardown() {
   sleep 10
   echo y | assert_command dfx canister send message-inc.json
   assert_command dfx canister send message-inc.json --status
-  assert_match "To see the content of response, copy-paste the encoded string into cbor.me."
+  assert_match "Response:"
 }
 
 @test "sign outside of a dfx project" {
