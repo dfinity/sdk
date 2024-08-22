9a9a6f159296bbb540dc966bde2a3a1171093ab5
Adam Spofford <adam.spofford@dfinity.org>
2024-08-22 09:35:46 -0700

    Update agent for transport changes

--- src/canisters/frontend/ic-asset/src/lib.rs
+++ src/canisters/frontend/ic-asset/src/lib.rs
@@ -3,7 +3,7 @@
 //! # Example
 //!
 //! ```rust,no_run
-//! use ic_agent::agent::{Agent, http_transport::ReqwestTransport};
+//! use ic_agent::agent::Agent;
 //! use ic_agent::identity::BasicIdentity;
 //! use ic_utils::Canister;
 //! use std::time::Duration;
@@ -12,7 +12,7 @@
 //! # let pemfile = "";
 //! # let canister_id = "";
 //! let agent = Agent::builder()
-//!     .with_transport(ReqwestTransport::create(replica_url)?)
+//!     .with_url(replica_url)
 //!     .with_identity(BasicIdentity::from_pem_file(pemfile)?)
 //!     .build()?;
 //! let canister = Canister::builder()
