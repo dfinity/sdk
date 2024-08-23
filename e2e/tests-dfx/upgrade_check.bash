6db016f405455c6b20dadaf54b6696f0d174788f
luc-blaeser <luc.blaeser@dfinity.org>
2024-08-23 12:27:03 +0200

    Adjust e2e tests

--- e2e/tests-dfx/upgrade_check.bash
+++ e2e/tests-dfx/upgrade_check.bash
@@ -33,7 +33,7 @@ teardown() {
   jq '.canisters.hello_backend.main="v2_bad.mo"' dfx.json | sponge dfx.json
   echo yes | (
   assert_command dfx deploy
-  assert_match "Stable interface compatibility check failed"
+  assert_match "Stable interface compatibility check issued an ERROR"
   )
   assert_command dfx canister call hello_backend read '()'
   assert_match "(0 : nat)"
@@ -59,7 +59,7 @@ teardown() {
   jq '.canisters.hello_backend.main="v3_bad.mo"' dfx.json | sponge dfx.json
   echo yes | (
   assert_command dfx deploy
-  assert_match "Candid interface compatibility check failed"
+  assert_match "Candid interface compatibility check issued an ERROR"
   )
   assert_command dfx canister call hello_backend read2 '()'
   assert_match "(1 : int)"
@@ -73,8 +73,22 @@ teardown() {
   jq '.canisters.hello_backend.main="v4_bad.mo"' dfx.json | sponge dfx.json
   echo yes | (
   assert_command dfx deploy
-  assert_match "Candid interface compatibility check failed"
+  assert_match "Candid interface compatibility check issued an ERROR"
   )
   assert_command dfx canister call hello_backend f '()'
   assert_match "(opt \"\")"
+}
+
+@test "warning when dropping stable variable" {
+  install_asset upgrade
+  dfx_start
+  dfx deploy
+  dfx canister call hello_backend inc '()'
+  jq '.canisters.hello_backend.main="v5.mo"' dfx.json | sponge dfx.json
+  echo yes | (
+  assert_command dfx deploy
+  assert_match "Stable interface compatibility check issued a WARNING"
+  )
+  assert_command dfx canister call hello_backend read '()'
+  assert_match "(0 : nat)"
 }
