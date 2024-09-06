b5f5b9ce9b55516cd85d24cd52a56c613a56f7b9
krpeacock <kylpeacock@gmail.com>
2024-09-06 17:00:49 +0000

    Deploying to public-manifest from  @ 005182535ef9c5786d767dff390842d0bc9e9868 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="7b691ac988273e65823ce51a7cd00a99ec295068"
+SCRIPT_COMMIT_DESC="005182535ef9c5786d767dff390842d0bc9e9868"
 
 download_and_install() {
     SHASUM="$1"
