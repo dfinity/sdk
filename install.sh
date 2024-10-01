0fece3ae6d3baeedac9ae02f19794dceee4d4340
Sawchord <leon.arian.tan@gmail.com>
2024-10-01 10:26:26 +0000

    Deploying to public-manifest from  @ 3050c0334fa81f397a2d721b36de1e90f05aeaf8 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="9ef469f3d28b1310cbb7caa5036c477cce589f73"
+SCRIPT_COMMIT_DESC="3050c0334fa81f397a2d721b36de1e90f05aeaf8"
 
 download_and_install() {
     SHASUM="$1"
