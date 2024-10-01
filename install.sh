1a1f04af223b9834c04ed537b7dbda4399bd1d0c
sesi200 <severin.siffert@dfinity.org>
2024-10-01 08:56:18 +0000

    Deploying to public-manifest from  @ 9ef469f3d28b1310cbb7caa5036c477cce589f73 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="ba7bc4f61b6635ef3007b7e9793a2cc2d57721bb"
+SCRIPT_COMMIT_DESC="9ef469f3d28b1310cbb7caa5036c477cce589f73"
 
 download_and_install() {
     SHASUM="$1"
