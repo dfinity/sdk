5669bbb45a0f417172051ba44e9477b883c36e71
mraszyk <31483726+mraszyk@users.noreply.github.com>
2024-10-07 16:39:00 +0000

    Deploying to public-manifest from  @ da094deb76c0e95733db69ffea2114a0255fd007 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="ab9b342e608166502a3ab44092be8f23d1bff27d"
+SCRIPT_COMMIT_DESC="da094deb76c0e95733db69ffea2114a0255fd007"
 
 download_and_install() {
     SHASUM="$1"
