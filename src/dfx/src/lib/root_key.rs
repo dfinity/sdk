cf4ef4010cae24f35e5b1a974ba1cad5cff61324
Vincent Zhang <vincent.zhang@dfinity.org>
2024-10-29 21:33:35 +0800

    Move the error handling to 'fetch_root_key_if_needed'.

--- src/dfx/src/lib/root_key.rs
+++ src/dfx/src/lib/root_key.rs
@@ -1,11 +1,12 @@
 use crate::{lib::error::DfxResult, Environment};
-
+use anyhow::anyhow;
 use dfx_core::network::root_key;
 
 pub async fn fetch_root_key_if_needed(env: &dyn Environment) -> DfxResult {
     let agent = env.get_agent();
     let network = env.get_network_descriptor();
-    root_key::fetch_root_key_when_non_mainnet(agent, network).await?;
+    root_key::fetch_root_key_when_non_mainnet(agent, network).await
+        .map_err(|e| anyhow!("Failed to fetch the root key, did you run 'dfx start' to start the local replica?\n{}", e))?;
     Ok(())
 }
 
