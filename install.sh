31efd4bcf576ae48493ba75e39aad8c3c813709b
chenyan-dfinity <48968912+chenyan-dfinity@users.noreply.github.com>
2024-10-31 09:24:12 +0000

    Deploying to public-manifest from  @ d25c49fd3de709e061c0364953329e14c0aea2f0 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="abab449da053d33f72f08358d26067af637ec73c"
+SCRIPT_COMMIT_DESC="d25c49fd3de709e061c0364953329e14c0aea2f0"
 
 download_and_install() {
     SHASUM="$1"
