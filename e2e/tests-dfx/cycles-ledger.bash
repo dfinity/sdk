86dbb0143a78d9d64ddcf3fb0143ade40e4f51fe
Vincent Zhang <vincent.zhang@dfinity.org>
2024-11-05 16:04:36 +0800

    Fix tests by starting dfx first.

--- e2e/tests-dfx/cycles-ledger.bash
+++ e2e/tests-dfx/cycles-ledger.bash
@@ -118,6 +118,8 @@ current_time_nanoseconds() {
 }
 
 @test "balance without cycles ledger failed as expected" {
+  dfx_start
+
   assert_command_fail dfx cycles balance
   assert_contains "Cycles ledger with canister ID 'um5iw-rqaaa-aaaaq-qaaba-cai' cannot be found."
 }
