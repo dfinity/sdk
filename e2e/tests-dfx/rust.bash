5d4cae1bd2b25a8e60a5224328c726627d1a1262
Linwei Shang <linwei.shang@dfinity.org>
2024-11-14 15:14:28 +0000

    test: merge build_rust.bash into rust.bash

--- e2e/tests-dfx/rust.bash
+++ e2e/tests-dfx/rust.bash
@@ -75,3 +75,27 @@ teardown() {
   assert_command dfx canister call can3 id
   assert_contains '(3 : nat32)'
 }
+
+@test "build without cargo-audit installed cannot check for vulnerabilities" {
+  dfx_new
+  assert_command rustup default stable
+  assert_command rustup target add wasm32-unknown-unknown
+  install_asset vulnerable_rust_deps
+  dfx_start
+  dfx canister create --all
+  assert_command dfx build
+  assert_match "Cannot check for vulnerabilities in rust canisters because cargo-audit is not installed."
+}
+
+@test "build with vulnerabilities in rust dependencies emits a warning" {
+  dfx_new
+  assert_command rustup default stable
+  assert_command rustup target add wasm32-unknown-unknown
+  assert_command cargo install cargo-audit --locked
+  assert_command cargo audit --version
+  install_asset vulnerable_rust_deps
+  dfx_start
+  dfx canister create --all
+  assert_command dfx build
+  assert_match "Audit found vulnerabilities"
+}
