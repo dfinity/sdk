719c6374a9eeaf3a436cfc513facf022d2d5fb52
Jason <98767015+dfx-json@users.noreply.github.com>
2024-11-19 07:51:27 -0800

    chore: update test snapshots

--- src/dfx/assets/project_templates/svelte/src/__frontend_name__/src/routes/+page.svelte
+++ src/dfx/assets/project_templates/svelte/src/__frontend_name__/src/routes/+page.svelte
@@ -14,13 +14,11 @@
 <main class="container">
   <div class="card">
     <h1>{greeting}</h1>
-
     <form action="#" on:submit|preventDefault={onSubmit}>
       <label for="name">Enter your name:</label>
       <input type="text" id="name" required />
       <button type="submit">Click Me!</button>
     </form>
-
     <img src="/logo.svg" alt="DFINITY logo" width="256" />
   </div>
 </main>
