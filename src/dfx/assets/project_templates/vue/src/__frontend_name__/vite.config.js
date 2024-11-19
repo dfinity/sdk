516647a277846fcfe41c84fb42c6a11292e9bc06
Jason <98767015+dfx-json@users.noreply.github.com>
2024-11-18 17:24:28 -0800

    chore: use single quotes

--- src/dfx/assets/project_templates/vue/src/__frontend_name__/vite.config.js
+++ src/dfx/assets/project_templates/vue/src/__frontend_name__/vite.config.js
@@ -33,8 +33,8 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: "modern-compiler",
-        silenceDeprecations: ["legacy-js-api"],
+        api: 'modern-compiler',
+        silenceDeprecations: ['legacy-js-api'],
       },
     },
   },
@@ -44,5 +44,5 @@ export default defineConfig({
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
     ],
     dedupe: ['@dfinity/agent'],
-  }
+  },
 });
