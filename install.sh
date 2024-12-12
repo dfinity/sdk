34d22b87805a218da6006dffb24f469a47998e33
jessiemongeon1 <133128541+jessiemongeon1@users.noreply.github.com>
2024-12-12 20:46:41 +0000

    Deploying to public-manifest from  @ 26a976dac61991eaaf426c0fd9d31eaafecc7c42 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="5b6a5799097fd0654b6f930513983dce93ddf231"
+SCRIPT_COMMIT_DESC="26a976dac61991eaaf426c0fd9d31eaafecc7c42"
 
 download_and_install() {
     SHASUM="$1"
