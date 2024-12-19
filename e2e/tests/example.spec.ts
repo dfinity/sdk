11288359c7334415b5b25b44b4f32fea245cfbd9
Kaia Peacock <kaia@peacock.dev>
2024-12-19 13:57:04 -0800

    test: adds playwright test for svelte  project

---
+++ e2e/tests/example.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://br5f7-7uaaa-aaaaa-qaaca-cai.localhost:4943/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/IC Hello Starter/);
+});
+
+test("image has loaded", async ({ page }) => {
+  await page.goto("http://br5f7-7uaaa-aaaaa-qaaca-cai.localhost:4943/");
+
+  // Set the attribute so we can read it
+  await page.evaluate(() => {
+    document.querySelectorAll("img").forEach((img) => {
+      img.setAttribute("complete", String(img.complete));
+    });
+  });
+
+  const image = await page.getByAltText("DFINITY logo");
+  await expect(await image.getAttribute("complete")).toBe("true");
+});
+
+test("has hello form", async ({ page }) => {
+  await page.goto("http://br5f7-7uaaa-aaaaa-qaaca-cai.localhost:4943/");
+
+  // Fill out the form
+  const nameInput = await page.getByLabel("name");
+  nameInput.fill("World");
+
+  const submitButton = await page.locator("button[type='submit']");
+  await submitButton.click();
+
+  // Expects page to have a heading with the name of Installation.
+  const greeting = await page.locator("#greeting");
+  await expect(greeting).toBeVisible();
+  await expect(await greeting.innerText()).toBe("Hello, World!");
+});
