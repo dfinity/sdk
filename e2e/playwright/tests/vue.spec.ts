990082609a0b43f0ab9a805b21acb1ed4a651cbb
Kaia Peacock <kaia@peacock.dev>
2025-01-16 16:19:22 -0800

    test: adds playwright tests for `dfx new` project frontends (#4065)

---
+++ e2e/playwright/tests/vue.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://qopmg-3aaaa-aaaab-qadsq-cai.localhost:4943/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/IC Hello Starter/);
+});
+
+test("image has loaded", async ({ page }) => {
+  await page.goto("http://qopmg-3aaaa-aaaab-qadsq-cai.localhost:4943/");
+
+  // Set the attribute so we can read it
+  await page.evaluate(async () => {
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        document.querySelectorAll("img").forEach((img) => {
+          img.setAttribute("complete", String(img.complete));
+        });
+        resolve();
+      }, 1_000);
+    });
+  });
+
+  const image = await page.getByAltText("DFINITY logo");
+  await expect(await image.getAttribute("complete")).toBe("true");
+});
+
+test("has hello form", async ({ page }) => {
+  await page.goto("http://qopmg-3aaaa-aaaab-qadsq-cai.localhost:4943/");
+
+  // Fill out the form
+  const nameInput = await page.getByLabel("name");
+  nameInput.fill("World");
+
+  const submitButton = await page.locator("button[type='submit']");
+  await submitButton.click();
+
+  // Expects page to have a heading with the name of Installation.
+  const greeting = await page.locator("#greeting");
+  await expect(greeting).toBeVisible();
+  await expect(await greeting.innerText()).toBe("Hello, World!");
+});
