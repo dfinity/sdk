b4c9b79d1f0feac9e004cd919299280bf025fa68
Victor Porton <porton.victor@gmail.com>
2025-01-28 14:47:39 +0200

    output generate@ rules

--- src/dfx/src/commands/rules.rs
+++ src/dfx/src/commands/rules.rs
@@ -1,5 +1,7 @@
+use std::collections::BTreeMap;
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::path::Path;
 
 use crate::lib::agent::create_anonymous_agent_environment;
 use crate::lib::builders::CanisterBuilder;
@@ -7,6 +9,8 @@ use crate::lib::environment::Environment;
 use crate::lib::error::DfxResult;
 use crate::lib::models::canister::{CanisterPool, Import};
 use crate::lib::builders::custom::CustomBuilder;
+use itertools::Itertools;
+use dfx_core::config::model::dfinity::ConfigCanistersCanister;
 use clap::Parser;
 use petgraph::visit::EdgeRef;
 use petgraph::Graph;
@@ -53,16 +57,21 @@ pub fn exec(env1: &dyn Environment, opts: RulesOpts) -> DfxResult {
     output_file.write_fmt(format_args!("NETWORK ?= local\n\n"))?;
     output_file.write_fmt(format_args!("DEPLOY_FLAGS ?= \n\n"))?;
 
-    match &config.get_config().canisters {
+    let canisters = &config.get_config().canisters; 
+    match &canisters {
         Some(canisters) => {
+            let canisters: &BTreeMap<String, ConfigCanistersCanister> = canisters;
             output_file.write_fmt(format_args!(".PHONY:"))?;
             for canister in canisters {
                 output_file.write_fmt(format_args!(" canister@{}", canister.0))?;
             };
-            output_file.write_fmt(format_args!("\n\n"))?;
-            output_file.write_fmt(format_args!(".PHONY:"))?;
+            output_file.write_fmt(format_args!("\n\n.PHONY:"))?;
             for canister in canisters {
                 output_file.write_fmt(format_args!(" deploy@{}", canister.0))?;
+            }
+            output_file.write_fmt(format_args!("\n\n.PHONY:"))?;
+            for canister in canisters {
+                output_file.write_fmt(format_args!(" generate@{}", canister.0))?;
             }
             output_file.write_fmt(format_args!("\n\n"))?;
             for canister in canisters {
@@ -70,6 +79,44 @@ pub fn exec(env1: &dyn Environment, opts: RulesOpts) -> DfxResult {
                 let path1 = format!(".dfx/local/canisters/{}/{}.wasm", canister.0, canister.0);
                 let path2 = format!(".dfx/local/canisters/{}/{}.did", canister.0, canister.0);
                 output_file.write_fmt(format_args!("canister@{}: \\\n  {} {}\n\n", canister.0, path1, path2))?;
+            };
+            for canister in canisters {
+                let declarations_config_pre = &canister.1.declarations;
+                // let workspace_root = config.get_path().parent().unwrap();
+                // duplicate code:
+                let output = declarations_config_pre
+                    .output
+                    .clone()
+                    .unwrap_or_else(|| Path::new("src/declarations").join(canister.0));
+                let bindings = declarations_config_pre
+                    .bindings
+                    .clone() // probably, inefficient
+                    .unwrap_or_else(|| vec!["js".to_string(), "ts".to_string(), "did".to_string()]);
+                if !bindings.is_empty() {
+                    let deps = bindings.iter().map(|lang| {
+                        match lang.as_str() {
+                            "did" => vec![format!("{}.did", canister.0)],
+                            "mo" => vec![format!("{}.mo", canister.0)],
+                            "rs" => vec![], // TODO
+                            "js" => vec![format!("{}.did.js", canister.0), "index.js".to_string()],
+                            "ts" => vec![format!("{}.did.d.ts", canister.0), "index.d.ts".to_string()],
+                            _ => panic!("unknown canister type: {}", canister.0.as_str()), // TODO
+                        }
+                    }).flatten().map(|path| output.join(path).to_str().unwrap().to_string()).join(" "); // TODO: `unwrap`
+                    output_file.write_fmt(format_args!(
+                        "generate@{}: canister@{}\n  {}\n\n",
+                        canister.0,
+                        canister.0,
+                        deps,
+                    ))?;
+                    output_file.write_fmt(format_args!(
+                        "{}: canister@{}\n\t{} {}\n\n",
+                        deps,
+                        canister.0,
+                        "dfx generate --network $(NETWORK)",
+                        canister.0,
+                    ))?;
+                }
             };
         }
         None => {}
