64922a3569530355616c3c9cf2d335b4c261a5b9
Adam Spofford <adam.spofford@dfinity.org>
2025-03-17 01:39:41 -0700

    make telemetry tests local by default

--- e2e/tests-dfx/telemetry.bash
+++ e2e/tests-dfx/telemetry.bash
@@ -6,6 +6,8 @@ setup() {
   standard_setup
 
   dfx_new
+  
+  export DFX_TELEMETRY=local
 }
 
 teardown() {
@@ -53,13 +55,13 @@ teardown() {
     *) fail 'unknown platform';;
     esac
     log=$(dfx info telemetry-log-path)
-    assert_command env DFX_TELEMETRY=local dfx identity get-principal
+    assert_command dfx identity get-principal
     assert_command jq -se 'last | .command == "identity get-principal" and .platform == "'"$expected_platform"'"
         and .exit_code == 0 and (.parameters | length == 0)' "$log"
     n=$(jq -sr length "$log")
-    assert_command_fail env DFX_TELEMETRY=local DFX_NETWORK=ic dfx identity get-platypus
+    assert_command_fail env DFX_NETWORK=ic dfx identity get-platypus
     assert_command jq -se "length == $n" "$log"
-    assert_command_fail env DFX_TELEMETRY=local DFX_NETWORK=ic dfx identity get-principal --identity platypus
+    assert_command_fail env DFX_NETWORK=ic dfx identity get-principal --identity platypus
     assert_command jq -se 'length == '$((n+1))' and (last | .command == "identity get-principal" and .exit_code == 255 and
         (.parameters | any(.name == "network" and .source == "environment")
             and any(.name == "identity" and .source == "command-line")))' "$log"
@@ -69,7 +71,7 @@ teardown() {
     local log
     log=$(dfx info telemetry-log-path)
     assert_command dfx extension install nns --version 0.3.1
-    assert_command env DFX_TELEMETRY=local dfx nns import
+    assert_command dfx nns import
     assert_command jq -se 'last | .command == "extension run" and (.parameters | any(.name == "name"))' "$log"
 }
 
@@ -78,7 +80,7 @@ teardown() {
     log=$(dfx info telemetry-log-path)
     dfx identity get-principal # initialize it first
     for _ in {0..100}; do
-        assert_command env DFX_TELEMETRY=local dfx identity get-principal &
+        assert_command dfx identity get-principal &
     done
     wait
     assert_command jq -se '.[-101:-1] | all(.command == "identity get-principal") and length == 100' "$log"
