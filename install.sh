211d64071a6318b7a4d5aa8bb6bcdbc5a373ed62
vincent-dfinity <118719397+vincent-dfinity@users.noreply.github.com>
2025-03-18 15:57:07 +0000

    Deploying to public-manifest from  @ 593bb05beca6bde5842ef857ca46dacd4359f17e 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="a763cac83cd354742925c30a17f4d2043a933fa3"
+SCRIPT_COMMIT_DESC="593bb05beca6bde5842ef857ca46dacd4359f17e"
 
 download_and_install() {
     SHASUM="$1"
