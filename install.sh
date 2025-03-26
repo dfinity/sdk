8e4b7fd0b4948f26d87b89ab38ec09009afffea9
ericswanson-dfinity <64809312+ericswanson-dfinity@users.noreply.github.com>
2025-03-26 23:12:25 +0000

    Deploying to public-manifest from  @ 593624a7a0dbe5fd7fcf98cc9434bf3782422337 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="2013389f0d9aba148ab266647f0eb653d4514f1c"
+SCRIPT_COMMIT_DESC="593624a7a0dbe5fd7fcf98cc9434bf3782422337"
 
 download_and_install() {
     SHASUM="$1"
