7157fe4b979972d8891810aa0b7f430d6a67e90f
Adam Spofford <93943719+adamspofford-dfinity@users.noreply.github.com>
2025-05-06 09:52:21 -0700

    Remove obsolete niv binaries from scripts (#4233)

--- scripts/update-replica.sh
+++ scripts/update-replica.sh
@@ -10,29 +10,8 @@ fi
 
 SHA=$1
 echo "Updating sources to rev ${SHA}"
-niv update ic-admin-x86_64-darwin -a rev="$SHA"
-niv update ic-admin-x86_64-linux -a rev="$SHA"
-niv update ic-btc-adapter-x86_64-darwin -a rev="$SHA"
-niv update ic-btc-adapter-x86_64-linux -a rev="$SHA"
-niv update ic-https-outcalls-adapter-x86_64-darwin -a rev="$SHA"
-niv update ic-https-outcalls-adapter-x86_64-linux -a rev="$SHA"
-niv update ic-nns-init-x86_64-darwin -a rev="$SHA"
-niv update ic-nns-init-x86_64-linux -a rev="$SHA"
-niv update ic-starter-x86_64-darwin -a rev="$SHA"
-niv update ic-starter-x86_64-linux -a rev="$SHA"
-
-niv update replica-x86_64-darwin -a rev="$SHA"
-niv update replica-x86_64-linux -a rev="$SHA"
 niv update pocket-ic-x86_64-darwin -a rev="$SHA"
 niv update pocket-ic-x86_64-linux -a rev="$SHA"
-niv update canister_sandbox-x86_64-darwin -a rev="$SHA"
-niv update canister_sandbox-x86_64-linux -a rev="$SHA"
-niv update compiler_sandbox-x86_64-darwin -a rev="$SHA"
-niv update compiler_sandbox-x86_64-linux -a rev="$SHA"
-niv update sandbox_launcher-x86_64-darwin -a rev="$SHA"
-niv update sandbox_launcher-x86_64-linux -a rev="$SHA"
-niv update sns-x86_64-darwin -a rev="$SHA"
-niv update sns-x86_64-linux -a rev="$SHA"
 
 # pocket-ic client needs to be upgraded to the same SHA as the pocket-ic server
 perl -i.bak -pe "s/(pocket-ic = {[^}]*rev = \")[a-f0-9]+(\")/\${1}$SHA\${2}/" src/dfx/Cargo.toml
