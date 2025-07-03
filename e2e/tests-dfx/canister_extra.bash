c6790a12c15f4d04fc04ee4a451a05f6a5b8d88e
Vincent Zhang <vincent.zhang@dfinity.org>
2025-07-03 14:07:06 +0800

    Add the e2e test.

--- e2e/tests-dfx/canister_extra.bash
+++ e2e/tests-dfx/canister_extra.bash
@@ -45,6 +45,43 @@ teardown() {
     assert_contains 'Canister hello_backend is running and snapshots should not be taken of running canisters'
 }
 
+@test "canister snapshots download and upload" {
+    dfx_start
+    install_asset counter
+    dfx deploy
+
+    assert_command dfx canister call hello_backend inc_read
+    assert_contains '(1 : nat)'
+
+    dfx canister stop hello_backend
+    assert_command dfx canister snapshot create hello_backend
+    assert_match 'Snapshot ID: ([0-9a-f]+)'
+    snapshot=${BASH_REMATCH[1]}
+    dfx canister start hello_backend
+
+    OUTPUT_DIR="output"
+    mkdir -p "$OUTPUT_DIR"
+    assert_command dfx canister snapshot download hello_backend "$snapshot" --dir "$OUTPUT_DIR"
+    assert_contains "saved to '$OUTPUT_DIR'"
+
+    assert_command dfx canister call hello_backend inc_read
+    assert_contains '(2 : nat)'
+
+    assert_command dfx canister snapshot upload hello_backend --dir "$OUTPUT_DIR"
+    assert_match 'Snapshot ID: ([0-9a-f]+)'
+    snapshot_1=${BASH_REMATCH[1]}
+
+    assert_command dfx canister snapshot list hello_backend
+    assert_contains "${snapshot_1}"
+
+    dfx canister stop hello_backend
+    assert_command dfx canister snapshot load hello_backend "$snapshot_1"
+
+    dfx canister start hello_backend
+    assert_command dfx canister call hello_backend read
+    assert_contains '(1 : nat)'
+}
+
 @test "can query a website" {
   dfx_start
 
