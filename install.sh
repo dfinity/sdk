31a7c4271d90dfb213b111b225c46d8b441a8e2f
adamspofford-dfinity <93943719+adamspofford-dfinity@users.noreply.github.com>
2025-07-07 18:04:18 +0000

    Deploying to public-manifest from  @ 20a3afdc50a44fc42d5cb6d22759822eb3fa16c2 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="3c844b0e90ff18de2da82c645e7baa2d65840320"
+SCRIPT_COMMIT_DESC="20a3afdc50a44fc42d5cb6d22759822eb3fa16c2"
 
 download_and_install() {
     SHASUM="$1"
