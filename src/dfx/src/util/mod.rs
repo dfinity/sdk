9012aef3ce8c3de3a4f5daff7d143b190d2db148
Linwei Shang <linwei.shang@dfinity.org>
2025-08-26 13:10:22 -0400

    further relax the test and show how long it took

--- src/dfx/src/util/mod.rs
+++ src/dfx/src/util/mod.rs
@@ -555,7 +555,11 @@ mod tests {
             .unwrap()
             .block_on(download_file(&url));
         let time1 = std::time::Instant::now();
-        // Relax the time constraint so that the test is less flaky
-        assert!(time1 - time0 < std::time::Duration::from_secs(70));
+        let elapsed = (time1 - time0).as_secs();
+        assert!(
+            elapsed < 80, // Relax the time constraint so that the test is less flaky
+            "Download file took {} seconds (> 60s)",
+            elapsed
+        );
     }
 }
