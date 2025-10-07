d1170111159dad082e7907a35fb0c7a328be90f3
ilbertt <luca.bertelli@dfinity.org>
2025-10-07 18:12:23 +0200

    test: update tests according to svelte v5 and update changelog

--- src/dfx/assets/project_templates/svelte_tests/src/__frontend_name__/src/tests/App.test.js
+++ src/dfx/assets/project_templates/svelte_tests/src/__frontend_name__/src/tests/App.test.js
@@ -1,4 +1,5 @@
 import { test, expect, afterEach } from 'vitest';
+import { mount } from 'svelte';
 import App from '../routes/+page.svelte';
 
 let host;
@@ -11,7 +12,7 @@ test('mount component', async () => {
   host = document.createElement('div');
   host.setAttribute('id', 'host');
   document.body.appendChild(host);
-  const instance = new App({ target: host, props: {} });
+  const instance = mount(App, { target: host, props: {} });
   expect(instance).toBeTruthy();
   expect(host.innerHTML).toMatchInlineSnapshot(
     '"<main><img src="/logo2.svg" alt="DFINITY logo"> <br> <br> <form action="#"><label for="name">Enter your name: &nbsp;</label> <input id="name" alt="Name" type="text"> <button type="submit">Click Me!</button></form> <section id="greeting"></section></main>"'
