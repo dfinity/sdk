9124e19002d4b7d55d6d9e1e5c49b8dc96a5895d
Martin Raszyk <martin.raszyk@dfinity.org>
2025-12-15 12:31:41 +0100

    rename source/target to migrated/replaced

--- e2e/tests-dfx/canister_migration.bash
+++ e2e/tests-dfx/canister_migration.bash
@@ -16,26 +16,26 @@ teardown() {
     dfx_start --system-canisters
     install_asset counter
     
-    # Update dfx.json: rename hello_backend -> source, and add target canister
-    jq '.canisters.source = .canisters.hello_backend | del(.canisters.hello_backend)' dfx.json | sponge dfx.json
-    jq '.canisters.target = { "main": "counter.mo", "type": "motoko" }' dfx.json | sponge dfx.json
+    # Update dfx.json: rename hello_backend -> migrated, and add replaced canister
+    jq '.canisters.migrated = .canisters.hello_backend | del(.canisters.hello_backend)' dfx.json | sponge dfx.json
+    jq '.canisters.replaced = { "main": "counter.mo", "type": "motoko" }' dfx.json | sponge dfx.json
 
-    # Deploy the source to the application subnet.
-    dfx deploy source
+    # Deploy the migrated canister to the application subnet.
+    dfx deploy migrated
 
-    # Create the target canister on the fiduciary subnet.
-    dfx canister create target --subnet-type fiduciary
+    # Create the replaced canister on the fiduciary subnet.
+    dfx canister create replaced --subnet-type fiduciary
 
-    dfx canister stop source
-    dfx canister stop target
+    dfx canister stop migrated
+    dfx canister stop replaced
 
-    # Make sure the source has enough cycles to do the migration.
-    dfx ledger fabricate-cycles --canister source --cycles 10000000000000
+    # Make sure the migrated canister has enough cycles to do the migration.
+    dfx ledger fabricate-cycles --canister migrated --cycles 10000000000000
 
     # The migration will take a few minutes to complete.
-    assert_command dfx canister migrate-id source --replace target --yes
+    assert_command dfx canister migrate-id migrated --replace replaced --yes
     assert_contains "Migration succeeded"
 
-    assert_command dfx canister status source
-    assert_command_fail dfx canister status target
+    assert_command dfx canister status migrated
+    assert_command_fail dfx canister status replaced
 }
