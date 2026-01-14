fc54fbb680755f452a551a213a92d3e511ddbce9
sasa-tomic <sasa.tomic@caffeine.ai>
2026-01-14 13:01:52 +0000

    Deploying to public-manifest from  @ 9ffbb0f1d8c843f0bff22d085c01cf41687a4dac 🚀

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ DFX_VERSION="${DFX_VERSION-}"
 DFXVM_INIT_YES="${DFXVM_INIT_YES-}"
 
 # The SHA and the time of the last commit that touched this file.
-SCRIPT_COMMIT_DESC="650c497ee5abcdea00c32c2ec177f58fff0fae72"
+SCRIPT_COMMIT_DESC="9ffbb0f1d8c843f0bff22d085c01cf41687a4dac"
 
 download_and_install() {
     SHASUM="$1"
